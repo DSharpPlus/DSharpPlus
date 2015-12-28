433542188416593616008bfb5d87c7210f4ea2c2
Mike Santiago <miketheripper1@msn.com>
2015-12-28 10:51:14 -0500

    Remove serialization references for WP

--- DiscordSharp/DiscordLoginException.cs
+++ DiscordSharp/DiscordLoginException.cs
@@ -7,16 +7,13 @@ using System.Threading.Tasks;
 
 namespace DiscordSharp 
 {
-    [Serializable]
-    public class DiscordLoginException : Exception, ISerializable
+    public class DiscordLoginException : Exception
     {
         public DiscordLoginException() : base("A generic exception occurred while trying to login to Discord.")
         {}
         public DiscordLoginException(string message) : base(message)
         {}
         public DiscordLoginException(string message, Exception inner) : base(message, inner)
-        {}
-        protected DiscordLoginException(SerializationInfo info, StreamingContext context) : base(info, context)
         {}
     }
 }
