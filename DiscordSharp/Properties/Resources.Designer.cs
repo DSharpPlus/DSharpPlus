cafddafac7ea60f95389f522b9dec9a24ee7e79f
Mike Santiago <miketheripper1@msn.com>
2015-12-28 12:17:37 -0500

    Remove unecessary resources

--- DiscordSharp/Properties/Resources.Designer.cs
+++ DiscordSharp/Properties/Resources.Designer.cs
@@ -59,15 +59,5 @@ namespace DiscordSharp.Properties {
                 resourceCulture = value;
             }
         }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Icon similar to (Icon).
-        /// </summary>
-        internal static System.Drawing.Icon ico {
-            get {
-                object obj = ResourceManager.GetObject("ico", resourceCulture);
-                return ((System.Drawing.Icon)(obj));
-            }
-        }
     }
 }
