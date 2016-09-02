f8d31f39ed5ead17b6639048841df6a52b232dc5
DrCreo <DrCreo1@gmail.com>
2016-09-02 21:18:43 +0100

    Added Toolbox Module
    Added Toolbox Module where utility functions will be added.

---
+++ SharpCord.Utility/Tools.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharpCord.ToolBox
+{
+    public static class Tools
+    {
+        /// <summary>
+        /// Takes a String containing path and converts it in to a Stream.
+        /// </summary>
+        /// <param name="uri"></param>
+        /// <returns></returns>
+        public static Stream ToStream(string uri)
+        {
+            Stream fs = File.OpenRead(uri);
+            return fs;
+        }
+    }
+}
