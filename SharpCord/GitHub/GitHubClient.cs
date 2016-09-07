1995dfb779300d86baf1292ce3415480fa0df155
afroraydude <rayjo5000@yahoo.com>
2016-09-06 20:06:51 -0400

    A few changes
    - Initialize usage of Octokit and DSharpPlusGitHubClient
    - Default Namesspace Change
    - Assembly name change

---
+++ SharpCord/GitHub/GitHubClient.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Octokit;
+
+namespace DSharpPlus.GitHub
+{
+    public class DSharpPlusGithubClient
+    {
+        public GitHubClient ghclient;
+
+        /// <summary>
+        /// Initializes the GitHub client for you
+        /// </summary>
+        /// <param name="client">The name of the client you will need to use</param>
+
+        public DSharpPlusGithubClient (string client = "default-dsharp-client")
+        {
+            ghclient = new GitHubClient(new ProductHeaderValue(client));
+        }
+
+        
+    }
+    
+}
---
+++ SharpCord/TestBot/TestBot.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DSharpPlus.GitHub;
+
+namespace SharpCord.TestBot
+{
+    class TestBot
+    {
+        DSharpPlusGithubClient client;
+
+        void Main()
+        {
+            client = new DSharpPlusGithubClient();
+            
+        }
+    }
+}
