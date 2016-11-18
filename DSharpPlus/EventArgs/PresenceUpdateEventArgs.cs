3aa186dea9d1f9e2ff5da19a07ba53c438e46d48
NaamloosDT <ryandejonge@outlook.com>
2016-11-18 22:37:47 +0100

    Added PresenceUpdate event
    Console got spammed bc too many unknown events

---
+++ DSharpPlus/EventArgs/PresenceUpdateEventArgs.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DSharpPlus
+{
+    public class PresenceUpdateEventArgs : EventArgs
+    {
+        public DiscordUser User;
+        public List<ulong> RoleIDs;
+        public string Game;
+        public ulong GuildID;
+        public string Status;
+    }
+}
