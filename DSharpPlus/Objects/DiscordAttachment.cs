51c0248a10e818e6cf706f4d3db565d3ceb2675f
NaamloosDT <ryandejonge@outlook.com>
2016-11-21 21:26:46 +0100

    Added DiscordAttachment instead of just an object

---
+++ DSharpPlus/Objects/DiscordAttachment.cs
@@ -0,0 +1,51 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSharpPlus
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class DiscordAttachment : SnowflakeObject
+    {
+        /// <summary>
+        /// Name of the file
+        /// </summary>
+        [JsonProperty("filename")]
+        public string FileName { get; internal set; }
+
+        /// <summary>
+        /// File size in bytes
+        /// </summary>
+        [JsonProperty("size")]
+        public int FileSize { get; internal set; }
+
+        /// <summary>
+        /// File URL
+        /// </summary>
+        [JsonProperty("url")]
+        public string Url { get; internal set; }
+
+        /// <summary>
+        /// Proxy file URL
+        /// </summary>
+        [JsonProperty("proxy_url")]
+        public string ProxyUrl { get; internal set; }
+
+        /// <summary>
+        /// Height (if image)
+        /// </summary>
+        [JsonProperty("height")]
+        public int Height { get; internal set; }
+
+        /// <summary>
+        /// Width (if image)
+        /// </summary>
+        [JsonProperty("width")]
+        public int Width { get; internal set; }
+    }
+}
