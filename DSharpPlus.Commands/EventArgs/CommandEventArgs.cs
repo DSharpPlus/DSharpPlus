81eea6e5db713c2ca7abaee8ba5b09125e2e91f6
Emzi0767 <matiz100@windowslive.com>
2017-03-12 11:18:19 +0100

    Updated CommandEventArgs with easy access to certain properties.

--- DSharpPlus.Commands/EventArgs/CommandEventArgs.cs
+++ DSharpPlus.Commands/EventArgs/CommandEventArgs.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Collections.ObjectModel;
 
 namespace DSharpPlus.Commands
 {
@@ -10,7 +8,11 @@ namespace DSharpPlus.Commands
     {
         public DiscordMessage Message { get; }
         public Command Command { get; }
-        public string[] Arguments { get; }
+        public IReadOnlyCollection<string> Arguments { get; }
+
+        public DiscordChannel Channel => this.Message.Parent;
+        public DiscordGuild Guild => this.Channel.Parent;
+        public DiscordUser Auhtor => this.Message.Author;
 
         public CommandEventArgs(DiscordMessage message, Command command)
         {
@@ -19,7 +21,7 @@ namespace DSharpPlus.Commands
             if (message.Content.Length > (CommandModule.instance.config.Prefix.Length + command.Name.Length))
             {
                 string args = message.Content.Substring(CommandModule.instance.config.Prefix.Length + command.Name.Length + 1);
-                Arguments = args.Split(new char[] { ' ' });
+                Arguments = new ReadOnlyCollection<string>(args.Split(new char[] { ' ' }));
             }
         }
     }
