fe4d0b11aeb14c117bf422b3773e414327658b13
Emzi0767 <matiz100@windowslive.com>
2017-03-16 18:35:28 +0100

    Added docstrings for VoiceNext.

--- DSharpPlus.VoiceNext/DiscordClientExtensions.cs
+++ DSharpPlus.VoiceNext/DiscordClientExtensions.cs
@@ -1,12 +1,25 @@
-﻿namespace DSharpPlus.VoiceNext
+﻿using DSharpPlus.VoiceNext.Codec;
+
+namespace DSharpPlus.VoiceNext
 {
     public static class DiscordClientExtensions
     {
         private static VoiceNextClient ClientInstance { get; set; }
 
+        /// <summary>
+        /// Creates a new VoiceNext client with default settings.
+        /// </summary>
+        /// <param name="client">Discord client to create VoiceNext instance for.</param>
+        /// <returns>VoiceNext client instance.</returns>
         public static VoiceNextClient UseVoiceNext(this DiscordClient client) =>
-            UseVoiceNext(client, new VoiceNextConfiguration { VoiceApplication = Codec.VoiceApplication.Music });
+            UseVoiceNext(client, new VoiceNextConfiguration { VoiceApplication = VoiceApplication.Music });
 
+        /// <summary>
+        /// Creates a new VoiceNext client with specified settings.
+        /// </summary>
+        /// <param name="client">Discord client to create VoiceNext instance for.</param>
+        /// <param name="config">Configuration for the VoiceNext client.</param>
+        /// <returns>VoiceNext client instance.</returns>
         public static VoiceNextClient UseVoiceNext(this DiscordClient client, VoiceNextConfiguration config)
         {
             ClientInstance = new VoiceNextClient(config);
@@ -14,6 +27,11 @@
             return ClientInstance;
         }
 
+        /// <summary>
+        /// Gets the active instance of VoiceNext client for the DiscordClient.
+        /// </summary>
+        /// <param name="client">Discord client to get VoiceNext instance for.</param>
+        /// <returns>VoiceNext client instance.</returns>
         public static VoiceNextClient GetVoiceNextClient(this DiscordClient client) =>
             ClientInstance;
     }
--- DSharpPlus.VoiceNext/VoiceNextConfiguration.cs
+++ DSharpPlus.VoiceNext/VoiceNextConfiguration.cs
@@ -1,7 +1,15 @@
-﻿namespace DSharpPlus.VoiceNext
+﻿using DSharpPlus.VoiceNext.Codec;
+
+namespace DSharpPlus.VoiceNext
 {
+    /// <summary>
+    /// VoiceNext client configuration.
+    /// </summary>
     public sealed class VoiceNextConfiguration
     {
-        public Codec.VoiceApplication VoiceApplication { get; set; }
+        /// <summary>
+        /// Gets or sets the encoding settings for this client. This decides whether the encoder will favour quality or smaller bandwidth.
+        /// </summary>
+        public VoiceApplication VoiceApplication { get; set; }
     }
 }
