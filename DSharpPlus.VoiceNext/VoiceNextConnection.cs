ff29ffcb5a0d8a702ba4899a97c41a39f8b03bd2
NaamloosDT <ryandejonge@outlook.com>
2017-04-17 01:42:11 +0200

    Highly experimental MS websocket implementation
    Use at own risk. This still has some unneccesary reconnects but at least
    it DOES reconnect :^)

--- DSharpPlus.VoiceNext/VoiceNextConnection.cs
+++ DSharpPlus.VoiceNext/VoiceNextConnection.cs
@@ -51,7 +51,7 @@ namespace DSharpPlus.VoiceNext
         private DiscordChannel Channel { get; set; }
 
         private UdpClient UdpClient { get; set; }
-        private WebSocketClient VoiceWs { get; set; }
+        private WebSocketWrapper VoiceWs { get; set; }
         private Thread HeartbeatThread { get; set; }
         private int HeartbeatInterval { get; set; }
         private DateTime LastHeartbeat { get; set; }
@@ -113,11 +113,10 @@ namespace DSharpPlus.VoiceNext
             this.IsDisposed = false;
 
             this.UdpClient = new UdpClient();
-            this.VoiceWs = new WebSocketClient($"wss://{this.ConnectionEndpoint.Host}");
-            this.VoiceWs.SocketClosed += this.VoiceWS_SocketClosed;
-            this.VoiceWs.SocketError += this.VoiceWS_SocketError;
-            this.VoiceWs.SocketMessage += this.VoiceWS_SocketMessage;
-            this.VoiceWs.SocketOpened += this.VoiceWS_SocketOpened;
+            this.VoiceWs = WebSocketWrapper.Create($"wss://{this.ConnectionEndpoint.Host}");
+            this.VoiceWs.OnDisconnect += this.VoiceWS_SocketClosed;
+            this.VoiceWs.OnMessage += this.VoiceWS_SocketMessage;
+            this.VoiceWs.OnConnect += this.VoiceWS_SocketOpened;
         }
 
         static VoiceNextConnection()
@@ -136,7 +135,7 @@ namespace DSharpPlus.VoiceNext
         /// <returns>A task representing the connection operation.</returns>
         internal async Task ConnectAsync()
         {
-            await Task.Run(() => this.VoiceWs.Connect()).ConfigureAwait(false);
+            await Task.Run(() => this.VoiceWs.InternalConnectAsync()).ConfigureAwait(false);
         }
 
         internal Task StartAsync()
@@ -154,7 +153,7 @@ namespace DSharpPlus.VoiceNext
                 }
             };
             var vdj = JsonConvert.SerializeObject(vdp, Formatting.None);
-            this.VoiceWs._socket.Send(vdj);
+            this.VoiceWs.SendMessage(vdj);
 
             return Task.Delay(0);
         }
@@ -232,7 +231,7 @@ namespace DSharpPlus.VoiceNext
             };
 
             var plj = JsonConvert.SerializeObject(pld, Formatting.None);
-            await Task.Run(() => this.VoiceWs._socket.Send(plj));
+            await Task.Run(() => this.VoiceWs.SendMessage(plj));
         }
 
         /// <summary>
@@ -255,7 +254,7 @@ namespace DSharpPlus.VoiceNext
             this.IsInitialized = false;
             try
             {
-                this.VoiceWs.Disconnect();
+                this.VoiceWs.InternalDisconnectAsync();
                 this.UdpClient.Close();
             }
             catch (Exception)
@@ -285,7 +284,7 @@ namespace DSharpPlus.VoiceNext
                         Payload = UnixTimestamp(dt)
                     };
                     var hbj = JsonConvert.SerializeObject(hbd);
-                    this.VoiceWs._socket.Send(hbj);
+                    this.VoiceWs.SendMessage(hbj);
 
                     this.LastHeartbeat = dt;
                     Thread.Sleep(this.HeartbeatInterval);
@@ -333,7 +332,7 @@ namespace DSharpPlus.VoiceNext
                 }
             };
             var vsj = JsonConvert.SerializeObject(vsp, Formatting.None);
-            this.VoiceWs._socket.Send(vsj);
+            this.VoiceWs.SendMessage(vsj);
         }
 
         private Task Stage2()
@@ -391,9 +390,9 @@ namespace DSharpPlus.VoiceNext
             }
         }
 
-        private Task VoiceWS_SocketClosed(CloseEventArgs e)
+        private Task VoiceWS_SocketClosed()
         {
-            this.Discord.DebugLogger.LogMessage(LogLevel.Debug, "VoiceNext", $"Voice session closed: {e.Reason}; clean {e.WasClean}", DateTime.Now);
+            this.Discord.DebugLogger.LogMessage(LogLevel.Debug, "VoiceNext", $"Voice session closed", DateTime.Now);
             this.Dispose();
             return Task.Delay(0);
         }
@@ -403,9 +402,9 @@ namespace DSharpPlus.VoiceNext
             return Task.Delay(0);
         }
 
-        private async Task VoiceWS_SocketMessage(MessageEventArgs e)
+        private async Task VoiceWS_SocketMessage(WebSocketMessageArgs e)
         {
-            await this.HandleDispatch(JObject.Parse(e.Data));
+            await this.HandleDispatch(JObject.Parse(e.message));
         }
 
         private async Task VoiceWS_SocketOpened()
---
+++ DSharpPlus/Socket/WebSocketWrapper.cs
@@ -0,0 +1,222 @@
+﻿using System;
+using System.Net.WebSockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DSharpPlus
+{
+    public class WebSocketMessageArgs : EventArgs
+    {
+        public string message { get; set; }
+    }
+
+    public class WebSocketWrapper
+    {
+        internal const int ReceiveChunkSize = 1024;
+        internal const int SendChunkSize = 1024;
+
+        internal ClientWebSocket _ws;
+        internal Uri _uri;
+        internal CancellationTokenSource _cancellationTokenSource = new CancellationTokenSource();
+        internal CancellationToken _cancellationToken;
+        internal bool _connected;
+
+        public event AsyncEventHandler OnConnect
+        {
+            add { this._on_connect.Register(value); }
+            remove { this._on_connect.Unregister(value); }
+        }
+        private AsyncEvent _on_connect = new AsyncEvent();
+
+        public event AsyncEventHandler OnDisconnect
+        {
+            add { this._on_disconnect.Register(value); }
+            remove { this._on_disconnect.Unregister(value); }
+        }
+        private AsyncEvent _on_disconnect = new AsyncEvent();
+
+        public event AsyncEventHandler<WebSocketMessageArgs> OnMessage
+        {
+            add { this._on_message.Register(value); }
+            remove { this._on_message.Unregister(value); }
+        }
+        private AsyncEvent<WebSocketMessageArgs> _on_message = new AsyncEvent<WebSocketMessageArgs>();
+
+        protected WebSocketWrapper(string uri)
+        {
+            _ws = new ClientWebSocket();
+            _ws.Options.KeepAliveInterval = TimeSpan.FromSeconds(20);
+            _uri = new Uri(uri);
+            _cancellationToken = _cancellationTokenSource.Token;
+        }
+
+        /// <summary>
+        /// Creates a new instance.
+        /// </summary>
+        /// <param name="uri">The URI of the WebSocket server.</param>
+        /// <returns></returns>
+        public static WebSocketWrapper Create(string uri)
+        {
+            return new WebSocketWrapper(uri);
+        }
+
+        /// <summary>
+        /// Connects to the WebSocket server.
+        /// </summary>
+        /// <returns></returns>
+        public async Task<WebSocketWrapper> ConnectAsync()
+        {
+            await InternalConnectAsync();
+            return this;
+        }
+
+        /// <summary>
+        /// Set the Action to call when the connection has been established.
+        /// </summary>
+        /// <returns></returns>
+        public async Task<WebSocketWrapper> OnConnectAsync()
+        {
+            await _on_connect.InvokeAsync();
+            return this;
+        }
+
+        /// <summary>
+        /// Set the Action to call when the connection has been terminated.
+        /// </summary>
+        /// <returns></returns>
+        public async Task<WebSocketWrapper> OnDisconnectAsync()
+        {
+            await _on_disconnect.InvokeAsync();
+            return this;
+        }
+
+        /// <summary>
+        /// Send a message to the WebSocket server.
+        /// </summary>
+        /// <param name="message">The message to send</param>
+        public void SendMessage(string message)
+        {
+            SendMessageAsync(message);
+        }
+
+        internal void SendMessageAsync(string message)
+        {
+            if (_ws.State != WebSocketState.Open)
+            {
+                return;
+            }
+
+            var messageBuffer = Encoding.UTF8.GetBytes(message);
+            var messagesCount = (int)Math.Ceiling((double)messageBuffer.Length / SendChunkSize);
+
+            for (var i = 0; i < messagesCount; i++)
+            {
+                var offset = (SendChunkSize * i);
+                var count = SendChunkSize;
+                var lastMessage = ((i + 1) == messagesCount);
+
+                if ((count * (i + 1)) > messageBuffer.Length)
+                {
+                    count = messageBuffer.Length - offset;
+                }
+                _ws.SendAsync(new ArraySegment<byte>(messageBuffer, offset, count), WebSocketMessageType.Text, lastMessage, _cancellationToken).Wait();
+            }
+        }
+
+        internal async Task InternalConnectAsync()
+        {
+            // laziness intensifies
+            _connected = true;
+            _ws = new ClientWebSocket();
+            try
+            {
+                await _ws.ConnectAsync(_uri, _cancellationToken);
+                await CallOnConnectedAsync();
+                await StartListen();
+            }
+            catch (Exception)
+            {
+
+            }
+        }
+        internal async Task InternalDisconnectAsync()
+        {
+            // lazy again
+            _connected = false;
+            try
+            {
+                _ws.CloseAsync(WebSocketCloseStatus.NormalClosure, "", _cancellationToken).Wait();
+                _ws.Abort();
+                _ws.Dispose();
+                await CallOnDisconnectedAsync();
+            }
+            catch (Exception)
+            {
+
+            }
+        }
+
+        internal async Task InternalReconnectAsync()
+        {
+            await InternalDisconnectAsync();
+            await InternalConnectAsync();
+        }
+
+        internal async Task StartListen()
+        {
+            var buffer = new byte[ReceiveChunkSize];
+            try
+            {
+                while (_connected)
+                {
+                    var stringResult = new StringBuilder();
+                    WebSocketReceiveResult result;
+                    do
+                    {
+                        result = await _ws.ReceiveAsync(new ArraySegment<byte>(buffer), _cancellationToken);
+                        await Task.Yield();
+                        if (result.MessageType == WebSocketMessageType.Close)
+                        {
+                            await InternalDisconnectAsync();
+                        }
+                        else
+                        {
+                            var str = Encoding.UTF8.GetString(buffer, 0, result.Count);
+                            stringResult.Append(str);
+                        }
+
+                    } while (!result.EndOfMessage);
+                    await CallOnMessageAsync(stringResult);
+                }
+            }
+            catch (Exception)
+            {
+            }
+            await InternalDisconnectAsync();
+        }
+
+        internal async Task CallOnMessageAsync(StringBuilder stringResult)
+        {
+            await _on_message.InvokeAsync(new WebSocketMessageArgs()
+            {
+                message = stringResult.ToString()
+            });
+        }
+
+        internal async Task CallOnDisconnectedAsync()
+        {
+            await _on_disconnect.InvokeAsync();
+        }
+
+        internal async Task CallOnConnectedAsync()
+        {
+            await _on_connect.InvokeAsync();
+        }
+
+        internal static void RunInTask(Action action)
+        {
+            Task.Factory.StartNew(action);
+        }
+    }
+}
