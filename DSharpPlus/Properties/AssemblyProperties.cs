de0f4194f95dcf9b60193e0086f60e904b03a918
Emzi0767 <matiz100@windowslive.com>
2017-04-18 15:18:38 +0200

    First Ever™ .NET Core port. Still doesn't compile, but it's being worked on.

---
+++ DSharpPlus/Properties/AssemblyProperties.cs
@@ -0,0 +1,6 @@
+﻿// This defines properties of this assembly
+using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("DSharpPlus.Commands")]
+[assembly: InternalsVisibleTo("DSharpPlus.CommandsNext")]
+[assembly: InternalsVisibleTo("DSharpPlus.VoiceNext")]
