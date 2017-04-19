083afba3f5cd82757e2e9ea91ee0825f54bb1f9c
Emzi0767 <matiz100@windowslive.com>
2017-04-19 13:52:28 +0200

    Added WebSocket#-based WebSocket implementation.

---
+++ DSharpPlus.WebSocket.WebSocketSharp/WebSocketSharpClient.cs
@@ -0,0 +1,70 @@
+﻿using System.Threading.Tasks;
+using wss = WebSocketSharp;
+
+namespace DSharpPlus.WebSocket.WebSocketSharp
+{
+    /// <summary>
+    /// A WS#-based WebSocket client.
+    /// </summary>
+    public class WebSocketSharpClient : BaseWebSocketClient
+    {
+        internal wss.WebSocket _socket;
+
+        public override Task<BaseWebSocketClient> ConnectAsync(string uri)
+        {
+            _socket = new wss.WebSocket(uri);
+            _socket.OnOpen += (sender, e) => _connect.InvokeAsync().GetAwaiter().GetResult();
+            _socket.OnClose += (sender, e) => _disconnect.InvokeAsync().GetAwaiter().GetResult();
+            _socket.OnMessage += (sender, e) => _message.InvokeAsync(new WebSocketMessageEventArgs()
+            {
+                Message = e.Data
+            }).GetAwaiter().GetResult();
+            _socket.Connect();
+            return Task.FromResult<BaseWebSocketClient>(this);
+        }
+
+        public override Task InternalDisconnectAsync()
+        {
+            if (_socket.IsAlive)
+                _socket.Close();
+            return Task.Delay(0);
+        }
+
+        public override Task<BaseWebSocketClient> OnConnectAsync()
+        {
+            return Task.FromResult<BaseWebSocketClient>(this);
+        }
+
+        public override Task<BaseWebSocketClient> OnDisconnectAsync()
+        {
+            return Task.FromResult<BaseWebSocketClient>(this);
+        }
+
+        public override void SendMessage(string message)
+        {
+            if (_socket.IsAlive)
+                _socket.Send(message);
+        }
+
+        public override event AsyncEventHandler OnConnect
+        {
+            add { this._connect.Register(value); }
+            remove { this._connect.Unregister(value); }
+        }
+        private AsyncEvent _connect = new AsyncEvent();
+
+        public override event AsyncEventHandler OnDisconnect
+        {
+            add { this._disconnect.Register(value); }
+            remove { this._disconnect.Unregister(value);  }
+        }
+        private AsyncEvent _disconnect = new AsyncEvent();
+
+        public override event AsyncEventHandler<WebSocketMessageEventArgs> OnMessage
+        {
+            add { this._message.Register(value); }
+            remove { this._message.Unregister(value); }
+        }
+        private AsyncEvent<WebSocketMessageEventArgs> _message = new AsyncEvent<WebSocketMessageEventArgs>();
+    }
+}
--- DSharpPlus/Properties/AssemblyProperties.cs
+++ DSharpPlus/Properties/AssemblyProperties.cs
@@ -1,6 +1,9 @@
 ﻿// This defines properties of this assembly
 using System.Runtime.CompilerServices;
 
+[assembly: InternalsVisibleTo("DSharpPlus.WebSocket.WebSocketSharp")]
+[assembly: InternalsVisibleTo("DSharpPlus.WebSocket.WebSocket4Net")]
+
 [assembly: InternalsVisibleTo("DSharpPlus.Commands")]
 [assembly: InternalsVisibleTo("DSharpPlus.CommandsNext")]
 [assembly: InternalsVisibleTo("DSharpPlus.VoiceNext")]
