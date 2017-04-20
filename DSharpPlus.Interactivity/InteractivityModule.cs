767ce4bc9e765e900fd0c4d29c06bc291fff0419
NaamloosDT <ryandejonge@outlook.com>
2017-04-21 01:21:10 +0200

    First steps towards interactivity

---
+++ DSharpPlus.Interactivity/InteractivityModule.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DSharpPlus.Interactivity
+{
+    public static class InteractivityExtension
+    {
+        internal static InteractivityModule _interactivity_module;
+
+        public static InteractivityModule UseInteractivity(this DiscordClient c)
+        {
+            if (_interactivity_module != null)
+                throw new Exception("Interactivity module is already set!");
+
+            _interactivity_module = new InteractivityModule();
+            _interactivity_module.Setup(c);
+            return _interactivity_module;
+        }
+
+        public static InteractivityModule GetInteractivityModule(this DiscordClient c)
+        {
+            if (_interactivity_module == null)
+                throw new Exception("Interactivity module is not set!");
+
+            return _interactivity_module;
+        }
+    }
+
+    public class InteractivityModule : IModule
+    {
+        public DiscordClient Client { get { return this._client; } }
+        private DiscordClient _client;
+
+        public void Setup(DiscordClient client)
+        {
+            this._client = client;
+            // hook events 'n shit
+        }
+
+        public async Task<DiscordMessage> WaitForMessageAsync(Func<DiscordMessage, bool> predicate, TimeSpan timeout)
+        {
+            var tsc = new TaskCompletionSource<DiscordMessage>();
+            var ct = new CancellationTokenSource((int)timeout.TotalMilliseconds);
+            ct.Token.Register(() => tsc.TrySetResult(null));
+
+            _client.MessageCreated += async (e) =>
+            {
+                await Task.Yield();
+                if (predicate(e.Message))
+                {
+                    tsc.TrySetResult(e.Message);
+                    return;
+                }
+            };
+
+            DiscordMessage result = await tsc.Task;
+            return result;
+        }
+
+        public async Task<DiscordEmoji> WaitForReactionAsync(Func<DiscordEmoji, bool> predicate, ulong message_id, TimeSpan timeout)
+        {
+            bool found = false;
+            var tsc = new TaskCompletionSource<DiscordEmoji>();
+            var ct = new CancellationTokenSource((int)timeout.TotalMilliseconds);
+            ct.Token.Register(() => tsc.TrySetResult(null));
+
+            _client.MessageReactionAdd += async (e) =>
+            {
+                await Task.Yield();
+                if (predicate(e.Emoji))
+                {
+                    if (e.MessageID == message_id)
+                    {
+                        found = true;
+                        tsc.TrySetResult(e.Emoji);
+                        return;
+                    }
+                }
+            };
+
+            DiscordEmoji result = await tsc.Task;
+            return result;
+        }
+
+        public async Task<DiscordEmoji> WaitForReactionAsync(ulong message_id, TimeSpan timeout)
+        {
+            var tsc = new TaskCompletionSource<DiscordEmoji>();
+            var ct = new CancellationTokenSource((int)timeout.TotalMilliseconds);
+            ct.Token.Register(() => tsc.TrySetResult(null));
+
+            _client.MessageReactionAdd += async (e) =>
+            {
+                await Task.Yield();
+                if (e.MessageID == message_id)
+                {
+                    tsc.TrySetResult(e.Emoji);
+                    return;
+                }
+            };
+
+            DiscordEmoji result = await tsc.Task;
+            return result;
+        }
+
+        public async Task<ConcurrentDictionary<string, int>> CollectReactionsAsync(DiscordMessage m, TimeSpan timeout)
+        {
+            ConcurrentDictionary<string, int> Reactions = new ConcurrentDictionary<string, int>();
+            var tsc = new TaskCompletionSource<ConcurrentDictionary<string, int>>();
+            var ct = new CancellationTokenSource((int)timeout.TotalMilliseconds);
+            ct.Token.Register(() => tsc.TrySetResult(Reactions));
+            _client.MessageReactionAdd += async (e) =>
+            {
+                await Task.Yield();
+                if (e.MessageID == m.ID)
+                {
+                    if (Reactions.ContainsKey(e.Emoji.Name))
+                        Reactions[e.Emoji.Name]++;
+                    else
+                        Reactions.TryAdd(e.Emoji.Name, 1);
+                }
+            };
+
+            _client.MessageReactionRemove += async (e) =>
+            {
+                await Task.Yield();
+                if (e.MessageID == m.ID)
+                {
+                    if (Reactions.ContainsKey(e.Emoji.Name))
+                    {
+                        Reactions[e.Emoji.Name]--;
+                        if (Reactions[e.Emoji.Name] == 0)
+                            Reactions.TryRemove(e.Emoji.Name, out int something);
+                    }
+                }
+            };
+
+            _client.MessageReactionRemoveAll += async (e) =>
+            {
+                await Task.Yield();
+                if (e.MessageID == m.ID)
+                {
+                    Reactions = new ConcurrentDictionary<string, int>();
+                }
+            };
+
+            return await tsc.Task;
+        }
+
+    }
+}
