5786ba705ff434ead48e8ba41314c3c82c305cb6
NaamloosDT <ryandejonge@outlook.com>
2017-04-21 01:21:10 +0200

    First steps towards interactivity

--- DSharpPlus.Test/TestBotCommands.cs
+++ DSharpPlus.Test/TestBotCommands.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 using DSharpPlus.Commands;
 //using DSharpPlus.VoiceNext;
 using NAudio.Wave;
+using DSharpPlus.Interactivity;
 
 namespace DSharpPlus.Test
 {
@@ -22,6 +23,29 @@ namespace DSharpPlus.Test
 Servername: {e.Guild.Name}
 Serverowner: {e.Guild.OwnerID}
 ```");
+        }
+
+        public async Task Cunt(CommandEventArgs e)
+        {
+            await e.Message.Respond("u wot");
+            DiscordMessage m = await TestBot.Discord.GetInteractivityModule().WaitForMessageAsync(xm => xm.Content.ToLower() == "no u", TimeSpan.FromSeconds(30));
+            if (m == null)
+                await e.Message.Respond("that's what i thought u lil basterd");
+            else
+                await e.Message.Respond("What the fuck did you just fucking say about me, you little bitch? I’ll have you know I graduated top of my class in the Navy Seals, and I’ve been involved in numerous secret raids on Al-Quaeda, and I have over 300 confirmed kills. I am trained in gorilla warfare and I’m the top sniper in the entire US armed forces. You are nothing to me but just another target. I will wipe you the fuck out with precision the likes of which has never been seen before on this Earth, mark my fucking words. You think you can get away with saying that shit to me over the Internet? Think again, fucker. As we speak I am contacting my secret network of spies across the USA and your IP is being traced right now so you better prepare for the storm, maggot. The storm that wipes out the pathetic little thing you call your life. You’re fucking dead, kid. I can be anywhere, anytime, and I can kill you in over seven hundred ways, and that’s just with my bare hands. Not only am I extensively trained in unarmed combat, but I have access to the entire arsenal of the United States Marine Corps and I will use it to its full extent to wipe your miserable ass off the face of the continent, you little shit. If only you could have known what unholy retribution your little “clever” comment was about to bring down upon you, maybe you would have held your fucking tongue. But you couldn’t, you didn’t, and now you’re paying the price, you goddamn idiot. I will shit fury all over you and you will drown in it. You’re fucking dead, kiddo.");
+        }
+
+        public async Task Poll(CommandEventArgs e)
+        {
+            var m = await e.Message.Respond("Hey everyone! Add some reactions to this message! you've got 30 seconds!");
+            await e.Message.Delete();
+            var list = await TestBot.Discord.GetInteractivityModule().CollectReactionsAsync(m, TimeSpan.FromSeconds(30));
+            string reactions = "We're done people!\n\nReactions:";
+            foreach(var collected in list)
+            {
+                reactions += "\n" + collected.Key + ": " + collected.Value + "times!";
+            }
+            await m.Respond(reactions);
         }
 
         public async Task Kill(CommandEventArgs e)
