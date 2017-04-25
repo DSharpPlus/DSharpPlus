fbde4e933403e84d16f45b3c0d23a8fded9513f3
Emzi0767 <matiz100@windowslive.com>
2017-04-25 10:55:45 +0200

    ...and bumped versions.

--- DSharpPlus/Properties/AssemblyInfo.cs
+++ DSharpPlus/Properties/AssemblyInfo.cs
@@ -33,8 +33,8 @@ using System.Runtime.InteropServices;
 // Sie können alle Werte angeben oder die standardmäßigen Build- und Revisionsnummern 
 // übernehmen, indem Sie "*" eingeben:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.7.3.0")]
-[assembly: AssemblyFileVersion("1.7.3.0")]
+[assembly: AssemblyVersion("1.7.4.0")]
+[assembly: AssemblyFileVersion("1.7.4.0")]
 [assembly: NeutralResourcesLanguage("en")]
 
 [assembly: InternalsVisibleTo("DSharpPlus.Commands")]
