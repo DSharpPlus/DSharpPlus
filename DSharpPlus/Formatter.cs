dd4c20a47bca8b4506bb2377aeebad163ae81dd7
Emzi0767 <matiz100@windowslive.com>
2017-06-13 12:37:27 +0200

    Fixed Formatter.

--- DSharpPlus/Formatter.cs
+++ DSharpPlus/Formatter.cs
@@ -3,11 +3,11 @@
     public static class Formatter
     {
         public static string BlockCode(string content, string language = "") => $"```{language}\n{content}\n```";
-        public static string InlineCode(string content) => $"``{content}``";
+        public static string InlineCode(string content) => $"`{content}`";
         public static string Bold(string content) => $"**{content}**";
         public static string Italic(string content) => $"*{content}*";
-        public static string Underline(string content) => $"_{content}_";
-        public static string Strike(string content) => $"~{content}~";
+        public static string Underline(string content) => $"__{content}__";
+        public static string Strike(string content) => $"~~{content}~~";
 
         public static string Mention(DiscordMember member, bool nickname = false) => Mention(member, nickname);
         public static string Mention(DiscordUser user, bool nickname = false) => (nickname ? $"<@!{user.Id}>" : $"<@{user.Id}>");
