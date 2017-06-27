dce6dde119f72b7b2bcd01e3624f31575723ba89
Emzi0767 <matiz100@windowslive.com>
2017-06-27 13:50:36 +0200

    DiscordEmoji is now usable as dictionary key.

--- DSharpPlus/Objects/DiscordEmoji.cs
+++ DSharpPlus/Objects/DiscordEmoji.cs
@@ -6,27 +6,30 @@ using Newtonsoft.Json;
 namespace DSharpPlus
 {
     /// <summary>
-    /// 
+    /// Represents a Discord emoji.
     /// </summary>
-    public partial class DiscordEmoji : SnowflakeObject
+    public partial class DiscordEmoji : SnowflakeObject, IEquatable<DiscordEmoji>
     {
         /// <summary>
-        /// Emoji Name
+        /// Gets the name of this emoji.
         /// </summary>
         [JsonProperty("name", NullValueHandling = NullValueHandling.Ignore)]
         public string Name { get; internal set; }
+
         /// <summary>
-        /// Roles this emoji is active for
+        /// Gets IDs the roles this emoji is enabled for.
         /// </summary>
         [JsonProperty("roles", NullValueHandling = NullValueHandling.Ignore)]
         public List<ulong> Roles { get; internal set; }
+
         /// <summary>
-        /// Whether this emoji must be wrapped in colons
+        /// Gets whether this emoji requires colons to use.
         /// </summary>
         [JsonProperty("require_colons", NullValueHandling = NullValueHandling.Ignore)]
         public bool RequireColons { get; internal set; }
+
         /// <summary>
-        /// Whether this emoji is managed
+        /// Gets whether this emoji is managed by an integration.
         /// </summary>
         [JsonProperty("managed", NullValueHandling = NullValueHandling.Ignore)]
         public bool Managed { get; internal set; }
@@ -40,6 +43,45 @@ namespace DSharpPlus
             if (this.Id != 0)
                 return $"<:{this.Name}:{this.Id}>";
             return this.Name;
+        }
+
+        /// <summary>
+        /// Checks whether this <see cref="DiscordEmoji"/> is equal to another object.
+        /// </summary>
+        /// <param name="obj">Object to compare to.</param>
+        /// <returns>Whether the object is equal to this <see cref="DiscordEmoji"/>.</returns>
+        public override bool Equals(object obj)
+        {
+            return this.Equals(obj as DiscordEmoji);
+        }
+
+        /// <summary>
+        /// Checks whether this <see cref="DiscordEmoji"/> is equal to another <see cref="DiscordEmoji"/>.
+        /// </summary>
+        /// <param name="e"><see cref="DiscordEmoji"/> to compare to.</param>
+        /// <returns>Whether the <see cref="DiscordEmoji"/> is equal to this <see cref="DiscordEmoji"/>.</returns>
+        public bool Equals(DiscordEmoji e)
+        {
+            if (ReferenceEquals(e, null))
+                return false;
+
+            if (ReferenceEquals(this, e))
+                return true;
+
+            return this.Id == e.Id && this.Name == e.Name;
+        }
+
+        /// <summary>
+        /// Gets the hash code for this <see cref="DiscordEmoji"/>.
+        /// </summary>
+        /// <returns>The hash code for this <see cref="DiscordEmoji"/>.</returns>
+        public override int GetHashCode()
+        {
+            var hash = 13;
+            hash = (hash * 7) + this.Id.GetHashCode();
+            hash = (hash * 7) + this.Name.GetHashCode();
+
+            return hash;
         }
 
         internal string ToReactionString()
@@ -111,5 +153,34 @@ namespace DSharpPlus
 
             throw new ArgumentException(nameof(name), "Invalid emoji name specified.");
         }
+
+        /// <summary>
+        /// Gets whether the two <see cref="DiscordEmoji"/> objects are equal.
+        /// </summary>
+        /// <param name="e1">First emoji to compare.</param>
+        /// <param name="e2">Second emoji to compare.</param>
+        /// <returns>Whether the two emoji are equal.</returns>
+        public static bool operator ==(DiscordEmoji e1, DiscordEmoji e2)
+        {
+            var o1 = e1 as object;
+            var o2 = e2 as object;
+
+            if ((o1 == null && o2 != null) || (o1 != null && o2 == null))
+                return false;
+
+            if (o1 == null && o2 == null)
+                return true;
+
+            return e1.Id == e2.Id && e1.Name == e2.Name;
+        }
+
+        /// <summary>
+        /// Gets whether the two <see cref="DiscordEmoji"/> objects are not equal.
+        /// </summary>
+        /// <param name="e1">First emoji to compare.</param>
+        /// <param name="e2">Second emoji to compare.</param>
+        /// <returns>Whether the two emoji are not equal.</returns>
+        public static bool operator !=(DiscordEmoji e1, DiscordEmoji e2) => 
+            !(e1 == e2);
     }
 }
