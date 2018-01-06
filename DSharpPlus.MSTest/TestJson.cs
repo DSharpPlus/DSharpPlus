54c1304eb419fc2b7ed08c10934e2fa4c2fee066
uwx <cadrekucra@gmx.com>
2018-01-06 20:10:25 -0200

    add an MSTest project, and a test for the custom Json serializer

---
+++ DSharpPlus.MSTest/TestJson.cs
@@ -0,0 +1,30 @@
+﻿using DSharpPlus.Entities;
+using DSharpPlus.Net.Serialization;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json;
+
+namespace DSharpPlus.Test
+{
+    [TestClass]
+    public class TestJson
+    {
+        [TestMethod]
+        public void TestOptionalSerialization()
+        {
+            // ulong
+            Assert.AreEqual("[0]", DiscordJson.SerializeObject(new[] { new Optional<ulong?>(0UL) }));
+            Assert.AreEqual("[]" , DiscordJson.SerializeObject(new[] { new Optional<ulong?>() }));
+            // bool
+            Assert.AreEqual("[true]" , DiscordJson.SerializeObject(new[] { new Optional<bool?>(true) }));
+            Assert.AreEqual("[false]", DiscordJson.SerializeObject(new[] { new Optional<bool?>(false) }));
+            Assert.AreEqual("[]"     , DiscordJson.SerializeObject(new[] { new Optional<bool?>() }));
+            
+            Assert.AreEqual(@"[{""HasValue"":true,""Value"":0}]", DiscordJson.SerializeObject(new[] { new Optional<ulong>(0UL) }));
+            // `System.InvalidOperationException: Value is not set.` wrapped in JsonSerializationException 
+            Assert.ThrowsException<JsonSerializationException>(() =>
+            {
+                DiscordJson.SerializeObject(new[] {new Optional<ulong>()});
+            });
+        }
+    }
+}
