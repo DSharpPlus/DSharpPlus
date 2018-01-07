d43ab7998cb64b34e8072bbb0047a089ad1ffde5
uwx <cadrekucra@gmx.com>
2018-01-07 02:34:09 -0200

    fix SetMuteAsync, SetDeafAsync and ReplaceRolesAsync not correctly using optional params

--- DSharpPlus/Entities/DiscordMember.cs
+++ DSharpPlus/Entities/DiscordMember.cs
@@ -286,7 +286,7 @@ namespace DSharpPlus.Entities
         /// <param name="reason">Reason for audit logs.</param>
         /// <returns></returns>
         public Task SetMuteAsync(bool mute, string reason = null) 
-            => this.Discord.ApiClient.ModifyGuildMemberAsync(_guild_id, this.Id, null, null, mute, null, null, reason);
+            => this.Discord.ApiClient.ModifyGuildMemberAsync(_guild_id, this.Id, default, default, mute, default, default, reason);
         
         /// <summary>
         /// Sets this member's voice deaf status.
@@ -295,7 +295,7 @@ namespace DSharpPlus.Entities
         /// <param name="reason">Reason for audit logs.</param>
         /// <returns></returns>
         public Task SetDeafAsync(bool deaf, string reason = null) 
-            => this.Discord.ApiClient.ModifyGuildMemberAsync(_guild_id, this.Id, null, null, null, deaf, null, reason);
+            => this.Discord.ApiClient.ModifyGuildMemberAsync(_guild_id, this.Id, default, default, default, deaf, default, reason);
 
         /// <summary>
         /// Modifies this member.
@@ -351,7 +351,8 @@ namespace DSharpPlus.Entities
         /// <param name="reason">Reason for audit logs.</param>
         /// <returns></returns>
         public Task ReplaceRolesAsync(IEnumerable<DiscordRole> roles, string reason = null) 
-            => this.Discord.ApiClient.ModifyGuildMemberAsync(this.Guild.Id, this.Id, null, roles.Select(xr => xr.Id), null, null, null, reason);
+            => this.Discord.ApiClient.ModifyGuildMemberAsync(this.Guild.Id, this.Id, default,
+                new Optional<IEnumerable<ulong>>(roles.Select(xr => xr.Id)), default, default, default, reason);
 
         /// <summary>
         /// Bans a this member from their guild.
