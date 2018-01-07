90e29450f49ad19da53cf5c15233d03f2006988b
uwx <cadrekucra@gmx.com>
2018-01-06 22:09:59 -0200

    fix the test case no longer applying to the latest version of the Optional serializer

--- DSharpPlus.MSTest/TestJson.cs
+++ DSharpPlus.MSTest/TestJson.cs
@@ -14,17 +14,14 @@ namespace DSharpPlus.Test
             // ulong
             Assert.AreEqual("[0]", DiscordJson.SerializeObject(new[] { new Optional<ulong?>(0UL) }));
             Assert.AreEqual("[]" , DiscordJson.SerializeObject(new[] { new Optional<ulong?>() }));
-            // bool
+            // bool?
             Assert.AreEqual("[true]" , DiscordJson.SerializeObject(new[] { new Optional<bool?>(true) }));
             Assert.AreEqual("[false]", DiscordJson.SerializeObject(new[] { new Optional<bool?>(false) }));
             Assert.AreEqual("[]"     , DiscordJson.SerializeObject(new[] { new Optional<bool?>() }));
-            
-            Assert.AreEqual(@"[{""HasValue"":true,""Value"":0}]", DiscordJson.SerializeObject(new[] { new Optional<ulong>(0UL) }));
-            // `System.InvalidOperationException: Value is not set.` wrapped in JsonSerializationException 
-            Assert.ThrowsException<JsonSerializationException>(() =>
-            {
-                DiscordJson.SerializeObject(new[] {new Optional<ulong>()});
-            });
+            // bool
+            Assert.AreEqual("[true]" , DiscordJson.SerializeObject(new[] { new Optional<bool>(true) }));
+            Assert.AreEqual("[false]", DiscordJson.SerializeObject(new[] { new Optional<bool>(false) }));
+            Assert.AreEqual("[]"     , DiscordJson.SerializeObject(new[] { new Optional<bool>() }));
         }
     }
 }
