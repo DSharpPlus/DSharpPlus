0e9eda9cbd0450a076efbb515daf6869415d7f35
uwx <cadrekucra@gmx.com>
2018-01-13 01:49:04 -0200

    change Optional serializer to use ContractResolver to check whether or not to serialize the value

--- DSharpPlus.MSTest/TestJson.cs
+++ DSharpPlus.MSTest/TestJson.cs
@@ -1,4 +1,5 @@
 ﻿using DSharpPlus.Entities;
+using DSharpPlus.Net.Abstractions;
 using DSharpPlus.Net.Serialization;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Newtonsoft.Json;
@@ -22,6 +23,27 @@ namespace DSharpPlus.Test
             Assert.AreEqual("[true]" , DiscordJson.SerializeObject(new[] { new Optional<bool>(true) }));
             Assert.AreEqual("[false]", DiscordJson.SerializeObject(new[] { new Optional<bool>(false) }));
             Assert.AreEqual("[]"     , DiscordJson.SerializeObject(new[] { new Optional<bool>() }));
+        }
+
+        [TestMethod]
+        public void TestOptionalSerialization2()
+        {
+            var p = new RestGuildModifyPayload();
+            Assert.AreEqual("{}", DiscordJson.SerializeObject(p));
+            Assert.AreEqual("{}", DiscordJson.SerializeObject(new RestGuildModifyPayload()
+            {
+                SystemChannelId = default,
+            }));
+            Assert.AreEqual(@"{""name"":""doobilip"",""system_channel_id"":null}", DiscordJson.SerializeObject(new RestGuildModifyPayload()
+            {
+                Name = "doobilip",
+                SystemChannelId = null,
+            }));
+            Assert.AreEqual(@"{""name"":""doobilip2"",""system_channel_id"":420}", DiscordJson.SerializeObject(new RestGuildModifyPayload()
+            {
+                Name = "doobilip2",
+                SystemChannelId = 420,
+            }));
         }
     }
 }
--- DSharpPlus/Net/Serialization/DiscordJson.cs
+++ DSharpPlus/Net/Serialization/DiscordJson.cs
@@ -4,19 +4,59 @@ using System.Reflection;
 using DSharpPlus.Entities;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
+using Newtonsoft.Json.Serialization;
 
 namespace DSharpPlus.Net.Serialization
 {
     public static class DiscordJson
     {
-        private static readonly OptionalJsonConverter OptionalJsonConverter = new OptionalJsonConverter();
+        private static readonly JsonConverter[] JsonConverters =
+        {
+            new OptionalJsonConverter()
+        };
+        private static readonly OptionalJsonContractResolver OptionalJsonContractResolver = new OptionalJsonContractResolver();
 
         /// <summary>Serializes the specified object to a JSON string.</summary>
         /// <param name="value">The object to serialize.</param>
         /// <returns>A JSON string representation of the object.</returns>
         public static string SerializeObject(object value)
         {
-            return JsonConvert.SerializeObject(value, OptionalJsonConverter);
+            return JsonConvert.SerializeObject(value, new JsonSerializerSettings
+            {
+                Converters = JsonConverters,
+                ContractResolver = OptionalJsonContractResolver
+            });
+        }
+    }
+
+    public class OptionalJsonContractResolver : DefaultContractResolver
+    {
+        protected override JsonProperty CreateProperty(MemberInfo member, MemberSerialization memberSerialization)
+        {
+            var property = base.CreateProperty(member, memberSerialization);
+
+            var type = property.PropertyType;
+            
+            if (!type.GetTypeInfo().IsGenericType)
+                return property;
+
+            if (type.GetGenericTypeDefinition() != typeof(Optional<>))
+                return property;
+            
+            // we cache the PropertyInfo object here (it's captured in closure). we don't have direct 
+            // access to the property value so we have to reflect into it from the parent instance
+            // we use UnderlyingName instead of PropertyName in case the C# name is different from the Json name.
+            var optionalProp = property.DeclaringType.GetTypeInfo().GetDeclaredProperty(property.UnderlyingName);
+            property.ShouldSerialize = instance => // instance here is the declaring (parent) type
+            {
+                // this is the Optional<T> object
+                var optionalValue = optionalProp.GetValue(instance);
+                // get the HasValue property of the Optional<T> object and cast it to a bool, and only serialize it if
+                // it's present
+                return (bool)optionalValue.GetType().GetTypeInfo().GetDeclaredProperty("HasValue").GetValue(optionalValue);
+            };
+    
+            return property;
         }
     }
 
@@ -25,11 +65,22 @@ namespace DSharpPlus.Net.Serialization
         public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
         {
             var typeInfo = value.GetType().GetTypeInfo();
-            var hasValue = (bool) typeInfo.GetDeclaredProperty("HasValue").GetValue(value);
-
-            if (!hasValue) return;
-            var t = JToken.FromObject(typeInfo.GetDeclaredProperty("Value").GetValue(value));
-            t.WriteTo(writer);
+            // we don't check for HasValue here since it's checked above
+            var val = typeInfo.GetDeclaredProperty("Value").GetValue(value);
+            // JToken.FromObject will throw if `null` so we manually write a null value.
+            if (val == null)
+            {
+                // you can read serializer.NullValueHandling here, but unfortunately you can **not** skip serialization
+                // here, or else you will get a nasty JsonWriterException, so we just ignore its value and manually
+                // write the null.
+                writer.WriteToken(JsonToken.Null);
+            }
+            else
+            {
+                // convert the value to a JSON object and write it to the property value.
+                var t = JToken.FromObject(val);
+                t.WriteTo(writer);
+            }
         }
 
         public override object ReadJson(JsonReader reader, Type objectType, object existingValue,
@@ -47,18 +98,5 @@ namespace DSharpPlus.Net.Serialization
 
             return objectType.GetGenericTypeDefinition() == typeof(Optional<>);
         }
-
-//        private static void DisplayTypeInfo(Type t)
-//        {
-//            Console.WriteLine($"\r\n{t}");
-//            Console.WriteLine($"\tIs this a generic type definition? {t.IsGenericTypeDefinition}");
-//            Console.WriteLine($"\tIs it a generic type? {t.IsGenericType}");
-//            var typeArguments = t.GetGenericArguments();
-//            Console.WriteLine($"\tList type arguments ({typeArguments.Length}):");
-//            foreach (var tParam in typeArguments)
-//            {
-//                Console.WriteLine($"\t\t{tParam}");
-//            }
-//        }
     }
 }
