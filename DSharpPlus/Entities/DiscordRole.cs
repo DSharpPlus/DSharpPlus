711c5ca45744fcbcf4bd8bede5dae2f534618749
uwx <cadrekucra@gmx.com>
2018-01-13 23:49:40 -0200

    implement Member.CanInteract and Role.CanInteract

--- DSharpPlus/Entities/DiscordRole.cs
+++ DSharpPlus/Entities/DiscordRole.cs
@@ -124,7 +124,20 @@ namespace DSharpPlus.Entities
                 return PermissionLevel.Allowed;
             return PermissionLevel.Unset;
         }
-
+        
+        /// <summary>
+        /// Checks if a given role can interact with another role (kick, ban, modify permissions).
+        /// Note that this only checks the role position and not the actual permission.
+        /// </summary>
+        /// <param name="target">the role to check against</param>
+        /// <returns>true if this role can interact with the target</returns>
+        /// <exception cref="ArgumentNullException">if target is null</exception>
+        public bool CanInteract(DiscordRole target)
+        {
+            if (target == null) throw new ArgumentNullException(nameof(target));
+            
+            return target.Position < this.Position;
+        }
         /// <summary>
         /// Returns a string representation of this role.
         /// </summary>
