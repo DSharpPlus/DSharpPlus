a8737d46abd8eb3236bdaebdd4433ad4ad85faa4
uwx <chris331hansen@outlook.com>
2018-01-15 21:46:51 -0200

    only use RegexOptions.Compiled on .NET FX and Standard 2.0

--- DSharpPlus.CommandsNext/Converters/TimeConverters.cs
+++ DSharpPlus.CommandsNext/Converters/TimeConverters.cs
@@ -34,10 +34,10 @@ namespace DSharpPlus.CommandsNext.Converters
 
         static TimeSpanConverter()
         {
-#if NETSTANDARD1_1
-            TimeSpanRegex = new Regex(@"^(?<days>\d+d\s*)?(?<hours>\d{1,2}h\s*)?(?<minutes>\d{1,2}m\s*)?(?<seconds>\d{1,2}s\s*)?$", RegexOptions.ECMAScript);
-#else
+#if NET45 || NET46 || NET47 || NETSTANDARD2_0
             TimeSpanRegex = new Regex(@"^(?<days>\d+d\s*)?(?<hours>\d{1,2}h\s*)?(?<minutes>\d{1,2}m\s*)?(?<seconds>\d{1,2}s\s*)?$", RegexOptions.ECMAScript | RegexOptions.Compiled);
+#else
+            TimeSpanRegex = new Regex(@"^(?<days>\d+d\s*)?(?<hours>\d{1,2}h\s*)?(?<minutes>\d{1,2}m\s*)?(?<seconds>\d{1,2}s\s*)?$", RegexOptions.ECMAScript);
 #endif
         }
 
