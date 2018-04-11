d701f08db0dd602ad1730c1e8b4af084835f6d0a
uwx <cadrekucra@gmx.com>
2018-04-11 19:48:22 -0300

    use string interpolation when possible

--- DSharpPlus/DiscordClient.cs
+++ DSharpPlus/DiscordClient.cs
@@ -2259,7 +2259,7 @@ namespace DSharpPlus
                 route += Endpoints.BOT;
             var bucket = this.ApiClient.Rest.GetBucket(RestRequestMethod.GET, route, new { }, out var path);
 
-            var url = new Uri(string.Concat(Utilities.GetApiBaseUri(), path));
+            var url = Utilities.GetApiUriFor(path);
             var request = new RestRequest(this, bucket, url, RestRequestMethod.GET, headers);
             DebugLogger.LogTaskFault(this.ApiClient.Rest.ExecuteRequestAsync(request), LogLevel.Error, "DSharpPlus", "Error while executing request: ");
             var response = await request.WaitForCompletionAsync().ConfigureAwait(false);
