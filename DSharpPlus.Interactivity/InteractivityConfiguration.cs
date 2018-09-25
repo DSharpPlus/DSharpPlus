a7b999825a48eb8fbc69d9497268cc765a34307f
NaamloosDT <NaamloosDT@gmail.com>
2018-09-25 23:19:35 +0200

    Simple start, I kept a few files.

--- DSharpPlus.Interactivity/InteractivityConfiguration.cs
+++ DSharpPlus.Interactivity/InteractivityConfiguration.cs
@@ -12,19 +12,6 @@ namespace DSharpPlus.Interactivity
         /// <para>Defaults to 1 minute.</para>
         /// </summary>
         public TimeSpan Timeout { internal get; set; } = TimeSpan.FromMinutes(1);
-
-        /// <summary>
-        /// <para>Sets the default pagination timeout.</para>
-        /// <para>Defaults to 2 minutes.</para>
-        /// </summary>
-        public TimeSpan PaginationTimeout { internal get; set; } = TimeSpan.FromMinutes(2);
-
-        /// <summary>
-        /// <para>Sets the default pagination timeout behaviour.</para>
-        /// <para>Defaults to <see cref="TimeoutBehaviour.Ignore"/>.</para>
-        /// </summary>
-        public TimeoutBehaviour PaginationBehavior { internal get; set; } = TimeoutBehaviour.Ignore;
-
         /// <summary>
         /// Creates a new instance of <see cref="InteractivityConfiguration"/>.
         /// </summary>
@@ -36,8 +23,6 @@ namespace DSharpPlus.Interactivity
         /// <param name="other">Configuration the properties of which are to be copied.</param>
         public InteractivityConfiguration(InteractivityConfiguration other)
         {
-            this.PaginationBehavior = other.PaginationBehavior;
-            this.PaginationTimeout = other.PaginationTimeout;
             this.Timeout = other.Timeout;
         }
     }
