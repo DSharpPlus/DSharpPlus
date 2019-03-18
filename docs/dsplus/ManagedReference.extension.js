1e554392fbbcd69922cab0286b1f0285a553d1ee
uwx <cadrekucra@gmx.com>
2019-03-17 23:37:29 -0300

    Make MREF extension logging prettier, use built xref for alias links

--- docs/dsplus/ManagedReference.extension.js
+++ docs/dsplus/ManagedReference.extension.js
@@ -1,5 +1,31 @@
 // Copyright (c) Microsoft. All rights reserved. Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+var Reset = "\x1b[0m"
+var Bright = "\x1b[1m"
+var Dim = "\x1b[2m"
+var Underscore = "\x1b[4m"
+var Blink = "\x1b[5m"
+var Reverse = "\x1b[7m"
+var Hidden = "\x1b[8m"
+
+var FgBlack = "\x1b[30m"
+var FgRed = "\x1b[31m"
+var FgGreen = "\x1b[32m"
+var FgYellow = "\x1b[33m"
+var FgBlue = "\x1b[34m"
+var FgMagenta = "\x1b[35m"
+var FgCyan = "\x1b[36m"
+var FgWhite = "\x1b[37m"
+
+var BgBlack = "\x1b[40m"
+var BgRed = "\x1b[41m"
+var BgGreen = "\x1b[42m"
+var BgYellow = "\x1b[43m"
+var BgBlue = "\x1b[44m"
+var BgMagenta = "\x1b[45m"
+var BgCyan = "\x1b[46m"
+var BgWhite = "\x1b[47m"
+
 /**
  * This method will be called at the start of exports.transform in ManagedReference.html.primary.js
  */
@@ -12,7 +38,7 @@ exports.preTransform = function (model) {
  */
 exports.postTransform = function (model) {
   try {
-    console.log('\nhi model: ' + model.type);
+    console.log('hi model: ' + model.type);
     
     model.aliases = [];
     handleItem(model, model.aliases);
@@ -23,23 +49,29 @@ exports.postTransform = function (model) {
     }
     
   } catch (e) {
-    console.log('\nFail: ' + e);
+    console.log(Bright + FgRed + '\nFail: ' + e + ',' + Object.keys(e) + Reset);
   }
 
   return model;
 }
 
 function handleItem(item, aliases) {
+  //console.log('item: ' + JSON.stringify(item));
   if (item.remarks) {
-    item.remarks = item.remarks.replace(/\[alias=(['"])(.*?)\1]/g, function($$) {
+    console.log('Remarks: ' + item.remarks);
+    item.remarks = item.remarks.replace(/\[alias=(['"]|&quot;)(.*?)\1]/g, function($$, $quot, $aliasName) {
       console.log($$);
+      console.log($quot);
+      console.log($aliasName);
       aliases.push({
         isAlias: true,
-        name: item.name.replace(),
+        name: item.name[0].value.replace(/^.*?(\(|$)/, $aliasName + '$1'),
         id: $$,
         aliasTo: item.uid,
-        targetName: item.name,
-        targetFullName: item.fullName
+        
+        targetXref: item.specName[0].value
+        //targetName: item.name[0].value,
+        //targetFullName: item.fullName[0].value
       });
       return '';
     });
