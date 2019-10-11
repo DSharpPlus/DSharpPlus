833c9c1e78d4d8c293202dca5e8e8f917bb90ffe
F1restar4 <yeahibiketowork@hitler.rocks>
2019-10-11 14:34:24 -0500

    Move this so we can skip a step

--- DSharpPlus/Entities/DiscordChannel.cs
+++ DSharpPlus/Entities/DiscordChannel.cs
@@ -604,11 +604,6 @@ namespace DSharpPlus.Entities
 
             // roles that member is in
             var mbRoles = mbr.Roles.Where(xr => xr.Id != everyoneRole.Id).ToArray();
-            // channel overrides for roles that member is in
-            var mbRoleOverrides = mbRoles
-                .Select(xr => this._permissionOverwrites.FirstOrDefault(xo => xo.Id == xr.Id))
-                .Where(xo => xo != null)
-                .ToList();
 
             // assign permissions from member's roles (in order)
             perms |= mbRoles.Aggregate(Permissions.None, (c, role) => c | role.Permissions);
@@ -616,6 +611,12 @@ namespace DSharpPlus.Entities
             // Adminstrator grants all permissions and cannot be overridden
             if ((perms & Permissions.Administrator) == Permissions.Administrator)
                 return PermissionMethods.FULL_PERMS;
+
+            // channel overrides for roles that member is in
+            var mbRoleOverrides = mbRoles
+                .Select(xr => this._permissionOverwrites.FirstOrDefault(xo => xo.Id == xr.Id))
+                .Where(xo => xo != null)
+                .ToList();
             
             // assign channel permission overwrites for @everyone pseudo-role
             var everyoneOverwrites = this._permissionOverwrites.FirstOrDefault(xo => xo.Id == everyoneRole.Id);
