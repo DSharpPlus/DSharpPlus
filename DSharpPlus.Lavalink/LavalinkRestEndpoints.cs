fbdc2843c4a4da400a853421061f35b95410c8ce
Neuheit <38368299+Neuheit@users.noreply.github.com>
2020-01-08 14:54:38 -0500

    Clean up unused usings

--- DSharpPlus.Lavalink/LavalinkRestEndpoints.cs
+++ DSharpPlus.Lavalink/LavalinkRestEndpoints.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace DSharpPlus.Lavalink
+﻿namespace DSharpPlus.Lavalink
 {
     internal static class Endpoints
     {
