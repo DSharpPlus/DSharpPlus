f4016624547aa8a93f2eb6e16489baf4c3afe9ee
Lachee <lachlan.h@hotmail.com>
2020-03-05 00:36:12 +1100

    Initial Mentions and DiscordMentions
    The DiscordMentions will be for internal user and the Mentions interface will be exposed on message functions.

---
+++ DSharpPlus/Entities/DiscordMentions.cs
@@ -0,0 +1,66 @@
+﻿using DSharpPlus.Net.Serialization;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DSharpPlus.Entities
+{
+    /// <summary>
+    /// Handles mentionables
+    /// </summary>
+    internal class DiscordMentions
+    {
+        //https://discordapp.com/developers/docs/resources/channel#allowed-mentions-object
+
+        /// <summary>
+        /// Collection roles to serialize
+        /// </summary>
+        [JsonProperty("roles", NullValueHandling = NullValueHandling.Ignore)]
+        public IEnumerable<ulong> Roles { get; }
+
+        /// <summary>
+        /// Collection of users to serialize
+        /// </summary>
+        [JsonProperty("users", NullValueHandling = NullValueHandling.Ignore)]
+        public IEnumerable<ulong> Users { get; }
+
+        /// <summary>
+        /// The values to be parsed
+        /// </summary>
+        [JsonProperty("parse", NullValueHandling = NullValueHandling.Ignore)]
+        public IEnumerable<string> Parse { get; }
+
+        internal DiscordMentions(IEnumerable<IMention> mentions)
+        {
+            HashSet<ulong> roles = new HashSet<ulong>();
+            HashSet<ulong> users = new HashSet<ulong>();
+            HashSet<string> parse = new HashSet<string>();
+
+            foreach(var m in mentions)
+            {
+                switch(m)
+                {
+                    case UserMention u:
+                        users.Add(u.Id);
+                        parse.Add("users");
+                        break;
+
+                    case RoleMention r:
+                        roles.Add(r.Id);
+                        parse.Add("roles");
+                        break;
+
+                    case EveryoneMention e:
+                        parse.Add("everyone");
+                        break;
+                }
+            }
+
+            Roles = roles;
+            Users = users;
+            Parse = parse;
+        }
+    }
+}
---
+++ DSharpPlus/Entities/Mentions.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DSharpPlus.Entities
+{
+    /// <summary>
+    /// Interface for mentionables
+    /// </summary>
+    internal interface IMention { }
+
+    /// <summary>
+    /// A everyone mention
+    /// </summary>
+    public readonly struct EveryoneMention : IMention { }
+
+    /// <summary>
+    /// A user mention
+    /// </summary>
+    public readonly struct UserMention : IMention
+    {
+        /// <summary>
+        /// Id of the user that is allowed to be mentioned
+        /// </summary>
+        public ulong Id { get; }
+
+        /// <summary>
+        /// Creates a new user mention based of the id
+        /// </summary>
+        /// <param name="id"></param>
+        public UserMention(ulong id) { this.Id = id;  }
+
+        /// <summary>
+        /// Creates a new user mention based of the user
+        /// </summary>
+        /// <param name="user"></param>
+        public UserMention(DiscordUser user) : this(user.Id) { }
+
+        public static implicit operator UserMention(DiscordUser user) => new UserMention(user.Id);
+    }
+
+    /// <summary>
+    /// A role mention
+    /// </summary>
+    public readonly struct RoleMention : IMention
+    {
+        /// <summary>
+        /// Id of the role that is allowed to be mentioned
+        /// </summary>
+        public ulong Id { get; }
+
+        /// <summary>
+        /// Creates a new role mention based of the id
+        /// </summary>
+        /// <param name="id"></param>
+        public RoleMention(ulong id) { this.Id = id; }
+
+        /// <summary>
+        /// Creates a new role mention based of the id
+        /// </summary>
+        /// <param name="role"></param>
+        public RoleMention(DiscordRole role) : this(role.Id) { }
+
+        public static implicit operator RoleMention(DiscordRole role) => new RoleMention(role.Id);
+    }
+
+
+}
