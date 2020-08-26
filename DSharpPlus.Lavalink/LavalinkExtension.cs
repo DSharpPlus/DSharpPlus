2d41b0801090cd12e9e3c2ae6fea0d0bc6e7f9df
Neuheit <38368299+Neuheit@users.noreply.github.com>
2020-08-26 16:26:37 -0400

    Why is XML here

--- DSharpPlus.Lavalink/LavalinkExtension.cs
+++ DSharpPlus.Lavalink/LavalinkExtension.cs
@@ -3,7 +3,6 @@ using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using System.Xml;
 using DSharpPlus.Entities;
 using DSharpPlus.Lavalink.EventArgs;
 using DSharpPlus.Net;
