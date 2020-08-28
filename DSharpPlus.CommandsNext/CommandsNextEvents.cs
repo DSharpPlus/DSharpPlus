6405c55ce353c6cd2253f6c8a4bea15eacd009cc
Mateusz Brawański <emzi0767@gmail.com>
2020-08-29 00:59:18 +0200

    Made logger event IDs public for comparison purposes.

--- DSharpPlus.CommandsNext/CommandsNextEvents.cs
+++ DSharpPlus.CommandsNext/CommandsNextEvents.cs
@@ -2,9 +2,19 @@
 
 namespace DSharpPlus.CommandsNext
 {
-    internal static class CommandsNextEvents
+    /// <summary>
+    /// Contains well-defined event IDs used by CommandsNext.
+    /// </summary>
+    public static class CommandsNextEvents
     {
+        /// <summary>
+        /// Miscellaneous events, that do not fit in any other category.
+        /// </summary>
         internal static EventId Misc { get; } = new EventId(200, "CommandsNext");
+
+        /// <summary>
+        /// Events pertaining to Gateway Intents. Typically diagnostic information.
+        /// </summary>
         internal static EventId Intents { get; } = new EventId(201, nameof(Intents));
     }
 }
