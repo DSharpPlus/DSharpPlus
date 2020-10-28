cf05bcbb0c9d16b077a2b49847551bde37a2ed1b
Neuheit <38368299+Neuheit@users.noreply.github.com>
2020-10-27 21:56:53 -0400

    Remove unused usings from test commands.

--- DSharpPlus.Test/TestBotCommands.cs
+++ DSharpPlus.Test/TestBotCommands.cs
@@ -1,6 +1,4 @@
 ﻿using System.Collections.Concurrent;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 using DSharpPlus.CommandsNext;
 using DSharpPlus.CommandsNext.Attributes;
