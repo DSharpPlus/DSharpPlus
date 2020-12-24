cf3e6f8064d70144d11e057b527ec34ce2c97d29
Neuheit <38368299+Neuheit@users.noreply.github.com>
2020-12-24 16:32:22 -0500

    feat: Add support for gateway interactions. tweak(core.entities): Move entities into similar folders for organization.

--- DSharpPlus/Clients/DiscordShardedClient.Events.cs
+++ DSharpPlus/Clients/DiscordShardedClient.Events.cs
@@ -500,6 +500,16 @@ namespace DSharpPlus
         #region Misc
 
         /// <summary>
+        /// Fired when a guild uses a slash command. 
+        /// </summary>
+        public event AsyncEventHandler<DiscordClient, InteractionCreateEventArgs> InteractionCreated
+        {
+            add => this._interactionCreated.Register(value);
+            remove => this._interactionCreated.Unregister(value);
+        }
+        private AsyncEvent<DiscordClient, InteractionCreateEventArgs> _interactionCreated;
+
+        /// <summary>
         /// Fired when a user starts typing in a channel.
         /// </summary>
         public event AsyncEventHandler<DiscordClient, TypingStartEventArgs> TypingStarted
@@ -664,6 +674,9 @@ namespace DSharpPlus
 
         private Task Client_MessageBulkDelete(DiscordClient client, MessageBulkDeleteEventArgs e)
             => this._messageBulkDeleted.InvokeAsync(client, e);
+
+        private Task Client_InteractionCreate(DiscordClient client, InteractionCreateEventArgs e)
+            => this._interactionCreated.InvokeAsync(client, e);
 
         private Task Client_TypingStart(DiscordClient client, TypingStartEventArgs e)
             => this._typingStarted.InvokeAsync(client, e);
---
+++ DSharpPlus/Entities/Interaction/DiscordInteractionDataOption.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace DSharpPlus.Entities
+{
+    /// <summary>
+    /// Represents parameters for interaction commands. 
+    /// </summary>
+    public class DiscordInteractionDataOption
+    {
+        /// <summary>
+        /// Gets the name of this interaction parameter.
+        /// </summary>
+        [JsonProperty("name")]
+        public string Name { get; internal set; }
+
+        /// <summary>
+        /// Gets the option type of this interaction parameter.
+        /// </summary>
+        [JsonProperty("value", NullValueHandling = NullValueHandling.Ignore)]
+        public ApplicationCommandOptionType OptionType { get; internal set; }
+
+        /// <summary>
+        /// Gets the additional parameters if this parameter is a subcommand.
+        /// </summary>
+        [JsonProperty("options", NullValueHandling = NullValueHandling.Ignore)]
+        public IEnumerable<DiscordInteractionDataOption> Options { get; internal set; }
+    }
+}
