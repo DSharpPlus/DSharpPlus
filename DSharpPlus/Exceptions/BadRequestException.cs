72dbf116eba174bd80d05177aba8784587a885d4
Neuheit <38368299+Neuheit@users.noreply.github.com>
2021-02-05 17:23:54 -0500

    feature: Support enhanced error details for bad requests.

--- DSharpPlus/Exceptions/BadRequestException.cs
+++ DSharpPlus/Exceptions/BadRequestException.cs
@@ -23,9 +23,19 @@ namespace DSharpPlus.Exceptions
         public RestResponse WebResponse { get; internal set; }
 
         /// <summary>
-        /// Gets the JSON received.
+        /// Gets the error code for this exception.
+        /// </summary>
+        public int Code { get; internal set; }
+
+        /// <summary>
+        /// Gets the JSON message received.
         /// </summary>
         public string JsonMessage { get; internal set; }
+
+        /// <summary>
+        /// Gets the form error responses in JSON format.
+        /// </summary>
+        public string Errors { get; internal set; }
 
         internal BadRequestException(BaseRestRequest request, RestResponse response) : base("Bad request: " + response.ResponseCode)
         {
@@ -36,10 +46,16 @@ namespace DSharpPlus.Exceptions
             {
                 var j = DiscordJson.LoadJObject(response.Response);
 
+                if (j["code"] != null)
+                    this.Code = (int)j["code"];
+
                 if (j["message"] != null)
-                    JsonMessage = j["message"].ToString();
+                    this.JsonMessage = j["message"].ToString();
+
+                if (j["errors"] != null)
+                    this.Errors = j["errors"].ToString();
             }
-            catch (Exception) { }
+            catch { }
         }
     }
 }
