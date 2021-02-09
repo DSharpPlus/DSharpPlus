1a29fb71953da152d5455720b42d117ca1e49587
Adi Mathur <origamibyadi@gmail.com>
2021-02-09 12:06:36 +0530

    Update XML docs

--- DSharpPlus/Enums/MembershipScreeningFieldType.cs
+++ DSharpPlus/Enums/MembershipScreeningFieldType.cs
@@ -4,9 +4,15 @@ using System.Runtime.Serialization;
 
 namespace DSharpPlus
 {
+    /// <summary>
+    /// Represents a membership screening field type
+    /// </summary>
     [JsonConverter(typeof(StringEnumConverter))]
     public enum MembershipScreeningFieldType
     {
+        /// <summary>
+        /// Specifies the server rules
+        /// </summary>
         [EnumMember(Value = "TERMS")]
         Terms
     }
