ee26afa7957ecf79cfcde99865fcbb4a64f93fa6
Neuheit <38368299+Neuheit@users.noreply.github.com>
2021-02-15 19:09:12 -0500

    feature: Support for the interaction create event.
    - Ignore other events Discord sends.
    - Also fix test bot with custom paginator.

---
+++ DSharpPlus/Entities/DiscordInteractionData.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace DSharpPlus.Entities
+{
+    /// <summary>
+    /// Represents the inner data payload of an interaction.
+    /// </summary>
+    public class DiscordInteractionData : SnowflakeObject
+    {
+        /// <summary>
+        /// Gets the name of the invoked interaction.
+        /// </summary>
+        [JsonProperty("name")]
+        public string Name { get; internal set; }
+
+        /// <summary>
+        /// Gets the parameters and values of the invoked interaction.
+        /// </summary>
+        [JsonProperty("options", NullValueHandling = NullValueHandling.Ignore)]
+        public IEnumerable<DiscordInteractionDataOption> Options { get; internal set; }
+    }
+}
