3844b9c100492079a328ffe38cbb094a1ef3f746
Neuheit <38368299+Neuheit@users.noreply.github.com>
2021-02-25 18:33:05 -0500

    fix: PageCount already exists.

--- DSharpPlus.Test/TestBotPaginator.cs
+++ DSharpPlus.Test/TestBotPaginator.cs
@@ -15,8 +15,6 @@ namespace DSharpPlus.Test
     /// </summary>
     public class TestBotPaginator : IPaginationRequest
     {
-        public int PageCount { get; }
-
         private List<Page> pages;
         private TaskCompletionSource<bool> _tcs;
         private CancellationTokenSource _cts;
@@ -31,7 +29,6 @@ namespace DSharpPlus.Test
         public TestBotPaginator(DiscordClient client, DiscordUser usr, DiscordMessage msg, List<Page> pages)
         {
             this.pages = pages;
-            this.PageCount = pages.Count;
             this._tcs = new TaskCompletionSource<bool>();
             this._cts = new CancellationTokenSource(TimeSpan.FromSeconds(30));
             this._cts.Token.Register(() => this._tcs.TrySetResult(true));
