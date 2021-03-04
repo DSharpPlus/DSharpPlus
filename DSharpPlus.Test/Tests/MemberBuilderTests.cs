d6918332536d9a553fb877b8983e18b7f56c8cf2
Jeffrey Ladd <jladd@Jeffreyladd.net>
2021-03-04 07:41:53 -0500

    Adds Tests for Member Modify

---
+++ DSharpPlus.Test/Tests/MemberBuilderTests.cs
@@ -0,0 +1,37 @@
+﻿using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+using DSharpPlus.Entities;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DSharpPlus.Test.Tests
+{
+    public class MemberBuilderTests : BaseCommandModule
+    {
+        [Command]
+        public async Task ModifyMember(CommandContext ctx, DiscordMember user)
+        {
+            await new DiscordMemberModifyBuilder()
+                .WithNickname("DummyTester")
+                .ModifyAysnc(user);
+        }
+
+        [Command]
+        public async Task ModifyMemberAction(CommandContext ctx, DiscordMember user)
+        {
+            var vcahnnel = ctx.Guild.Channels.Where(x => x.Value.Type == ChannelType.Voice).FirstOrDefault();
+            await user.ModifyAsync(m => {
+                m.WithVoiceChannel(vcahnnel.Value)
+                .WithDeafned(true)
+                .WithMute(true);
+            });
+
+            await Task.Delay(5000);
+
+            await user.ModifyAsync(m =>
+            {
+                m.WithVoiceChannel(null);
+            });
+        }
+    }
+}
