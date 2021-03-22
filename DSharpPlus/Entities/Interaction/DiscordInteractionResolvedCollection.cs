e6b490d4c3bb3727712b8134cac8dd305d4574dd
Neuheit <38368299+Neuheit@users.noreply.github.com>
2021-03-22 18:48:05 -0400

    fix: typos

--- DSharpPlus/Entities/Interaction/DiscordInteractionResolvedCollection.cs
+++ DSharpPlus/Entities/Interaction/DiscordInteractionResolvedCollection.cs
@@ -10,25 +10,25 @@ namespace DSharpPlus.Entities
     public sealed class DiscordInteractionResolvedCollection
     {
         /// <summary>
-        /// Gets thr resolved user objects, if any.
+        /// Gets the resolved user objects, if any.
         /// </summary>
         [JsonProperty("users", NullValueHandling = NullValueHandling.Ignore)]
         public IReadOnlyDictionary<ulong, DiscordUser> Users { get; internal set; }
 
         /// <summary>
-        /// Gets thr resolved member objects, if any.
+        /// Gets the resolved member objects, if any.
         /// </summary>
         [JsonProperty("members", NullValueHandling = NullValueHandling.Ignore)]
         public IReadOnlyDictionary<ulong, DiscordMember> Members { get; internal set; }
 
         /// <summary>
-        /// Gets thr resolved channel objects, if any.
+        /// Gets the resolved channel objects, if any.
         /// </summary>
         [JsonProperty("channels", NullValueHandling = NullValueHandling.Ignore)]
         public IReadOnlyDictionary<ulong, DiscordChannel> Channels { get; internal set; }
 
         /// <summary>
-        /// Gets thr resolved role objects, if any.
+        /// Gets the resolved role objects, if any.
         /// </summary>
         [JsonProperty("roles", NullValueHandling = NullValueHandling.Ignore)]
         public IReadOnlyDictionary<ulong, DiscordRole> Roles { get; internal set; }
