bae77ca3d98b6989a8f986cbab3b665e35697f28
Adi Mathur <origamibyadi@gmail.com>
2021-04-01 21:28:04 +0530

    Update WelcomeScreenEditModel.cs
    I forgot

--- DSharpPlus/Net/Models/WelcomeScreenEditModel.cs
+++ DSharpPlus/Net/Models/WelcomeScreenEditModel.cs
@@ -7,10 +7,19 @@ namespace DSharpPlus.Net.Models
 {
     public class WelcomeScreenEditModel
     {
+        /// <summary>
+        /// Sets whether the welcome screen should be enabled.
+        /// </summary>
         public Optional<bool> Enabled { internal get; set; }
 
+        /// <summary>
+        /// Sets the welcome channels.
+        /// </summary>
         public Optional<IEnumerable<DiscordGuildWelcomeScreenChannel>> WelcomeChannels { internal get; set; }
 
+        /// <summary>
+        /// Sets the serer description shown.
+        /// </summary>
         public Optional<string> Description { internal get; set; }
     }
 }
