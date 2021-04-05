d34127315b718384a2325a780ad9dc41c7f22347
Adi Mathur <65034167+IDoEverything@users.noreply.github.com>
2021-04-05 10:01:32 +0530

    Add limits on application commands name and description (#822)
    * Add limits on application commands name and description
    
    * nameof stuff

--- DSharpPlus/Net/Models/ApplicationCommandEditModel.cs
+++ DSharpPlus/Net/Models/ApplicationCommandEditModel.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using DSharpPlus.Entities;
 
 namespace DSharpPlus.Net.Models
@@ -8,12 +9,31 @@ namespace DSharpPlus.Net.Models
         /// <summary>
         /// Sets the command's new name.
         /// </summary>
-        public Optional<string> Name { internal get; set; }
+        public Optional<string> Name
+        {
+            internal get => this._name;
+            set
+            {
+                if (value.Value.Length > 32)
+                    throw new ArgumentException("Slash command name cannot exceed 32 characters.", nameof(value));
+                this._name = value;
+            }
+        }
+        private Optional<string> _name;
 
         /// <summary>
         /// Sets the command's new description
         /// </summary>
-        public Optional<string> Description { internal get; set; }
+        public Optional<string> Description
+        {   internal get => this._description;
+            set 
+            {
+                if(value.Value.Length > 100)
+                    throw new ArgumentException("Slash command description cannot exceed 100 characters.", nameof(value));
+                this._description = value;
+            }
+        }
+        private Optional<string> _description;
 
         /// <summary>
         /// Sets the command's new options.
