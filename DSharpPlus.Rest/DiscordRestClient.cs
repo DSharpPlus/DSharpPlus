c358c12fa570a9eb76361858ae183d027b27365a
Neuheit <38368299+Neuheit@users.noreply.github.com>
2021-04-05 00:33:58 -0400

    fix: Add action to rest client.

--- DSharpPlus.Rest/DiscordRestClient.cs
+++ DSharpPlus.Rest/DiscordRestClient.cs
@@ -454,6 +454,19 @@ namespace DSharpPlus
             => this.ApiClient.CreateMessageAsync(channel_id, builder);
 
         /// <summary>
+        /// Sends a message
+        /// </summary>
+        /// <param name="channel_id">Channel id</param>
+        /// <param name="action">The Discord Mesage builder.</param>
+        /// <returns></returns>
+        public Task<DiscordMessage> CreateMessageAsync(ulong channel_id, Action<DiscordMessageBuilder> action)
+        {
+            var builder = new DiscordMessageBuilder();
+            action(builder);
+            return this.ApiClient.CreateMessageAsync(channel_id, builder);
+        }
+
+        /// <summary>
         /// Gets channels from a guild
         /// </summary>
         /// <param name="guild_id">Guild id</param>
