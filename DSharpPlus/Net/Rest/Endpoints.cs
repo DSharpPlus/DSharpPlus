5f567254db0a9a8f3c7dc9fec2b78bcd8ca03431
Adi Mathur <65034167+IDoEverything@users.noreply.github.com>
2021-04-27 10:55:57 +0530

    Implement Stage Channels (#859)
    * Implement stage channels
    
    * fixes
    
    * Update DiscordVoiceState.cs

--- DSharpPlus/Net/Rest/Endpoints.cs
+++ DSharpPlus/Net/Rest/Endpoints.cs
@@ -55,5 +55,6 @@
         public const string INTERACTIONS = "/interactions";
         public const string CALLBACK = "/callback";
         public const string WELCOME_SCREEN = "/welcome-screen";
+        public const string VOICE_STATES = "/voice-states";
     }
 }
