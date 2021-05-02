7e2d7cbcb5b68fc566141ce0ed737197554a0bba
ZaptoInc <38966434+ZaptoInc@users.noreply.github.com>
2021-05-02 11:31:22 +0200

    Added Role Tags object

---
+++ DSharpPlus/Entities/Guild/DiscordRoleTags.cs
@@ -0,0 +1,41 @@
+﻿using Newtonsoft.Json;
+
+namespace DSharpPlus.Entities
+{
+    public class DiscordRoleTags
+    {
+        /// <summary>
+        /// Gets the id of the bot this role belongs to.
+        /// </summary>
+        [JsonProperty("bot_id", NullValueHandling = NullValueHandling.Ignore)]
+        public ulong? BotID { get; internal set; }
+
+        /// <summary>
+        /// Gets the id of the integration this role belongs to.
+        /// </summary>
+        [JsonProperty("integration_id", NullValueHandling = NullValueHandling.Ignore)]
+        public ulong? IntegrationID { get; internal set; }
+
+        /// <summary>
+        /// Gets whether this is the guild's premium subscriber role.
+        /// </summary>
+        [JsonIgnore]
+        public bool IsPremiumSubscriber
+        {
+            get
+            {
+                if(_premium_subscriber.HasValue)
+                {
+                    return _premium_subscriber.Value;
+                } else
+                {
+                    return true;
+                }
+            }
+        }
+
+        [JsonProperty("premium_subscriber", NullValueHandling = NullValueHandling.Include)]
+        internal bool? _premium_subscriber = false;
+
+    }
+}
