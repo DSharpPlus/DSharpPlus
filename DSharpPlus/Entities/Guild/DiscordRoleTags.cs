882e3899482a427a231496b7a8c0cafa3244399d
ZaptoInc <38966434+ZaptoInc@users.noreply.github.com>
2021-05-02 23:04:45 +0200

    Added Role Tags object (#869)
    * Added Role Tags object
    
    * Missing description.
    
    * Replaced ID to Id
    
    * Reduced the size of the code and did what Emzi asked
    
    * fixed bad code
    
    * missing tab

---
+++ DSharpPlus/Entities/Guild/DiscordRoleTags.cs
@@ -0,0 +1,33 @@
+﻿using Newtonsoft.Json;
+
+namespace DSharpPlus.Entities
+{
+    /// <summary>
+    /// Represents a discord role tags.
+    /// </summary>
+    public class DiscordRoleTags
+    {
+        /// <summary>
+        /// Gets the id of the bot this role belongs to.
+        /// </summary>
+        [JsonProperty("bot_id", NullValueHandling = NullValueHandling.Ignore)]
+        public ulong? BotId { get; internal set; }
+
+        /// <summary>
+        /// Gets the id of the integration this role belongs to.
+        /// </summary>
+        [JsonProperty("integration_id", NullValueHandling = NullValueHandling.Ignore)]
+        public ulong? IntegrationId { get; internal set; }
+
+        /// <summary>
+        /// Gets whether this is the guild's premium subscriber role.
+        /// </summary>
+        [JsonIgnore]
+        public bool IsPremiumSubscriber
+            => this._premiumSubscriber.HasValue && this._premiumSubscriber.Value is null;
+
+        [JsonProperty("premium_subscriber", NullValueHandling = NullValueHandling.Include)]
+        internal Optional<bool?> _premiumSubscriber = false;
+
+    }
+}
