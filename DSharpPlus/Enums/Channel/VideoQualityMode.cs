697ee017393f0ff846abb4d1cde3e23b17412864
John Marsden <johnswork15@gmail.com>
2021-05-31 16:53:05 -0400

    Remove any usage of default values for DiscordChannel#QualityMode

--- DSharpPlus/Enums/Channel/VideoQualityMode.cs
+++ DSharpPlus/Enums/Channel/VideoQualityMode.cs
@@ -33,11 +33,6 @@ namespace DSharpPlus
     public enum VideoQualityMode : int
     {
         /// <summary>
-        /// Indicates default video type, or channel is not a voice channel.
-        /// </summary>
-        DefaultValue = 0,
-
-        /// <summary>
         /// Indicates that the video quality is automatically chosen, or there is no value set.
         /// </summary>
         Auto = 1,
