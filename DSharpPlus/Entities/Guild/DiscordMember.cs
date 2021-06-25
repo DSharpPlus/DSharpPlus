1ee9ffda4966df532d5aaa9eeb7d7b76981f951a
Velvet <42438262+VelvetThePanda@users.noreply.github.com>
2021-06-25 01:13:00 -0400

    Update

--- DSharpPlus/Entities/Guild/DiscordMember.cs
+++ DSharpPlus/Entities/Guild/DiscordMember.cs
@@ -177,8 +177,9 @@ namespace DSharpPlus.Entities
         /// Gets the permissions for the current member.
         /// </summary>
         [JsonIgnore]
-        public Permissions Permissions
-            => this.Roles.Aggregate(Permissions.None, (perms, role) => perms |= role.Permissions);
+        public Permissions Permissions => this.GetPermissions();
+
+
 
         #region Overridden user properties
         [JsonIgnore]
@@ -601,5 +602,29 @@ namespace DSharpPlus.Entities
         /// <returns>Whether the two members are not equal.</returns>
         public static bool operator !=(DiscordMember e1, DiscordMember e2)
             => !(e1 == e2);
+
+        /// <summary>
+        /// Get's the current member's roles based on the sum of the permissions of their given roles.
+        /// </summary>
+        private Permissions GetPermissions()
+        {
+            if (this.Guild.OwnerId == this.Id)
+                return PermissionMethods.FULL_PERMS;
+
+            Permissions perms;
+
+            // assign @everyone permissions
+            var everyoneRole = this.Guild.EveryoneRole;
+            perms = everyoneRole.Permissions;
+
+            // assign permissions from member's roles (in order)
+            perms |= this.Roles.Aggregate(Permissions.None, (c, role) => c | role.Permissions);
+
+            // Adminstrator grants all permissions and cannot be overridden
+            if ((perms & Permissions.Administrator) == Permissions.Administrator)
+                return PermissionMethods.FULL_PERMS;
+
+            return perms;
+        }
     }
 }
