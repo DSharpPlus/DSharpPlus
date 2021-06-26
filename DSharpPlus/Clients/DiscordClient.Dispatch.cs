8d73513f831b64aeb163a889d1fe6ca3b7c4319f
Neuheit <38368299+Neuheit@users.noreply.github.com>
2021-06-26 16:33:10 -0400

    fix: log warning instead of throwing exception if role cannot be found.
    fixes #948

--- DSharpPlus/Clients/DiscordClient.Dispatch.cs
+++ DSharpPlus/Clients/DiscordClient.Dispatch.cs
@@ -1197,7 +1197,7 @@ namespace DSharpPlus
         internal async Task OnGuildRoleDeleteEventAsync(ulong roleId, DiscordGuild guild)
         {
             if (!guild._roles.TryRemove(roleId, out var role))
-                throw new InvalidOperationException("Attempted to delete a nonexistent role.");
+                this.Logger.LogWarning($"Attempted to delete a nonexistent role ({roleId}) from guild ({guild}).");
 
             var ea = new GuildRoleDeleteEventArgs
             {
