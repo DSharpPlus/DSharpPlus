c0223ce708e5dc03b0297df969e5b0e067a94582
Adi Mathur <65034167+IDoEverything@users.noreply.github.com>
2021-07-22 02:14:41 +0530

    Add ignoreDms for require permission attributes (#1001)

--- DSharpPlus.CommandsNext/Attributes/RequireBotPermissionsAttribute.cs
+++ DSharpPlus.CommandsNext/Attributes/RequireBotPermissionsAttribute.cs
@@ -46,9 +46,11 @@ namespace DSharpPlus.CommandsNext.Attributes
         /// Defines that usage of this command is only possible when the bot is granted a specific permission.
         /// </summary>
         /// <param name="permissions">Permissions required to execute this command.</param>
-        public RequireBotPermissionsAttribute(Permissions permissions)
+        /// <param name="ignoreDms">Sets this check's behaviour in DMs. True means the check will always pass in DMs, whereas false means that it will always fail.</param>
+        public RequireBotPermissionsAttribute(Permissions permissions, bool ignoreDms = true)
         {
             this.Permissions = permissions;
+            this.IgnoreDms = ignoreDms;
         }
 
         public override async Task<bool> ExecuteCheckAsync(CommandContext ctx, bool help)
