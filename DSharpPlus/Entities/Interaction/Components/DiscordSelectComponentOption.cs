9fa8062f8864cd9599a0a9424bcf111c217da54a
Velvet <42438262+VelvetThePanda@users.noreply.github.com>
2021-08-09 23:40:25 -0400

    You saw nothing

--- DSharpPlus/Entities/Interaction/Components/DiscordSelectComponentOption.cs
+++ DSharpPlus/Entities/Interaction/Components/DiscordSelectComponentOption.cs
@@ -67,25 +67,6 @@ namespace DSharpPlus.Entities
             this.Description = description;
             this.Default = isDefault;
             this.Emoji = emoji;
-
-
-
-            // Create the options for the user to pick
-            var options = new List<DiscordSelectComponentOption>()
-            {
-                new DiscordSelectComponentOption("Label, no description", "label_no_desc"),
-                new DiscordSelectComponentOption("Label, Description", "label_with_desc", "This is a description!"),
-                new DiscordSelectComponentOption("Label, Description, Emoji", "label_with_desc_emoji", "This is a description!", emoji: new DiscordComponentEmoji(854260064906117121)),
-                new DiscordSelectComponentOption("Label, Description, Emoji (Default selection)", "label_with_desc_emoji_default", "This is a description!", isDefault: true, new DiscordComponentEmoji(854260064906117121))
-            };
-
-// Make the dropdown
-            var dropdown = new DiscordSelectComponent("dropdown", null, options, false, 1, 2);
-
-// [...] Code trunctated for brevity
-
-            var builder = new DiscordMessageBuilder().WithContent("Look, it's a dropdown!").AddComponents(dropdown);
-
         }
     }
 }
