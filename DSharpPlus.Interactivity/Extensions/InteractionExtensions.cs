fa0851f647c0efda39eecffe3fa768aa7ae7c215
Velvet <42438262+VelvetThePanda@users.noreply.github.com>
2021-08-25 08:44:51 -0400

    Add documentation because yes

--- DSharpPlus.Interactivity/Extensions/InteractionExtensions.cs
+++ DSharpPlus.Interactivity/Extensions/InteractionExtensions.cs
@@ -31,6 +31,20 @@ namespace DSharpPlus.Interactivity.Extensions
 {
     public static class InteractionExtensions
     {
+        /// <summary>
+        /// Sends a paginated message in response to an interaction.
+        /// <para>
+        /// <b>Pass the interaction directly. Interactivity will ACK it.</b>
+        /// </para>
+        /// </summary>
+        /// <param name="interaction">The interaction to create a response to.</param>
+        /// <param name="ephemeral">Whether the response should be ephemeral.</param>
+        /// <param name="user">The user to listen for button presses from.</param>
+        /// <param name="pages">The pages to paginate.</param>
+        /// <param name="buttons">Optional: custom buttons</param>
+        /// <param name="behaviour">Pagination behaviour.</param>
+        /// <param name="deletion">Deletion behaviour</param>
+        /// <param name="token">A custom cancellation token that can be cancelled at any point.</param>
         public static Task SendPaginatedResponseAsync(this DiscordInteraction interaction, bool ephemeral, DiscordUser user, IEnumerable<Page> pages, PaginationButtons? buttons = null, PaginationBehaviour? behaviour = default, ButtonPaginationBehavior? deletion = default, CancellationToken token = default)
             => MessageExtensions.GetInteractivity(interaction.Message).SendPaginatedResponseAsync(interaction, ephemeral, user, pages, buttons, behaviour, deletion, token);
     }
