571b630e84c3581a26ae97d56936cbb0ce0be5eb
Syed Rizvi <srizvi1112@gmail.com>
2021-09-19 19:32:27 -0500

    Add .this qualifiers in ThreadChannelMember because @VelvetThePanda

--- DSharpPlus/Entities/Channel/Thread/DiscordThreadChannelMember.cs
+++ DSharpPlus/Entities/Channel/Thread/DiscordThreadChannelMember.cs
@@ -68,7 +68,7 @@ namespace DSharpPlus.Entities
         /// </summary>
         [JsonIgnore]
         public DiscordMember Member
-            => this.Guild != null ? (this.Guild._members.TryGetValue(this.Id, out var member) ? member : new DiscordMember { Id = Id, _guild_id = _guild_id, Discord = Discord}) : null;
+            => this.Guild != null ? (this.Guild._members.TryGetValue(this.Id, out var member) ? member : new DiscordMember { Id = this.Id, _guild_id = this._guild_id, Discord = this.Discord}) : null;
 
         /// <summary>
         /// Gets the category that contains this channel. For threads, gets the channel this thread was created in.
