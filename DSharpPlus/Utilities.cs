d8f58c68c799d986da8da3c456ee5b98d51762fc
Velvet <42438262+VelvetThePanda@users.noreply.github.com>
2021-10-28 23:04:11 -0400

    Add utlity method to replace duped code

--- DSharpPlus/Utilities.cs
+++ DSharpPlus/Utilities.cs
@@ -192,6 +192,21 @@ namespace DSharpPlus
         internal static bool HasTypingIntents(DiscordIntents intents)
             => intents.HasIntent(DiscordIntents.GuildMessageTyping) || intents.HasIntent(DiscordIntents.DirectMessageTyping);
 
+        internal static bool CanSendMessage(DiscordChannel channel)
+            => channel.Type switch
+            {
+                ChannelType.Text => true,
+                ChannelType.Voice => true,
+                ChannelType.Group => true,
+                ChannelType.Private => true,
+                ChannelType.PublicThread => true,
+                ChannelType.PrivateThread => true,
+                ChannelType.NewsThread => true,
+                ChannelType.News => true,
+                _ => false,
+            };
+
+
         // https://discord.com/developers/docs/topics/gateway#sharding-sharding-formula
         /// <summary>
         /// Gets a shard id from a guild id and total shard count.
