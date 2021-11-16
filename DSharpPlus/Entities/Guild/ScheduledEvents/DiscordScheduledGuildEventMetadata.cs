5680bda71e2195b1c214153c0167e0b28d8b686f
Velvet <42438262+VelvetThePanda@users.noreply.github.com>
2021-11-16 18:10:45 -0500

    Documentation

--- DSharpPlus/Entities/Guild/ScheduledEvents/DiscordScheduledGuildEventMetadata.cs
+++ DSharpPlus/Entities/Guild/ScheduledEvents/DiscordScheduledGuildEventMetadata.cs
@@ -25,9 +25,11 @@ using Newtonsoft.Json;
 
 namespace DSharpPlus.Entities
 {
+    /// <summary>
+    /// Metadata for a <see cref="DiscordScheduledGuildEvent"/>.
+    /// </summary>
     public sealed class DiscordScheduledGuildEventMetadata
     {
-
         /// <summary>
         /// If this is an external event, where this event is hosted.
         /// </summary>
