df52ee549512fb30043d4b566c2074583256aa79
Velvet <42438262+VelvetThePanda@users.noreply.github.com>
2022-01-21 18:43:42 -0500

    Fix, I think

--- DSharpPlus/Net/Serialization/ISO8601DateTimeOffsetJsonConverter.cs
+++ DSharpPlus/Net/Serialization/ISO8601DateTimeOffsetJsonConverter.cs
@@ -23,24 +23,23 @@
 using System;
 using System.Globalization;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace DSharpPlus.Net.Serialization
 {
     /// <summary>
     /// Json converter for handling DateTimeOffset values.
     /// </summary>
-    internal sealed class ISO8601DateTimeOffsetJsonConverter : JsonConverter<DateTimeOffset>
+    internal sealed class ISO8601DateTimeOffsetJsonConverter : JsonConverter
     {
-        public override void WriteJson(JsonWriter writer, DateTimeOffset value, JsonSerializer serializer)
+        public override void WriteJson(JsonWriter writer, object? value, JsonSerializer serializer)
+            => writer.WriteValue(((DateTimeOffset)value).ToString("O", CultureInfo.InvariantCulture));
+        public override object? ReadJson(JsonReader reader, Type objectType, object? existingValue, JsonSerializer serializer)
         {
-            writer.WriteValue(value.ToString("o", CultureInfo.InvariantCulture));
-        }
+            var jr = JToken.Load(reader);
 
-        public override DateTimeOffset ReadJson(JsonReader reader, Type objectType, DateTimeOffset existingValue, bool hasExistingValue, JsonSerializer serializer)
-        {
-            var value = reader.ReadAsString();
-
-            return DateTimeOffset.TryParse("o", CultureInfo.InvariantCulture, DateTimeStyles.None, out var dto) ? dto : default;
+            return jr.ToObject<DateTimeOffset>();
         }
+        public override bool CanConvert(Type objectType) => objectType == typeof(DateTimeOffset);
     }
 }
