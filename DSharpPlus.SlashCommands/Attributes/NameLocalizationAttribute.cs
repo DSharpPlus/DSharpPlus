41d73d5f273f13af386932c3e3a45de3f6dfcd18
Velvet <42438262+VelvetThePanda@users.noreply.github.com>
2022-02-23 13:57:18 -0500

    Name localizations are also valid

--- DSharpPlus.SlashCommands/Attributes/NameLocalizationAttribute.cs
+++ DSharpPlus.SlashCommands/Attributes/NameLocalizationAttribute.cs
@@ -29,7 +29,7 @@ namespace DSharpPlus.SlashCommands
     /// <summary>
     /// Specifies a locale for a slash command name. The longest name is the name that counts toward character limits.
     /// </summary>
-    [AttributeUsage(AttributeTargets.Method | AttributeTargets.Class, AllowMultiple = true)]
+    [AttributeUsage(AttributeTargets.Method | AttributeTargets.Class | AttributeTargets.Parameter, AllowMultiple = true)]
     public sealed class NameLocalizationAttribute : Attribute
     {
         public string Locale { get; }
