04a9f812e15e75fe608157ef7e6f3142a204e9c1
Velvet <42438262+VelvetThePanda@users.noreply.github.com>
2022-02-23 03:15:09 -0500

    Localizations are valid for parameters

--- DSharpPlus.SlashCommands/Attributes/DescriptionLocalizationAttribute.cs
+++ DSharpPlus.SlashCommands/Attributes/DescriptionLocalizationAttribute.cs
@@ -29,7 +29,7 @@ namespace DSharpPlus.SlashCommands
     /// <summary>
     /// Specifies a locale for a slash command description. The longest description is the one that counts toward character limits.
     /// </summary>
-    [AttributeUsage(AttributeTargets.Method | AttributeTargets.Class, AllowMultiple = true)]
+    [AttributeUsage(AttributeTargets.Method | AttributeTargets.Class | AttributeTargets.Parameter, AllowMultiple = true)]
     public sealed class DescriptionLocalizationAttribute : Attribute
     {
         public string Locale { get; }
