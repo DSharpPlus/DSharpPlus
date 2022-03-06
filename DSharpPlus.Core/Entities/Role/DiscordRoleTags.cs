a64f06cefd625730642b85fd66bf5a1d33ba15e4
Lunar Starstrum <lunar@forsaken-borders.net>
2022-03-05 23:44:10 -0600

    Added Guild Member and Role

---
+++ DSharpPlus.Core/Entities/Role/DiscordRoleTags.cs
@@ -0,0 +1,58 @@
+// This file is part of the DSharpPlus project.
+//
+// Copyright (c) 2015 Mike Santiago
+// Copyright (c) 2016-2022 DSharpPlus Contributors
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+using System;
+using System.Text.Json.Serialization;
+using DSharpPlus.Core.JsonConverters;
+
+namespace DSharpPlus.Core.Entities
+{
+    /// <summary>
+    /// A <see cref="DiscordRole"/>'s metadata.
+    /// </summary>
+    /// <value></value>
+    public sealed record DiscordRoleTags
+    {
+        /// <summary>
+        /// The id of the bot this role belongs to.
+        /// </summary>
+        [JsonPropertyName("bot_id")]
+        public DiscordSnowflake? BotId { get; init; }
+
+        /// <summary>
+        /// The id of the integration this role belongs to.
+        /// </summary>
+        [JsonPropertyName("integration_id")]
+        public DiscordSnowflake? IntegrationId { get; init; }
+
+        /// <summary>
+        /// Whether this is the guild's premium subscriber role.
+        /// </summary>
+        [JsonPropertyName("premium_subscriber"), JsonConverter(typeof(NullBoolConverter))]
+        public bool? IsNitroRole { get; init; } = false;
+
+        internal DiscordRoleTags() { }
+
+        public override int GetHashCode() => HashCode.Combine(BotId, IntegrationId, IsNitroRole);
+    }
+}
