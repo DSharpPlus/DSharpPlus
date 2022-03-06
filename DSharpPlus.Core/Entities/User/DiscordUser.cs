45b1b19223c481af58662e0c26c4bf27d8e8f89f
Lunar Starstrum <lunar@forsaken-borders.net>
2022-03-05 22:39:06 -0600

    Restart V5 to remove bells and whistles, this means all we have now are records which directly reflects the documentated API; Remove generated properties

---
+++ DSharpPlus.Core/Entities/User/DiscordUser.cs
@@ -0,0 +1,153 @@
+// This file is part of the DSharpPlus project.
+//
+// Copyright (c) 2015 Mike Santiago
+// Copyright (c) 2016-2022 DSharpPlus Contributors
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+using System;
+using System.Drawing;
+using System.Text.Json.Serialization;
+using DSharpPlus.Core.Enums;
+
+namespace DSharpPlus.Core.Entities
+{
+    /// <summary>
+    /// Implements a <see href="https://discord.com/developers/docs/resources/user#user-object-user-structure">Discord User</see>.
+    /// </summary>
+    public sealed record DiscordUser
+    {
+        /// <summary>
+        /// The user's id, used to identify the user across all of Discord.
+        /// </summary>
+        [JsonPropertyName("id")]
+        public DiscordSnowflake Id { get; init; } = null!;
+
+        /// <summary>
+        /// The user's username, not unique across the platform.
+        /// </summary>
+        [JsonPropertyName("username")]
+        public string Username { get; init; } = null!;
+
+        /// <summary>
+        /// The user's 4-digit discord-tag.
+        /// </summary>
+        [JsonPropertyName("discriminator")]
+        public string Discriminator { get; init; } = null!;
+
+        /// <summary>
+        /// The user's avatar hash.
+        /// </summary>
+        [JsonPropertyName("avatar")]
+        public string? AvatarHash { get; private set; }
+
+        /// <summary>
+        /// Whether the user belongs to an OAuth2 application.
+        /// </summary>
+        [JsonPropertyName("bot")]
+        public bool? IsBot { get; init; }
+
+        /// <summary>
+        /// Whether the user is an Official Discord System user (part of the urgent message system).
+        /// </summary>
+        [JsonPropertyName("system")]
+        public bool? IsSystem { get; init; }
+
+        /// <summary>
+        /// Whether the user has two factor enabled on their account.
+        /// </summary>
+        [JsonPropertyName("mfa_enabled")]
+        public bool? MFAEnabled { get; private set; }
+
+        /// <summary>
+        /// The user's banner hash.
+        /// </summary>
+        [JsonPropertyName("banner")]
+        public string? BannerHash { get; private set; }
+
+        /// <summary>
+        /// The user's banner color.
+        /// </summary>
+        [JsonPropertyName("accent_color")]
+        public Color? AccentColor { get; private set; }
+
+        // TODO: Can probably be serialized to a class or dictionary. See https://discord.com/developers/docs/reference#locales
+        /// <summary>
+        /// The user's chosen language option.
+        /// </summary>
+        [JsonPropertyName("locale")]
+        public string? Locale { get; private set; }
+
+        /// <summary>
+        /// Whether the email on this account has been verified. Requires the email oauth2 scope.
+        /// </summary>
+        [JsonPropertyName("verified")]
+        public bool? Verified { get; private set; }
+
+        /// <summary>
+        /// The user's email. Requires the email oauth2 scope.
+        /// </summary>
+        [JsonPropertyName("email")]
+        public string? Email { get; private set; }
+
+        /// <summary>
+        /// The user flags on a user's account.
+        /// </summary>
+        [JsonPropertyName("flags")]
+        public DiscordUserFlags? UserFlags { get; private set; }
+
+
+        /// <summary>
+        /// The type of Nitro subscription on a user's account.
+        /// </summary>
+        [JsonPropertyName("premium_type")]
+        public DiscordPremiumType? PremiumType { get; private set; }
+
+        /// <summary>
+        /// The public flags on a user's account.
+        /// </summary>
+        [JsonPropertyName("public_flags")]
+        public DiscordUserFlags? PublicUserFlags { get; private set; }
+
+        internal DiscordUser() { }
+
+        public override int GetHashCode()
+        {
+            var hash = new HashCode();
+            hash.Add(Id);
+            hash.Add(Username);
+            hash.Add(Discriminator);
+            hash.Add(AvatarHash);
+            hash.Add(IsBot);
+            hash.Add(IsSystem);
+            hash.Add(MFAEnabled);
+            hash.Add(BannerHash);
+            hash.Add(AccentColor);
+            hash.Add(Locale);
+            hash.Add(Verified);
+            hash.Add(Email);
+            hash.Add(UserFlags);
+            hash.Add(PremiumType);
+            hash.Add(PublicUserFlags);
+            return hash.ToHashCode();
+        }
+
+        public static implicit operator ulong(DiscordUser user) => user.Id;
+    }
+}
---
+++ DSharpPlus.Core/Enums/User/DiscordUserFlags.cs
@@ -0,0 +1,107 @@
+// This file is part of the DSharpPlus project.
+//
+// Copyright (c) 2015 Mike Santiago
+// Copyright (c) 2016-2022 DSharpPlus Contributors
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+using System;
+
+namespace DSharpPlus.Core.Enums
+{
+
+    [Flags]
+    public enum DiscordUserFlags
+    {
+        /// <summary>
+        /// The user has no flags.
+        /// </summary>
+        None = 0,
+
+        /// <summary>
+        /// The user is a Discord Employee.
+        /// </summary>
+        Staff = 1 << 0,
+
+        /// <summary>
+        /// The user is a Partnered Server Owner.
+        /// </summary>
+        Partner = 1 << 1,
+
+        /// <summary>
+        /// The user is a HypeSquad Events Coordinator.
+        /// </summary>
+        Hypesquad = 1 << 2,
+
+        /// <summary>
+        /// The user is a Level 1 Bug Hunter.
+        /// </summary>
+        BugHunterLevel1 = 1 << 3,
+
+        /// <summary>
+        /// The user is part of the Hypesquad House of Bravery.
+        /// </summary>
+        HypesquadBravery = 1 << 6,
+
+        /// <summary>
+        /// The user is part of the Hypesquad House of Brilliance.
+        /// </summary>
+        HypesquadBrilliance = 1 << 7,
+
+        /// <summary>
+        /// The user is part of the Hypesquad House of Balance.
+        /// </summary>
+        HypesquadBalance = 1 << 8,
+
+        /// <summary>
+        /// The user is Early Nitro Supporter.
+        /// </summary>
+        NitroEarlySupporter = 1 << 9,
+
+        /// <summary>
+        /// The user is a team.
+        /// </summary>
+        TeamPseudoUser = 1 << 10,
+
+        /// <summary>
+        /// The user is a Level 2 Bug Hunter.
+        /// </summary>
+        BugHunterLevel2 = 1 << 14,
+
+        /// <summary>
+        /// The user is a Verified Bot.
+        /// </summary>
+        VerifiedBot = 1 << 16,
+
+        /// <summary>
+        /// The user is a Verified Bot Developer.
+        /// </summary>
+        VerifiedDeveloper = 1 << 17,
+
+        /// <summary>
+        /// The user is a Discord Certified Moderator.
+        /// </summary>
+        CertifiedModerator = 1 << 18,
+
+        /// <summary>
+        /// The user is a bot that uses only HTTP interactions and is shown in the online member list.
+        /// </summary>
+        BotHttpInteractions = 1 << 19,
+    }
+}
