328b79eb24ea7fd0ddd3de651c2a2d80e0cf90ef
Lunar Starstrum <lunar@forsaken-borders.net>
2022-03-12 19:15:44 -0600

    Add Newtonsoft.Json; Convert JsonConverters to Newtonsoft.Json over STJ due to STJ's inability to exclude properties (see https://github.com/dotnet/runtime/issues/36275); Create Optional<T>

--- DSharpPlus.Core/JsonConverters/OptionalConverter.cs
+++ DSharpPlus.Core/JsonConverters/OptionalConverter.cs
@@ -22,21 +22,39 @@
 // SOFTWARE.
 
 using System;
+using System.Linq;
+using System.Reflection;
 using DSharpPlus.Core.Entities;
 using Newtonsoft.Json;
 
 namespace DSharpPlus.Core.JsonConverters
 {
-    internal class DiscordSnowflakeConverter : JsonConverter<DiscordSnowflake>
+    public class OptionalConverter : JsonConverter
     {
-        public override DiscordSnowflake? ReadJson(JsonReader reader, Type objectType, DiscordSnowflake? existingValue, bool hasExistingValue, JsonSerializer serializer)
+        public override bool CanConvert(Type objectType) => typeof(Optional<>).IsAssignableFrom(objectType);
+        public override object? ReadJson(JsonReader reader, Type objectType, object? existingValue, JsonSerializer serializer)
         {
-            if (reader.TokenType == JsonToken.Null)
-                return null;
-
-            return ulong.TryParse(reader.Value!.ToString(), out var snowflake) ? new DiscordSnowflake(snowflake) : null;
+            var genericType = objectType.GenericTypeArguments[0];
+            var constructor = objectType.GetTypeInfo().DeclaredConstructors.FirstOrDefault(e => e.GetParameters()[0].ParameterType == genericType);
+            return constructor!.Invoke(new[] { serializer.Deserialize(reader, genericType) });
         }
 
-        public override void WriteJson(JsonWriter writer, DiscordSnowflake? value, JsonSerializer serializer) => writer.WriteValue(value?.ToString());
+        public override void WriteJson(JsonWriter writer, object? value, JsonSerializer serializer)
+        {
+            if (value == null)
+            {
+                writer.WriteNull();
+            }
+
+            var optional = (Optional<object>)value!;
+            if (!optional.HasValue)
+            {
+                writer.WriteNull();
+            }
+            else
+            {
+                serializer.Serialize(writer, optional.Value);
+            }
+        }
     }
 }
