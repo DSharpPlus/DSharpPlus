0dab172a9aae67d305cac53b7ea43cec5292b3fe
aPinat <apinat@pinat.dev>
2022-04-08 16:16:06 +0200

    Fixed incorrect nullability for Command in CommandEventArgs
    introduced in #1246

--- DSharpPlus.CommandsNext/EventArgs/CommandExecutionEventArgs.cs
+++ DSharpPlus.CommandsNext/EventArgs/CommandExecutionEventArgs.cs
@@ -28,5 +28,10 @@ namespace DSharpPlus.CommandsNext
     /// </summary>
     public class CommandExecutionEventArgs : CommandEventArgs
     {
+        /// <summary>
+        /// Gets the command that was executed.
+        /// </summary>
+        public new Command Command
+            => this.Context.Command!;
     }
 }
