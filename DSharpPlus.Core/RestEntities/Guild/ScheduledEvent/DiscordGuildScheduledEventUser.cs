acdf89d536fefd9f1e405c644da89b599426086e
Lunar Starstrum <lunar@forsaken-borders.net>
2022-06-15 13:20:32 -0500

    Update GuildScheduledEvents to DAPI commit 4fa7db8828707129a23374cc5fd83f5fd69f618a

---
+++ DSharpPlus.Core/RestEntities/Guild/ScheduledEvent/DiscordGuildScheduledEventUser.cs
@@ -0,0 +1,25 @@
+using Newtonsoft.Json;
+
+namespace DSharpPlus.Core.RestEntities
+{
+    public sealed record DiscordGuildScheduledEventUser
+    {
+        /// <summary>
+        /// The scheduled event id which the user subscribed to.
+        /// </summary>
+        [JsonProperty("guild_scheduled_event_id", NullValueHandling = NullValueHandling.Ignore)]
+        public DiscordSnowflake Id { get; init; } = null!;
+
+        /// <summary>
+        /// The user which subscribed to an event.
+        /// </summary>
+        [JsonProperty("user", NullValueHandling = NullValueHandling.Ignore)]
+        public DiscordUser User { get; init; } = null!;
+
+        /// <summary>
+        /// The guild member data for this user for the guild which this event belongs to, if any.
+        /// </summary>
+        [JsonProperty("member", NullValueHandling = NullValueHandling.Ignore)]
+        public Optional<DiscordGuildMember> Member { get; init; }
+    }
+}
