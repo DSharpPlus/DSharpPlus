950bdd3112a91bc2fb36a65094ce373e6c3def57
Lunar Starstrum <lunar@forsaken-borders.net>
2022-06-15 14:03:32 -0500

    Add missing DiscordUser docs and types to match DAPI commit 4fa7db8828707129a23374cc5fd83f5fd69f618a

---
+++ DSharpPlus.Core/Enums/User/DiscordUserConnectionVisibilityType.cs
@@ -0,0 +1,15 @@
+namespace DSharpPlus.Core.Enums
+{
+    public enum DiscordUserConnectionVisibilityType
+    {
+        /// <summary>
+        /// Invisible to everyone except the user themselves.
+        /// </summary>
+        None = 0,
+
+        /// <summary>
+        /// Visible to everyone.
+        /// </summary>
+        Everyone = 1
+    }
+}
