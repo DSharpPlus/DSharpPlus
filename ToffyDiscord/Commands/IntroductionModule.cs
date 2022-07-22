c05e77ecde484486a53f2076918a86c6b77390dd
RITE-X <re.ritex@gmail.com>
2022-07-22 21:57:34 +0300

    feat: add introduction module

---
+++ ToffyDiscord/Commands/IntroductionModule.cs
@@ -0,0 +1,14 @@
+﻿using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+
+namespace ToffyDiscord.Commands;
+
+
+public class IntroductionModule : BaseCommandModule
+{
+    [Command("greet")]
+    public async Task GreetCommandAsync(CommandContext ctx)
+    {
+        await ctx.RespondAsync("Greetings! Thank you for executing me!");
+    }
+}
--- ToffyDiscord/Program.cs
+++ ToffyDiscord/Program.cs
@@ -1,3 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using DSharpPlus;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.EventArgs;
+using ToffyDiscord.Commands;
 
-Console.WriteLine("Hello, World!");
+namespace ToffyDiscord
+{
+    class Program
+    {
+        static void Main(string[] args) => MainAsync().GetAwaiter().GetResult();
+
+        static async Task MainAsync()
+        {
+            var discord = new DiscordClient(new DiscordConfiguration()
+            {
+                Token = "MTAwMDA1MjA0MzY1NTk1NDU4Mg.GhNYxe.yck7Icxj5rphOpCl3xNVuejP6xjrxP8E-S7wTY",
+                TokenType = TokenType.Bot
+            });
+
+            discord.MessageCreated += async (s, e) =>
+            {
+                if (!e.Author.IsBot)
+                    if (e.Message.Content.ToLower().StartsWith("hello"))
+                        await e.Message.RespondAsync($"Hello {e.Message.Author.Username}!");
+            };
+            var commands = discord.UseCommandsNext(new CommandsNextConfiguration {StringPrefixes = new[] {"!"}});
+            commands.RegisterCommands<IntroductionModule>();
+
+            discord.GuildMemberAdded += DiscordOnGuildMemberAdded;
+
+            await discord.ConnectAsync();
+            await Task.Delay(-1);
+        }
+
+        private static Task DiscordOnGuildMemberAdded(DiscordClient sender, GuildMemberAddEventArgs e)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
