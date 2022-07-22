b954fab53ff8df94c83f71f19ec69bf32126e804
RITE-X <re.ritex@gmail.com>
2022-07-22 23:18:44 +0300

    feat: add command handler  add auto handler for added commands

--- ToffyDiscord/Program.cs
+++ ToffyDiscord/Program.cs
@@ -1,6 +1,8 @@
 ﻿using DSharpPlus;
 using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Converters;
 using DSharpPlus.EventArgs;
+using DSharpPlus.Lavalink;
 using ToffyDiscord.Commands;
 
 namespace ToffyDiscord
@@ -13,27 +15,54 @@ namespace ToffyDiscord
         {
             var discord = new DiscordClient(new DiscordConfiguration()
             {
-                Token = "MTAwMDA1MjA0MzY1NTk1NDU4Mg.GhNYxe.yck7Icxj5rphOpCl3xNVuejP6xjrxP8E-S7wTY",
+                Token = "MTAwMDA1MjA0MzY1NTk1NDU4Mg.GlmBjS.fxKaSKKyEpfurLN8TVry6WtFEn4XeHfr5Y6ATc",
                 TokenType = TokenType.Bot
             });
 
-            discord.MessageCreated += async (s, e) =>
+            var commands = discord.UseCommandsNext(new CommandsNextConfiguration()
             {
-                if (!e.Author.IsBot)
-                    if (e.Message.Content.ToLower().StartsWith("hello"))
-                        await e.Message.RespondAsync($"Hello {e.Message.Author.Username}!");
-            };
-            var commands = discord.UseCommandsNext(new CommandsNextConfiguration {StringPrefixes = new[] {"!"}});
+                UseDefaultCommandHandler = false
+            });
+
+            var lavalink = discord.UseLavalink();
+
+
+
             commands.RegisterCommands<IntroductionModule>();
+            commands.RegisterCommands<MusicModule>();
+            commands.SetHelpFormatter<DefaultHelpFormatter>();
+
+            discord.MessageCreated += CommandHandler;
 
             discord.GuildMemberAdded += DiscordOnGuildMemberAdded;
 
             await discord.ConnectAsync();
+            await lavalink.ConnectAsync(new LavalinkConfiguration{ }); // Make sure this is after Discord.ConnectAsync().
             await Task.Delay(-1);
         }
 
         private static Task DiscordOnGuildMemberAdded(DiscordClient sender, GuildMemberAddEventArgs e)
         {
+            return Task.CompletedTask;
+        }
+
+        private static Task CommandHandler(DiscordClient client, MessageCreateEventArgs e)
+        {
+            var nextCommand = client.GetCommandsNext();
+            var msg = e.Message;
+
+            var cmdStart = msg.GetStringPrefixLength("!");
+            if (cmdStart == -1) return Task.CompletedTask;
+
+            var prefix = msg.Content[..cmdStart];
+            var cmdString = msg.Content[cmdStart..];
+
+            var command = nextCommand.FindCommand(cmdString, out var args);
+            if (command == null) return Task.CompletedTask;
+
+            var ctx = nextCommand.CreateContext(msg, prefix, command, args);
+            Task.Run(async () => await nextCommand.ExecuteCommandAsync(ctx));
+
             return Task.CompletedTask;
         }
     }
