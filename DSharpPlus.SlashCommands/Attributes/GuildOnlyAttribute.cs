06cd11fe85df725b522338c30d098ea0ac445084
Lunar Starstrum <lunar@forsaken-borders.net>
2022-08-01 16:36:48 -0500

    Variable renaming and correct constraints in ExtensionMethods.GetName<T> and `dotnet format` with required file headers

--- DSharpPlus.SlashCommands/Attributes/GuildOnlyAttribute.cs
+++ DSharpPlus.SlashCommands/Attributes/GuildOnlyAttribute.cs
@@ -1,7 +1,7 @@
 // This file is part of the DSharpPlus project.
 //
 // Copyright (c) 2015 Mike Santiago
-// Copyright (c) 2016-2021 DSharpPlus Contributors
+// Copyright (c) 2016-2022 DSharpPlus Contributors
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
@@ -20,6 +20,7 @@
 // LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
+
 using System;
 
 namespace DSharpPlus.SlashCommands
--- DSharpPlus.SlashCommands/ExtensionMethods.cs
+++ DSharpPlus.SlashCommands/ExtensionMethods.cs
@@ -1,3 +1,26 @@
+// This file is part of the DSharpPlus project.
+//
+// Copyright (c) 2015 Mike Santiago
+// Copyright (c) 2016-2022 DSharpPlus Contributors
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
 using System;
 using System.Collections.Generic;
 using System.Globalization;
@@ -20,11 +43,11 @@ namespace DSharpPlus.SlashCommands
         public static SlashCommandsExtension UseSlashCommands(this DiscordClient client, SlashCommandsConfiguration config = null)
         {
             if (client.GetExtension<SlashCommandsExtension>() != null)
-                throw new InvalidOperationException("Slash commands are already enabled for that client.");
+                throw new InvalidOperationException("Slash commands are already enabled for this client.");
 
-            var scomm = new SlashCommandsExtension(config);
-            client.AddExtension(scomm);
-            return scomm;
+            var slashCommands = new SlashCommandsExtension(config);
+            client.AddExtension(slashCommands);
+            return slashCommands;
         }
 
         /// <summary>
@@ -81,25 +104,23 @@ namespace DSharpPlus.SlashCommands
         /// Gets the name from the <see cref="ChoiceNameAttribute"/> for this enum value.
         /// </summary>
         /// <returns>The name.</returns>
-        public static string GetName<T>(this T e) where T : IConvertible
+        public static string GetName<T>(this T @enum) where T : Enum, IConvertible
         {
-            if (e is Enum)
+            var type = @enum.GetType();
+            var values = Enum.GetValues(type);
+
+            foreach (int value in values)
             {
-                var type = e.GetType();
-                var values = Enum.GetValues(type);
-
-                foreach (int val in values)
+                if (value == @enum.ToInt32(CultureInfo.InvariantCulture))
                 {
-                    if (val == e.ToInt32(CultureInfo.InvariantCulture))
-                    {
-                        var memInfo = type.GetMember(type.GetEnumName(val));
+                    var memberInfo = type.GetMember(type.GetEnumName(value));
 
-                        return memInfo[0].GetCustomAttributes(typeof(ChoiceNameAttribute), false).FirstOrDefault() is ChoiceNameAttribute nameAttribute
-                            ? nameAttribute.Name
-                            : type.GetEnumName(val);
-                    }
+                    return memberInfo[0].GetCustomAttributes(typeof(ChoiceNameAttribute), false).FirstOrDefault() is ChoiceNameAttribute nameAttribute
+                        ? nameAttribute.Name
+                        : type.GetEnumName(value);
                 }
             }
+
             return null;
         }
     }
