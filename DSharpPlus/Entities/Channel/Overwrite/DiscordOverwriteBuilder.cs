a58445ebfa18a38d75a35ae141b92aa5a36762a2
Lilith Athrandil <61018569+ExaInsanity@users.noreply.github.com>
2022-10-19 15:24:07 +0200

    improve obsoletion message
    and update it to specify 4.4, as discussed; as 5.0 shall be a full rewrite.

--- DSharpPlus/Entities/Channel/Overwrite/DiscordOverwriteBuilder.cs
+++ DSharpPlus/Entities/Channel/Overwrite/DiscordOverwriteBuilder.cs
@@ -72,7 +72,7 @@ namespace DSharpPlus.Entities
         /// <summary>
         /// Creates a new Discord permission overwrite builder. This class can be used to construct permission overwrites for guild channels, used when creating channels.
         /// </summary>
-        [Obsolete("Will be removed in 5.0. Use specialized constructors instead", false)]
+        [Obsolete("The parameterless constructor will be removed in 4.4. Use the specialized constructors instead", false)]
         public DiscordOverwriteBuilder()
         {
 
