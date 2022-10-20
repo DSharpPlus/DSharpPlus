0d839a3a33c184aca33ab6e3681946f4dba9fec6
Velvet <42438262+VelvetThePanda@users.noreply.github.com>
2022-10-20 16:50:13 -0400

    Add missing serialization cases

--- DSharpPlus/Net/Serialization/DiscordComponentJsonConverter.cs
+++ DSharpPlus/Net/Serialization/DiscordComponentJsonConverter.cs
@@ -50,6 +50,10 @@ namespace DSharpPlus.Net.Serialization
                 ComponentType.Button => new DiscordButtonComponent(),
                 ComponentType.StringSelect => new DiscordSelectComponent(),
                 ComponentType.FormInput => new TextInputComponent(),
+                ComponentType.UserSelect => new DiscordUserSelectComponent(),
+                ComponentType.RoleSelect => new DiscordRoleSelectComponent(),
+                ComponentType.MentionableSelect => new DiscordMentionableSelectComponent(),
+                ComponentType.ChannelSelect => new DiscordChannelSelectComponent(),
                 _ => new DiscordComponent() { Type = type.Value }
             };
 
