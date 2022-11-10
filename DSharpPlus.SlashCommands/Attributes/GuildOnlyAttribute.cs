5a20ac54397aff4889cbedd8d181cefd79d71550
Ryan de Jonge <ryandejonge@outlook.com>
2022-11-10 20:24:31 +0100

    Add AttributeTargets.Method on GuildOnlyAttribute since it's missing

--- DSharpPlus.SlashCommands/Attributes/GuildOnlyAttribute.cs
+++ DSharpPlus.SlashCommands/Attributes/GuildOnlyAttribute.cs
@@ -27,6 +27,6 @@ namespace DSharpPlus.SlashCommands
     /// <summary>
     /// Indicates that a global application command cannot be invoked in DMs.
     /// </summary>
-    [AttributeUsage(AttributeTargets.Class)]
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
     public class GuildOnlyAttribute : Attribute { }
 }
