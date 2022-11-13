43aa5a0797b741e61d7aa36fe1e8e4de4eeeecd9
Ryan de Jonge <ryandejonge@outlook.com>
2022-11-13 01:20:11 +0100

    Add comments to builder interface methods and fields.

--- DSharpPlus/Entities/IDiscordMessageBuilder.cs
+++ DSharpPlus/Entities/IDiscordMessageBuilder.cs
@@ -35,44 +35,132 @@ namespace DSharpPlus.Entities
     public interface IDiscordMessageBuilder<T> where T : IDiscordMessageBuilder<T>
         // This has got to be the most big brain thing I have ever done with interfaces lmfao
     {
+        /// <summary>
+        /// Getter / setter for message content.
+        /// </summary>
         string Content { get; set; }
 
+        /// <summary>
+        /// Whether this message will play as a text-to-speech message.
+        /// </summary>
         bool IsTTS { get; set; }
 
+        /// <summary>
+        /// All embeds on this message.
+        /// </summary>
         IReadOnlyList<DiscordEmbed> Embeds { get; }
 
+        /// <summary>
+        /// All files on this message.
+        /// </summary>
         IReadOnlyList<DiscordMessageFile> Files { get; }
 
+        /// <summary>
+        /// All components on this message.
+        /// </summary>
         IReadOnlyList<DiscordActionRowComponent> Components { get; }
 
+        /// <summary>
+        /// All allowed mentions on this message.
+        /// </summary>
         IReadOnlyList<IMention> Mentions { get; }
 
+        /// <summary>
+        /// Adds content to this message
+        /// </summary>
+        /// <param name="content">Message content to use</param>
+        /// <returns></returns>
         T WithContent(string content);
 
+        /// <summary>
+        /// Adds components to this message. Each call should append to a new row.
+        /// </summary>
+        /// <param name="components">Components to add.</param>
+        /// <returns></returns>
         T AddComponents(params DiscordComponent[] components);
 
+        /// <summary>
+        /// Adds components to this message. Each call should append to a new row.
+        /// </summary>
+        /// <param name="components">Components to add.</param>
+        /// <returns></returns>
         T AddComponents(IEnumerable<DiscordComponent> components);
 
+        /// <summary>
+        /// Adds an action row component to this message.
+        /// </summary>
+        /// <param name="components">Action row to add to this message. Should contain child components.</param>
+        /// <returns></returns>
         T AddComponents(IEnumerable<DiscordActionRowComponent> components);
 
+        /// <summary>
+        /// Sets whether this message should play as a text-to-speech message.
+        /// </summary>
+        /// <param name="isTTS"></param>
+        /// <returns></returns>
         T WithTTS(bool isTTS);
 
+        /// <summary>
+        /// Adds an embed to this message.
+        /// </summary>
+        /// <param name="embed">Embed to add.</param>
+        /// <returns></returns>
         T AddEmbed(DiscordEmbed embed);
 
+        /// <summary>
+        /// Adds multiple embeds to this message.
+        /// </summary>
+        /// <param name="embeds">Collection of embeds to add.</param>
+        /// <returns></returns>
         T AddEmbeds(IEnumerable<DiscordEmbed> embeds);
 
+        /// <summary>
+        /// Attaches a file to this message.
+        /// </summary>
+        /// <param name="fileName">Name of the file to attach.</param>
+        /// <param name="stream">Stream containing said file's contents.</param>
+        /// <param name="resetStream">Whether to reset the stream to position 0 after sending.</param>
+        /// <returns></returns>
         T AddFile(string fileName, Stream stream, bool resetStream = false);
 
+        /// <summary>
+        /// Attaches a file to this message.
+        /// </summary>
+        /// <param name="stream">FileStream pointiong to the file to attach.</param>
+        /// <param name="resetStream">Whether to reset the stream position to 0 after sending.</param>
+        /// <returns></returns>
         T AddFile(FileStream stream, bool resetStream = false);
 
+        /// <summary>
+        /// Attaches multiple files to this message.
+        /// </summary>
+        /// <param name="files">Dictionary of files to add, where <see cref="string"/> is a file name and <see cref="Stream"/> is a stream containing the file's contents.</param>
+        /// <param name="resetStreams">Whether to reset all stream positions to 0 after sending.</param>
+        /// <returns></returns>
         T AddFiles(IDictionary<string, Stream> files, bool resetStreams = false);
 
+        /// <summary>
+        /// Adds an allowed mention to this message.
+        /// </summary>
+        /// <param name="mention">Mention to allow in this message.</param>
+        /// <returns></returns>
         T AddMention(IMention mention);
 
+        /// <summary>
+        /// Adds multiple allowed mentions to this message.
+        /// </summary>
+        /// <param name="mentions">Collection of mentions to allow in this message.</param>
+        /// <returns></returns>
         T AddMentions(IEnumerable<IMention> mentions);
 
+        /// <summary>
+        /// Clears all components attached to this builder.
+        /// </summary>
         void ClearComponents();
 
+        /// <summary>
+        /// Clears this builder.
+        /// </summary>
         void Clear();
     }
 }
