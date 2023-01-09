66732fe484c0e2434872f146ef74996ca89e96e9
InFTord <inftord@gmail.com>
2023-01-09 19:49:20 +0500

    rewriting powershell script in bash

---
+++ tools/rebuild-docs.sh
@@ -0,0 +1,17 @@
+# Download .NET install script
+echo "Downloading .NET install script"
+curl -O https://dot.net/v1/dotnet-install.sh
+
+# make install script launchable
+
+chmod u+x ./dotnet-install.sh
+# install .NET 
+echo "Installing .NET"
+./dotnet-install.sh
+echo "Installing DocFX"
+rm -f ./dotnet-install.sh
+# install docfx preview (so it would work with .NET 6+)
+dotnet tool update -g docfx --prerelease
+
+echo "Building docs"
+docfx build ../docs/docfx.json
