b52254df3f4f352549561a6f9a89a52c6a4e0484
Lunar Starstrum <lunar@forsaken-borders.net>
2023-01-27 22:26:07 -0600

    Cleanup Github Actions (#1451)
    * Cleanup Github Actions
    
    - Remove custom dotnet install dir since the runners are now in a Docker container (which has the .NET SDK preinstalled)
    - Fix a bug in the release action which had removed/incorrect set the latest nightly version
    - Readability
    
    * Skip exporting the tag when it's not a commit to the master branch

--- tools/AutoUpdateChannelDescription/src/Program.cs
+++ tools/AutoUpdateChannelDescription/src/Program.cs
@@ -23,11 +23,13 @@
 
 using System;
 using System.Globalization;
+using System.Linq;
 using System.Reflection;
+using System.Text;
 using System.Threading.Tasks;
 using DSharpPlus.Exceptions;
 
-namespace DSharpPlus.Tools.AutoUpdateChannelDescription
+namespace DSharpPlus.Tools.AutoUpdateChannelDescription.src
 {
     public sealed class Program
     {
@@ -37,14 +39,9 @@ namespace DSharpPlus.Tools.AutoUpdateChannelDescription
             var guildId = Environment.GetEnvironmentVariable("DISCORD_GUILD_ID") ?? throw new InvalidOperationException("DISCORD_GUILD_ID environment variable is not set.");
             var channelId = Environment.GetEnvironmentVariable("DISCORD_CHANNEL_ID") ?? throw new InvalidOperationException("DISCORD_CHANNEL_ID environment variable is not set.");
             var channelTopic = Environment.GetEnvironmentVariable("DISCORD_CHANNEL_TOPIC") ?? throw new InvalidOperationException("DISCORD_DESCRIPTION environment variable is not set.");
-            var latestStableVersion = args.Length == 1 ? args[0] : throw new InvalidOperationException("LATEST_STABLE_VERSION should be the first argument passed.");
             var nugetUrl = Environment.GetEnvironmentVariable("NUGET_URL") ?? throw new InvalidOperationException("NUGET_URL environment variable is not set.");
             var githubUrl = Environment.GetEnvironmentVariable("GITHUB_URL") ?? throw new InvalidOperationException("GITHUB_URL environment variable is not set.");
-
-            if (latestStableVersion.StartsWith("v", StringComparison.OrdinalIgnoreCase))
-            {
-                latestStableVersion = latestStableVersion[1..];
-            }
+            var latestStableVersion = Environment.GetEnvironmentVariable("LATEST_STABLE_VERSION");
 
             DiscordClient client = new(new DiscordConfiguration
             {
@@ -60,19 +57,39 @@ namespace DSharpPlus.Tools.AutoUpdateChannelDescription
                 // Task.Run in case ratelimit gets hit and event handler is cancelled.
                 _ = Task.Run(async () =>
                 {
+                    StringBuilder builder = new(channelTopic);
+                    builder.AppendLine();
+                    builder.AppendLine(Formatter.Bold("GitHub") + ": " + githubUrl);
+
+                    // If the latest stable version is not set, try to get it from the channel topic.
+                    latestStableVersion ??= channel.Topic.Split('\n').FirstOrDefault(line => line.StartsWith(Formatter.Bold("Latest stable version") + ": " + nugetUrl + "/", false, CultureInfo.InvariantCulture))?.Split('/').LastOrDefault();
+                    if (!string.IsNullOrWhiteSpace(latestStableVersion))
+                        builder.AppendLine(Formatter.Bold("Latest stable version") + ": " + nugetUrl + "/" + latestStableVersion);
+
+                    var nightlyVersion = typeof(DiscordClient).Assembly.GetCustomAttribute<AssemblyInformationalVersionAttribute>()?.InformationalVersion;
+
+                    // Default version string is 1.0.0, which happens when the -p:Nightly compiler argument is not set.
+                    if (string.IsNullOrWhiteSpace(nightlyVersion) || nightlyVersion.Equals("1.0.0", StringComparison.Ordinal))
+                        // Get the previous version from the channel topic.
+                        nightlyVersion = channel.Topic.Split('\n').FirstOrDefault(x => x.StartsWith(Formatter.Bold("Latest preview version") + ": " + nugetUrl + "/"))?.Split('/').LastOrDefault() ?? throw new InvalidOperationException("Could not find previous nightly version in channel topic.");
+
+                    // Update the channel topic with the latest nightly version.
+                    builder.AppendLine(Formatter.Bold("Latest preview version") + ": " + nugetUrl + "/" + nightlyVersion);
+
                     try
                     {
-                        await channel.ModifyAsync(channel => channel.Topic = @$"{channelTopic}
-
-{Formatter.Bold("GitHub")}: {githubUrl}
-{Formatter.Bold("Latest stable version")}: {nugetUrl}/{latestStableVersion}
-{Formatter.Bold("Latest preview version")}: {nugetUrl}/{typeof(DiscordClient).Assembly.GetCustomAttribute<AssemblyInformationalVersionAttribute>()!.InformationalVersion}");
+                        await channel.ModifyAsync(channel =>
+                        {
+                            channel.AuditLogReason = $"Github Actions: Updating channel topic to match stable version {latestStableVersion} and nightly version {nightlyVersion}.";
+                            channel.Topic = builder.ToString();
+                        });
                     }
                     catch (DiscordException error)
                     {
                         Console.WriteLine($"Error: HTTP {error.WebResponse.ResponseCode}, {error.WebResponse.Response}");
-                        Environment.Exit(1);
                     }
+
+                    await client.DisconnectAsync();
                     Environment.Exit(0);
                 });
 
@@ -83,7 +100,7 @@ namespace DSharpPlus.Tools.AutoUpdateChannelDescription
 
             // The program should exit ASAP after the channel description is updated.
             // However it may get caught in a ratelimit, so we'll wait for a bit.
-            // The program will exit after 30 seconds no matter what.
+            // The program will exit after 10 seconds no matter what.
             // This includes the time it takes to connect to the Discord gateway.
             await Task.Delay(TimeSpan.FromSeconds(30));
         }
