f2b8f50fe468edd644c68c97c24cb6f2a4a17579
Velvet <42438262+VelvetThePanda@users.noreply.github.com>
2023-02-09 02:48:26 -0500

    New flags just dropped

--- DSharpPlus/Enums/Message/MessageFlags.cs
+++ DSharpPlus/Enums/Message/MessageFlags.cs
@@ -45,26 +45,31 @@ namespace DSharpPlus
         /// <summary>
         /// Whether this message is the original message that was published from a news channel to subscriber channels.
         /// </summary>
+        /// <remarks>This flag is inbound only (it cannot be set).</remarks>
         Crossposted = 1 << 0,
 
         /// <summary>
         /// Whether this message is crossposted (automatically posted in a subscriber channel).
         /// </summary>
+        /// <remarks>This flag is inbound only (it cannot be set).</remarks>
         IsCrosspost = 1 << 1,
 
         /// <summary>
         /// Whether any embeds in the message are hidden.
         /// </summary>
+        /// <remarks>This flag is inbound only (it cannot be set).</remarks>
         SuppressedEmbeds = 1 << 2,
 
         /// <summary>
         /// The source message for this crosspost has been deleted.
         /// </summary>
-        SourceMessageDelete = 1 << 3,
+        /// <remarks>This flag is inbound only (it cannot be set).</remarks>
+        SourceMessageDeleted = 1 << 3,
 
         /// <summary>
         /// The message came from the urgent message system.
         /// </summary>
+        /// <remarks>This flag is inbound only (it cannot be set).</remarks>
         Urgent = 1 << 4,
 
         /// <summary>
@@ -75,6 +80,26 @@ namespace DSharpPlus
         /// <summary>
         /// The message is an interaction response and the bot is "thinking".
         /// </summary>
-        Loading = 1 << 7
+        /// <remarks>This flag is inbound only (it cannot be set).</remarks>
+        Loading = 1 << 7,
+
+        /// <summary>
+        /// Indicates that some roles mentioned in the message could not be added to the current thread.
+        /// </summary>
+        /// <remarks>This flag is inbound only (it cannot be set).</remarks>
+        FailedToMentionSomeRolesInThread = 1 << 8,
+
+        /// <summary>
+        /// Indicates that the message contains a link (usually to a file) that will prompt the user
+        /// with a precautionary message saying that the link may be unsafe.
+        /// </summary>
+        /// <remarks>This flag is inbound only (it cannot be set).</remarks>
+        ContainsSuspiciousThirdPartyLink = 1 << 10,
+
+        /// <summary>
+        /// Indicates that this message will supress push notifications.
+        /// Mentions in the message will still have a mention indicator, however.
+        /// </summary>
+        SupressNotifications = 1 << 12
     }
 }
