866851017577969b0212eee70637067cc03a3aee
InFTord <inftord@gmail.com>
2023-02-14 21:59:21 +0500

    update copyright year, yet again

--- DSharpPlus/EventArgs/Socket/SocketEventArgs.cs
+++ DSharpPlus/EventArgs/Socket/SocketEventArgs.cs
@@ -1,7 +1,7 @@
 // This file is part of the DSharpPlus project.
 //
 // Copyright (c) 2015 Mike Santiago
-// Copyright (c) 2016-2022 DSharpPlus Contributors
+// Copyright (c) 2016-2023 DSharpPlus Contributors
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
@@ -24,7 +24,7 @@
 namespace DSharpPlus.EventArgs
 {
     /// <summary>
-    /// Represents basic socket event arguments. 
+    /// Represents basic socket event arguments.
     /// </summary>
     public class SocketEventArgs : DiscordEventArgs
     {
