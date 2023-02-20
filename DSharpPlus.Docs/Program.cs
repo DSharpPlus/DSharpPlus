c3aa1749ea08492d83ec1d7f1f8bab9f275b0aba
Lunar Starstrum <lunar@forsaken-borders.net>
2023-02-19 18:55:40 -0600

    Move doc program to the tools folder and remove stale files

--- DSharpPlus.Docs/Program.cs
+++
@@ -1,8 +0,0 @@
-using Microsoft.DocAsCode;
-namespace DSharpPlus
-{
-    public class Program
-    {
-        public static async Task Main(string[] args) => await Docset.Build("docfx.json");
-    }
-}
