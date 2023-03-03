c2d9b7c451a6ca02b40ae59bb8834801890d7701
InFTord <inftord@gmail.com>
2023-03-03 23:38:21 +0500

    update license year to 2023

--- DSharpPlus.VoiceNext/VoiceTransmitSink.cs
+++ DSharpPlus.VoiceNext/VoiceTransmitSink.cs
@@ -1,7 +1,7 @@
 // This file is part of the DSharpPlus project.
 //
 // Copyright (c) 2015 Mike Santiago
-// Copyright (c) 2016-2022 DSharpPlus Contributors
+// Copyright (c) 2016-2023 DSharpPlus Contributors
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
@@ -47,7 +47,7 @@ namespace DSharpPlus.VoiceNext
             => this.PcmBufferDuration;
 
         /// <summary>
-        /// Gets the length of the PCM buffer for this sink. 
+        /// Gets the length of the PCM buffer for this sink.
         /// Written packets should adhere to this size, but the sink will adapt to fit.
         /// </summary>
         public int SampleLength
