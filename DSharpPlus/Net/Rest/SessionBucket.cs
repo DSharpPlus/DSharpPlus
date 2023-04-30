cdeed945cb14acfc40c18c9ca161dc896af3ed86
akiraveliara <akiraveliara@outlook.com>
2023-04-30 12:54:48 +0200

    kill dead code and reformat SessionBucket

--- DSharpPlus/Net/Rest/SessionBucket.cs
+++ DSharpPlus/Net/Rest/SessionBucket.cs
@@ -24,45 +24,41 @@
 using System;
 using Newtonsoft.Json;
 
-namespace DSharpPlus.Net
+namespace DSharpPlus.Net;
+
+/// <summary>
+/// Represents the bucket limits for identifying to Discord.
+/// <para>This is only relevant for clients that are manually sharding.</para>
+/// </summary>
+public class SessionBucket
 {
     /// <summary>
-    /// Represents the bucket limits for identifying to Discord.
-    /// <para>This is only relevant for clients that are manually sharding.</para>
+    /// Gets the total amount of sessions per token.
     /// </summary>
-    public class SessionBucket
-    {
-        /// <summary>
-        /// Gets the total amount of sessions per token.
-        /// </summary>
-        [JsonProperty("total")]
-        public int Total { get; internal set; }
+    [JsonProperty("total")]
+    public int Total { get; internal set; }
 
-        /// <summary>
-        /// Gets the remaining amount of sessions for this token.
-        /// </summary>
-        [JsonProperty("remaining")]
-        public int Remaining { get; internal set; }
+    /// <summary>
+    /// Gets the remaining amount of sessions for this token.
+    /// </summary>
+    [JsonProperty("remaining")]
+    public int Remaining { get; internal set; }
 
-        /// <summary>
-        /// Gets the datetime when the <see cref="Remaining"/> will reset.
-        /// </summary>
-        [JsonIgnore]
-        public DateTimeOffset ResetAfter { get; internal set; }
+    /// <summary>
+    /// Gets the datetime when the <see cref="Remaining"/> will reset.
+    /// </summary>
+    [JsonIgnore]
+    public DateTimeOffset ResetAfter { get; internal set; }
 
-        /// <summary>
-        /// Gets the maximum amount of shards that can boot concurrently.
-        /// </summary>
-        [JsonProperty("max_concurrency")]
-        public int MaxConcurrency { get; internal set; }
+    /// <summary>
+    /// Gets the maximum amount of shards that can boot concurrently.
+    /// </summary>
+    [JsonProperty("max_concurrency")]
+    public int MaxConcurrency { get; internal set; }
 
-        [JsonProperty("reset_after")]
-        internal int ResetAfterInternal { get; set; }
+    [JsonProperty("reset_after")]
+    internal int ResetAfterInternal { get; set; }
 
-#pragma warning disable CS0114
-        public override string ToString()
-            => $"[{this.Remaining}/{this.Total}] {this.ResetAfter}. {this.MaxConcurrency}x concurrency";
-
-#pragma warning restore CS0114
-    }
+    public override string ToString()
+        => $"[{this.Remaining}/{this.Total}] {this.ResetAfter}. {this.MaxConcurrency}x concurrency";
 }
