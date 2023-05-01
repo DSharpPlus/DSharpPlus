1ccc68358bbb8d8d075191bc8d4ef20f1f167a1b
Patrick Belmokhtar <belmokhtarpatrick@gmail.com>
2023-05-01 19:41:21 +0200

    Removed useless builder to use a public constructor

--- DSharpPlus/Entities/AutoModeration/Action/DiscordAutoModerationAction.cs
+++ DSharpPlus/Entities/AutoModeration/Action/DiscordAutoModerationAction.cs
@@ -7,7 +7,7 @@ namespace DSharpPlus.Entities;
 public class DiscordAutoModerationAction
 {
     [JsonProperty("type")]
-    public RuleActionType? Type { get; internal set; }
+    public RuleActionType Type { get; internal set; }
 
     /// <summary>
     /// Gets additional metadata needed during execution for this specific action type.
@@ -19,31 +19,4 @@ public class DiscordAutoModerationAction
     {
 
     }
-
-    internal DiscordAutoModerationAction(RuleActionType? ruleActionType = null, DiscordRuleActionMetadata? ruleActionMetadata = null)
-    {
-        this.Type = ruleActionType;
-        this.Metadata = ruleActionMetadata;
-    }
-}
-
-
-public class DiscordAutoModerationActionBuilder
-{
-    public RuleActionType? Type { internal get; set; }
-
-    public DiscordRuleActionMetadata? Metadata { internal get; set; }
-
-    public DiscordAutoModerationActionBuilder()
-    {
-
-    }
-
-    public DiscordAutoModerationActionBuilder(RuleActionType? ruleActionType = null, DiscordRuleActionMetadata? ruleActionMetadata = null)
-    {
-        this.Type = ruleActionType;
-        this.Metadata = ruleActionMetadata;
-    }
-
-    public DiscordAutoModerationAction Build() => new DiscordAutoModerationAction(this.Type, this.Metadata);
 }
