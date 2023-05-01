8721f9ab7f4eacd81e37825f7bf89b4b40a6c92a
Patrick Belmokhtar <belmokhtarpatrick@gmail.com>
2023-05-01 02:02:32 +0200

    Updated to 2023.

--- DSharpPlus/Net/Serialization/DiscordForumChannelJsonConverter.cs
+++ DSharpPlus/Net/Serialization/DiscordForumChannelJsonConverter.cs
@@ -1,7 +1,7 @@
 // This file is part of the DSharpPlus project.
 //
 // Copyright (c) 2015 Mike Santiago
-// Copyright (c) 2016-2022 DSharpPlus Contributors
+// Copyright (c) 2016-2023 DSharpPlus Contributors
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
@@ -20,8 +20,10 @@
 // LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
+
 using System;
 using DSharpPlus.Entities;
+
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 
