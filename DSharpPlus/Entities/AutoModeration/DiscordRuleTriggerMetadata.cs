545d736da2927a0baa6540baa870e8bee0012072
Patrick Belmokhtar <belmokhtarpatrick@gmail.com>
2023-05-01 02:08:37 +0200

    Documentation

--- DSharpPlus/Entities/AutoModeration/DiscordRuleTriggerMetadata.cs
+++ DSharpPlus/Entities/AutoModeration/DiscordRuleTriggerMetadata.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+
 using DSharpPlus.Enums;
 
 using Newtonsoft.Json;
@@ -6,19 +8,34 @@ namespace DSharpPlus.Entities;
 
 public sealed class DiscordRuleTriggerMetadata
 {
+    /// <summary>
+    /// Gets substrings which will be searched in the content.
+    /// </summary>
     [JsonProperty("keyword_filter")]
-    public string[]? KeywordFilter { get; internal set; }
+    public IReadOnlyList<string>? KeywordFilter { get; internal set; }
 
+    /// <summary>
+    /// Gets regex patterns which will be matched against the content.
+    /// </summary>
     [JsonProperty("regex_patterns")]
-    public string[]? RegexPatterns { get; internal set; }
+    public IReadOnlyList<string>? RegexPatterns { get; internal set; }
 
+    /// <summary>
+    /// Gets the internally pre-defined wordsets which will be searched in the content.
+    /// </summary>
     [JsonProperty("presets")]
-    public RuleKeywordPresetType[] KeywordPresetTypes { get; internal set; }
+    public IReadOnlyList<RuleKeywordPresetType> KeywordPresetTypes { get; internal set; }
 
+    /// <summary>
+    /// Gets the substrings which should not trigger the rule.
+    /// </summary>
     [JsonProperty("allow_list")]
-    public string[] AllowedKeywords { get; internal set; }
+    public IReadOnlyList<string> AllowedKeywords { get; internal set; }
 
-    [JsonProperty("mention_total_limit")]
+    /// <summary>
+    /// Gets the total number of mentions (users and roles) allowed per message.
+    /// </summary>
+    [JsonProperty("mention_total_limit", NullValueHandling = NullValueHandling.Ignore)]
     public int? MentionTotalLimit { get; internal set; }
 
     internal DiscordRuleTriggerMetadata()
@@ -29,13 +46,13 @@ public sealed class DiscordRuleTriggerMetadata
 
 public sealed class DiscordRuleTriggerMetadataBuilder
 {
-    public string[]? KeywordFilter { get; set; }
+    public IReadOnlyList<string>? KeywordFilter { get; set; }
 
-    public string[]? RegexPatterns { get; set; }
+    public IReadOnlyList<string>? RegexPatterns { get; set; }
 
-    public RuleKeywordPresetType[] KeywordPresetTypes { get; set; }
+    public IReadOnlyList<RuleKeywordPresetType> KeywordPresetTypes { get; set; }
 
-    public string[] AllowedKeywords { get; set; }
+    public IReadOnlyList<string> AllowedKeywords { get; set; }
 
     public int? MentionTotalLimit { get; set; }
 
@@ -44,7 +61,7 @@ public sealed class DiscordRuleTriggerMetadataBuilder
 
     }
 
-    public RuleTriggerMetadataBuilder(string[]? keywordFilter, string[]? regexPatterns, RuleKeywordPresetType[] keywordPresetTypes, string[] allowKeywords, int? mentionTotalLimit)
+    public DiscordRuleTriggerMetadataBuilder(IReadOnlyList<string>? keywordFilter, IReadOnlyList<string>? regexPatterns, IReadOnlyList<RuleKeywordPresetType> keywordPresetTypes, IReadOnlyList<string> allowKeywords, int? mentionTotalLimit)
     {
         this.AllowedKeywords = allowKeywords;
         this.MentionTotalLimit = mentionTotalLimit;
