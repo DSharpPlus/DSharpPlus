f10dc5110dc66098a0e0a2950e4c69b264bf10f3
Patrick Belmokhtar <belmokhtarpatrick@gmail.com>
2023-05-01 02:00:33 +0200

    Documentation added

--- DSharpPlus/Enums/AutoModeration/RuleEventType.cs
+++ DSharpPlus/Enums/AutoModeration/RuleEventType.cs
@@ -1,6 +1,12 @@
 namespace DSharpPlus.Enums;
 
+/// <summary>
+/// Indicates in what event context a rule should be checked.
+/// </summary>
 public enum RuleEventType
 {
+    /// <summary>
+    /// The rule will trigger when a member send or modify a message.
+    /// </summary>
     MessageSend = 1
 }
