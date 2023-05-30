ccbfb3875dc977db8f460fdd5bb129308ad2765b
akiraveliara <akiraveliara@outlook.com>
2023-05-30 17:21:15 +0200

    select menus

---
+++ src/core/DSharpPlus.Core.Abstractions.Models/MessageComponents/IStringSelectComponent.cs
@@ -0,0 +1,45 @@
+// This Source Code form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+using System.Collections.Generic;
+
+using Remora.Rest.Core;
+
+namespace DSharpPlus.Core.Abstractions.Models;
+
+/// <summary>
+/// Represents a dropdown menu from where users can select dev-supplied strings.
+/// </summary>
+public interface IStringSelectComponent : IInteractiveComponent
+{
+    /// <summary>
+    /// The developer-defined ID for this select menu, up to 100 characters.
+    /// </summary>
+    public string CustomId { get; }
+
+    /// <summary>
+    /// Up to 25 specified choices for this select menu.
+    /// </summary>
+    public IReadOnlyList<ISelectOption> Options { get; }
+
+    /// <summary>
+    /// Placeholder text if nothing is selected, up to 150 characters.
+    /// </summary>
+    public Optional<string> Placeholder { get; }
+
+    /// <summary>
+    /// The minimum number of items that must be chosen, between 0 and 25.
+    /// </summary>
+    public Optional<int> MinValues { get; }
+
+    /// <summary>
+    /// The maximum number of items that can be chosen, between 1 and 25.
+    /// </summary>
+    public Optional<int> MaxValues { get; }
+
+    /// <summary>
+    /// Indicates whether this select menu is disabled.
+    /// </summary>
+    public Optional<bool> Disabled { get; }
+}
---
+++ src/core/DSharpPlus.Core.Abstractions.Models/MessageComponents/IUserSelectComponent.cs
@@ -0,0 +1,38 @@
+// This Source Code form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+using Remora.Rest.Core;
+
+namespace DSharpPlus.Core.Abstractions.Models;
+
+/// <summary>
+/// Represents a dropdown menu from where users can select discord-supplied users.
+/// </summary>
+public interface IUserSelectComponent : IInteractiveComponent
+{
+    /// <summary>
+    /// The developer-defined ID for this select menu, up to 100 characters.
+    /// </summary>
+    public string CustomId { get; }
+
+    /// <summary>
+    /// Placeholder text if nothing is selected, up to 150 characters.
+    /// </summary>
+    public Optional<string> Placeholder { get; }
+
+    /// <summary>
+    /// The minimum number of items that must be chosen, between 0 and 25.
+    /// </summary>
+    public Optional<int> MinValues { get; }
+
+    /// <summary>
+    /// The maximum number of items that can be chosen, between 1 and 25.
+    /// </summary>
+    public Optional<int> MaxValues { get; }
+
+    /// <summary>
+    /// Indicates whether this select menu is disabled.
+    /// </summary>
+    public Optional<bool> Disabled { get; }
+}
