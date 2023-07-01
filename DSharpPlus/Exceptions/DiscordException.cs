7c68a1ee970cd014b921b99ee21ffc6e56edb53b
Lunar Starstrum <lunar@forsaken-borders.net>
2023-06-30 21:48:16 -0500

    Update DiscordExceptions to include the json message within the error message. Additionally format the exception classes to conform to the `.editorconfig`

--- DSharpPlus/Exceptions/DiscordException.cs
+++ DSharpPlus/Exceptions/DiscordException.cs
@@ -24,27 +24,29 @@
 using System;
 using DSharpPlus.Net;
 
-namespace DSharpPlus.Exceptions
+namespace DSharpPlus.Exceptions;
+
+public abstract class DiscordException : Exception
 {
-    public abstract class DiscordException : Exception
-    {
-        /// <summary>
-        /// Gets the request that caused the exception.
-        /// </summary>
-        public virtual BaseRestRequest WebRequest { get; internal set; }
+    /// <summary>
+    /// Gets the request that caused the exception.
+    /// </summary>
+    public virtual BaseRestRequest WebRequest { get; internal set; } = null!;
 
-        /// <summary>
-        /// Gets the response to the request.
-        /// </summary>
-        public virtual RestResponse WebResponse { get; internal set; }
+    /// <summary>
+    /// Gets the response to the request.
+    /// </summary>
+    public virtual RestResponse WebResponse { get; internal set; } = null!;
 
-        /// <summary>
-        /// Gets the JSON message received.
-        /// </summary>
-        public virtual string JsonMessage { get; internal set; }
+    /// <summary>
+    /// Gets the JSON message received.
+    /// </summary>
+    public virtual string? JsonMessage { get; internal set; } = null!;
 
-        public DiscordException() : base() { }
-        public DiscordException(string message) : base(message) { }
-        public DiscordException(string message, Exception innerException) : base(message, innerException) { }
-    }
+    /// <inheritdoc />
+    public override string Message => $"{base.Message}. Json Message: {JsonMessage ?? "No Json Message Provided."}";
+
+    public DiscordException() : base() { }
+    public DiscordException(string message) : base(message) { }
+    public DiscordException(string message, Exception innerException) : base(message, innerException) { }
 }
