9ccd5494c340c7ffb226072610b6af5cb6dcc47b
akiraveliara <akiraveliara@outlook.com>
2023-07-01 14:33:26 +0200

    add non-partial channels

---
+++ src/core/DSharpPlus.Core.Abstractions.Models/Channels/IChannel.cs
@@ -0,0 +1,29 @@
+// This Source Code form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+using DSharpPlus.Entities;
+
+using Remora.Rest.Core;
+
+namespace DSharpPlus.Core.Abstractions.Models;
+
+/// <summary>
+/// Represents a fully populated channel object.
+/// </summary>
+public interface IChannel : IPartialChannel
+{
+    /// <inheritdoc cref="IPartialChannel.Id"/>
+    public new Snowflake Id { get; }
+
+    /// <inheritdoc cref="IPartialChannel.Type"/>
+    public new DiscordChannelType Type { get; }
+
+    // partial access routes
+
+    /// <inheritdoc/>
+    Optional<Snowflake> IPartialChannel.Id => this.Id;
+
+    /// <inheritdoc/>
+    Optional<DiscordChannelType> IPartialChannel.Type => this.Type;
+}
