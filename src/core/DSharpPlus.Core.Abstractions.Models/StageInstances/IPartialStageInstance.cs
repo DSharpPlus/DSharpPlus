8ccdf14d425c282df64da0163760659edf05edc7
akiraveliara <akiraveliara@outlook.com>
2023-07-01 19:59:06 +0200

    add stage instances

---
+++ src/core/DSharpPlus.Core.Abstractions.Models/StageInstances/IPartialStageInstance.cs
@@ -0,0 +1,45 @@
+// This Source Code form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+using DSharpPlus.Entities;
+
+using Remora.Rest.Core;
+
+namespace DSharpPlus.Core.Abstractions.Models;
+
+/// <summary>
+/// Represents a partial stage instance.
+/// </summary>
+public interface IPartialStageInstance
+{
+    /// <summary>
+    /// The snowflake identifier of this stage instance.
+    /// </summary>
+    public Optional<Snowflake> Id { get; }
+
+    /// <summary>
+    /// The snowflake identifier of the guild this stage instance is held in.
+    /// </summary>
+    public Optional<Snowflake> GuildId { get; }
+
+    /// <summary>
+    /// The snowflake identifier of the associated stage channel.
+    /// </summary>
+    public Optional<Snowflake> ChannelId { get; }
+
+    /// <summary>
+    /// The topic of this stage instance, 1 to 120 characters.
+    /// </summary>
+    public Optional<string> Topic { get; }
+
+    /// <summary>
+    /// The privacy level of this stage instance.
+    /// </summary>
+    public Optional<DiscordStagePrivacyLevel> PrivacyLevel { get; }
+
+    /// <summary>
+    /// The snowflake identifier of the scheduled event for this stage instance.
+    /// </summary>
+    public Optional<Snowflake?> GuildScheduledEventId { get; }
+}
---
+++ src/core/DSharpPlus.Shared/Entities/StageInstances/DiscordStagePrivacyLevel.cs
@@ -0,0 +1,16 @@
+// This Source Code form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+namespace DSharpPlus.Entities;
+
+/// <summary>
+/// Indicates to whom this stage instance is visible.
+/// </summary>
+public enum DiscordStagePrivacyLevel
+{
+    /// <summary>
+    /// This stage instance is only visible to guild members.
+    /// </summary>
+    GuildOnly = 2
+}
