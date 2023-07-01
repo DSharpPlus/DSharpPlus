6f8884e79765a25a16fbc85f0afc873b4de0c5c4
akiraveliara <akiraveliara@outlook.com>
2023-07-01 20:59:44 +0200

    add partial applicatioon command permissions

--- src/core/DSharpPlus.Core.Abstractions.Models/ApplicationCommands/IApplicationCommandPermissions.cs
+++ src/core/DSharpPlus.Core.Abstractions.Models/ApplicationCommands/IApplicationCommandPermissions.cs
@@ -1,4 +1,4 @@
-﻿// This Source Code form is subject to the terms of the Mozilla Public
+// This Source Code form is subject to the terms of the Mozilla Public
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
@@ -11,25 +11,31 @@ namespace DSharpPlus.Core.Abstractions.Models;
 /// <summary>
 /// Represents a collection of permissions for an application command in a guild
 /// </summary>
-public interface IApplicationCommandPermissions
+public interface IApplicationCommandPermissions : IPartialApplicationCommandPermissions
 {
-    /// <summary>
-    /// The snowflake identifier of this command.
-    /// </summary>
-    public Snowflake Id { get; }
+    /// <inheritdoc cref="IPartialApplicationCommandPermissions.Id"/>
+    public new Snowflake Id { get; }
 
-    /// <summary>
-    /// The snowflake identifier of the application this command belongs to.
-    /// </summary>
-    public Snowflake ApplicationId { get; }
+    /// <inheritdoc cref="IPartialApplicationCommandPermissions.ApplicationId"/>
+    public new Snowflake ApplicationId { get; }
 
-    /// <summary>
-    /// The snowflake identifier of the guild to which these permissions apply.
-    /// </summary>
-    public Snowflake GuildId { get; }
+    /// <inheritdoc cref="IPartialApplicationCommandPermissions.GuildId"/>
+    public new Snowflake GuildId { get; }
 
-    /// <summary>
-    /// The permission overrides for this command in this guild.
-    /// </summary>
-    public IReadOnlyList<IApplicationCommandPermission> Permissions { get; }
+    /// <inheritdoc cref="IPartialApplicationCommandPermissions.Permissions"/>
+    public new IReadOnlyList<IApplicationCommandPermission> Permissions { get; }
+
+    // partial access routes
+
+    /// <inheritdoc/>
+    Optional<Snowflake> IPartialApplicationCommandPermissions.Id => this.Id;
+
+    /// <inheritdoc/>
+    Optional<Snowflake> IPartialApplicationCommandPermissions.ApplicationId => this.ApplicationId;
+
+    /// <inheritdoc/>
+    Optional<Snowflake> IPartialApplicationCommandPermissions.GuildId => this.GuildId;
+
+    /// <inheritdoc/>
+    Optional<IReadOnlyList<IApplicationCommandPermission>> IPartialApplicationCommandPermissions.Permissions => new(this.Permissions);
 }
---
+++ src/core/DSharpPlus.Core.Abstractions.Models/ApplicationCommands/IPartialApplicationCommandPermissions.cs
@@ -0,0 +1,35 @@
+// This Source Code form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+using System.Collections.Generic;
+
+using Remora.Rest.Core;
+
+namespace DSharpPlus.Core.Abstractions.Models;
+
+/// <summary>
+/// Represents a partial set of application command permissions.
+/// </summary>
+public interface IPartialApplicationCommandPermissions
+{
+    /// <summary>
+    /// The snowflake identifier of this command.
+    /// </summary>
+    public Optional<Snowflake> Id { get; }
+
+    /// <summary>
+    /// The snowflake identifier of the application this command belongs to.
+    /// </summary>
+    public Optional<Snowflake> ApplicationId { get; }
+
+    /// <summary>
+    /// The snowflake identifier of the guild to which these permissions apply.
+    /// </summary>
+    public Optional<Snowflake> GuildId { get; }
+
+    /// <summary>
+    /// The permission overrides for this command in this guild.
+    /// </summary>
+    public Optional<IReadOnlyList<IApplicationCommandPermission>> Permissions { get; }
+}
