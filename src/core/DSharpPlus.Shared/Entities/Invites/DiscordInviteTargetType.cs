309204d9cc554693821b88ffe8549ae07b5d065d
akiraveliara <akiraveliara@outlook.com>
2023-07-01 19:38:23 +0200

    add invites

---
+++ src/core/DSharpPlus.Shared/Entities/Invites/DiscordInviteTargetType.cs
@@ -0,0 +1,14 @@
+// This Source Code form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+namespace DSharpPlus.Entities;
+
+/// <summary>
+/// Specifies where this invite points to, if not a channel.
+/// </summary>
+public enum DiscordInviteTargetType
+{
+    Stream = 1,
+    EmbeddedApplication
+}
