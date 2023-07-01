d233af5d44893f3c71f155a806f5baba9302f17e
akiraveliara <akiraveliara@outlook.com>
2023-07-01 21:13:14 +0200

    add partial auto mod rules

--- src/core/DSharpPlus.Core.Abstractions.Models/AutoModeration/IAutoModerationRule.cs
+++ src/core/DSharpPlus.Core.Abstractions.Models/AutoModeration/IAutoModerationRule.cs
@@ -13,60 +13,73 @@ namespace DSharpPlus.Core.Abstractions.Models;
 /// <summary>
 /// Represents an auto moderation rule within a guild.
 /// </summary>
-public interface IAutoModerationRule
+public interface IAutoModerationRule : IPartialAutoModerationRule
 {
-    /// <summary>
-    /// The snowflake identifier of this rule.
-    /// </summary>
-    public Snowflake Id { get; }
+    /// <inheritdoc cref="IPartialAutoModerationRule.Id"/>
+    public new Snowflake Id { get; }
 
-    /// <summary>
-    /// The snowflake identifier of the guild this rule belongs to.
-    /// </summary>
-    public Snowflake GuildId { get; }
+    /// <inheritdoc cref="IPartialAutoModerationRule.GuildId"/>
+    public new Snowflake GuildId { get; }
 
-    /// <summary>
-    /// The display name of this rule.
-    /// </summary>
-    public string Name { get; }
+    /// <inheritdoc cref="IPartialAutoModerationRule.Name"/>
+    public new string Name { get; }
 
-    /// <summary>
-    /// The snowflake identifier of the user who created this rule.
-    /// </summary>
-    public Snowflake CreatorId { get; }
+    /// <inheritdoc cref="IPartialAutoModerationRule.CreatorId"/>
+    public new Snowflake CreatorId { get; }
 
-    /// <summary>
-    /// The type of event that causes this rule to fire.
-    /// </summary>
-    public DiscordAutoModerationEventType EventType { get; }
+    /// <inheritdoc cref="IPartialAutoModerationRule.EventType"/>
+    public new DiscordAutoModerationEventType EventType { get; }
 
-    /// <summary>
-    /// The trigger type of this rule.
-    /// </summary>
-    public DiscordAutoModerationTriggerType TriggerType { get; }
+    /// <inheritdoc cref="IPartialAutoModerationRule.TriggerType"/>
+    public new DiscordAutoModerationTriggerType TriggerType { get; }
 
-    /// <summary>
-    /// Additional metadata for this rule trigger.
-    /// </summary>
-    public IAutoModerationTriggerMetadata TriggerMetadata { get; }
+    /// <inheritdoc cref="IPartialAutoModerationRule.TriggerMetadata"/>
+    public new IAutoModerationTriggerMetadata TriggerMetadata { get; }
 
-    /// <summary>
-    /// The actions which wille xecute when this rule is triggered.
-    /// </summary>
-    public IReadOnlyList<IAutoModerationAction> Actions { get; }
+    /// <inheritdoc cref="IPartialAutoModerationRule.Actions"/>
+    public new IReadOnlyList<IAutoModerationAction> Actions { get; }
 
-    /// <summary>
-    /// Indicates whether this rule should be enabled.
-    /// </summary>
-    public bool Enabled { get; }
+    /// <inheritdoc cref="IPartialAutoModerationRule.Enabled"/>
+    public new bool Enabled { get; }
 
-    /// <summary>
-    /// Up to 20 role IDs that should be exempted from this rule.
-    /// </summary>
-    public IReadOnlyList<Snowflake> ExemptRoles { get; }
+    /// <inheritdoc cref="IPartialAutoModerationRule.ExemptRoles"/>
+    public new IReadOnlyList<Snowflake> ExemptRoles { get; }
 
-    /// <summary>
-    /// Up to 50 channel IDs that should be exempted from this rule.
-    /// </summary>
-    public IReadOnlyList<Snowflake> ExemptChannels { get; }
+    /// <inheritdoc cref="IPartialAutoModerationRule.ExemptChannels"/>
+    public new IReadOnlyList<Snowflake> ExemptChannels { get; }
+
+    // partial access routes
+
+    /// <inheritdoc/>
+    Optional<Snowflake> IPartialAutoModerationRule.Id => this.Id;
+
+    /// <inheritdoc/>
+    Optional<Snowflake> IPartialAutoModerationRule.GuildId => this.GuildId;
+
+    /// <inheritdoc/>
+    Optional<string> IPartialAutoModerationRule.Name => this.Name;
+
+    /// <inheritdoc/>
+    Optional<Snowflake> IPartialAutoModerationRule.CreatorId => this.CreatorId;
+
+    /// <inheritdoc/>
+    Optional<DiscordAutoModerationEventType> IPartialAutoModerationRule.EventType => this.EventType;
+
+    /// <inheritdoc/>
+    Optional<DiscordAutoModerationTriggerType> IPartialAutoModerationRule.TriggerType => this.TriggerType;
+
+    /// <inheritdoc/>
+    Optional<IAutoModerationTriggerMetadata> IPartialAutoModerationRule.TriggerMetadata => new(this.TriggerMetadata);
+
+    /// <inheritdoc/>
+    Optional<IReadOnlyList<IAutoModerationAction>> IPartialAutoModerationRule.Actions => new(this.Actions);
+
+    /// <inheritdoc/>
+    Optional<bool> IPartialAutoModerationRule.Enabled => this.Enabled;
+
+    /// <inheritdoc/>
+    Optional<IReadOnlyList<Snowflake>> IPartialAutoModerationRule.ExemptRoles => new(this.ExemptRoles);
+
+    /// <inheritdoc/>
+    Optional<IReadOnlyList<Snowflake>> IPartialAutoModerationRule.ExemptChannels => new(this.ExemptChannels);
 }
---
+++ src/core/DSharpPlus.Core.Abstractions.Models/AutoModeration/IPartialAutoModerationRule.cs
@@ -0,0 +1,72 @@
+// This Source Code form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+using System.Collections.Generic;
+
+using DSharpPlus.Entities;
+
+using Remora.Rest.Core;
+
+namespace DSharpPlus.Core.Abstractions.Models;
+
+/// <summary>
+/// Represents a partial auto-moderation rule.
+/// </summary>
+public interface IPartialAutoModerationRule
+{
+    /// <summary>
+    /// The snowflake identifier of this rule.
+    /// </summary>
+    public Optional<Snowflake> Id { get; }
+
+    /// <summary>
+    /// The snowflake identifier of the guild this rule belongs to.
+    /// </summary>
+    public Optional<Snowflake> GuildId { get; }
+
+    /// <summary>
+    /// The display name of this rule.
+    /// </summary>
+    public Optional<string> Name { get; }
+
+    /// <summary>
+    /// The snowflake identifier of the user who created this rule.
+    /// </summary>
+    public Optional<Snowflake> CreatorId { get; }
+
+    /// <summary>
+    /// The type of event that causes this rule to fire.
+    /// </summary>
+    public Optional<DiscordAutoModerationEventType> EventType { get; }
+
+    /// <summary>
+    /// The trigger type of this rule.
+    /// </summary>
+    public Optional<DiscordAutoModerationTriggerType> TriggerType { get; }
+
+    /// <summary>
+    /// Additional metadata for this rule trigger.
+    /// </summary>
+    public Optional<IAutoModerationTriggerMetadata> TriggerMetadata { get; }
+
+    /// <summary>
+    /// The actions which wille xecute when this rule is triggered.
+    /// </summary>
+    public Optional<IReadOnlyList<IAutoModerationAction>> Actions { get; }
+
+    /// <summary>
+    /// Indicates whether this rule should be enabled.
+    /// </summary>
+    public Optional<bool> Enabled { get; }
+
+    /// <summary>
+    /// Up to 20 role IDs that should be exempted from this rule.
+    /// </summary>
+    public Optional<IReadOnlyList<Snowflake>> ExemptRoles { get; }
+
+    /// <summary>
+    /// Up to 50 channel IDs that should be exempted from this rule.
+    /// </summary>
+    public Optional<IReadOnlyList<Snowflake>> ExemptChannels { get; }
+}
