a2633d9cee23f0d89c35a632123ae18263a9a360
akiraveliara <akiraveliara@outlook.com>
2023-07-01 14:30:28 +0200

    add partial integrations

--- src/core/DSharpPlus.Core.Abstractions.Models/Guilds/IIntegration.cs
+++ src/core/DSharpPlus.Core.Abstractions.Models/Guilds/IIntegration.cs
@@ -2,11 +2,6 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
-using System;
-using System.Collections.Generic;
-
-using DSharpPlus.Entities;
-
 using Remora.Rest.Core;
 
 namespace DSharpPlus.Core.Abstractions.Models;
@@ -14,86 +9,37 @@ namespace DSharpPlus.Core.Abstractions.Models;
 /// <summary>
 /// Represents an integration between an external service and a guild.
 /// </summary>
-public interface IIntegration
+public interface IIntegration : IPartialIntegration
 {
-    /// <summary>
-    /// The snowflake identifier of the integration.
-    /// </summary>
-    public Snowflake Id { get; }
+    /// <inheritdoc cref="IPartialIntegration.Id"/>
+    public new Snowflake Id { get; }
 
-    /// <summary>
-    /// The name of this integration.
-    /// </summary>
-    public string Name { get; }
+    /// <inheritdoc cref="IPartialIntegration.Name"/>
+    public new string Name { get; }
 
-    /// <summary>
-    /// The type of this integration; twitch, youtube, discord or guild_subscription.
-    /// </summary>
-    public string Type { get; }
+    /// <inheritdoc cref="IPartialIntegration.Type"/>
+    public new string Type { get; }
 
-    /// <summary>
-    /// Indicates whether this integration is enabled.
-    /// </summary>
-    public bool Enabled { get; }
+    /// <inheritdoc cref="IPartialIntegration.Enabled"/>
+    public new bool Enabled { get; }
 
-    /// <summary>
-    /// Indicates whether this integration is synchronizing.
-    /// </summary>
-    public Optional<bool> Syncing { get; }
+    /// <inheritdoc cref="IPartialIntegration.Account"/>
+    public new IIntegrationAccount Account { get; }
 
-    /// <summary>
-    /// The snowflake identifier of the role that this integration uses for "subscribers".
-    /// </summary>
-    public Optional<Snowflake> RoleId { get; }
+    // partial access routes
 
-    /// <summary>
-    /// Indicates whether emoticons should be synced for this integration. This is currently only
-    /// applicable to twitch.
-    /// </summary>
-    public Optional<bool> EnableEmoticons { get; }
+    /// <inheritdoc/>
+    Optional<Snowflake> IPartialIntegration.Id => this.Id;
 
-    /// <summary>
-    /// Indicates how this integration should behave when a subscription expires.
-    /// </summary>
-    public Optional<DiscordIntegrationExpirationBehaviour> ExpireBehaviour { get; }
+    /// <inheritdoc/>
+    Optional<string> IPartialIntegration.Name => this.Name;
 
-    /// <summary>
-    /// The grace period, in days, before expiring subscribers.
-    /// </summary>
-    public Optional<int> ExpireGracePeriod { get; }
+    /// <inheritdoc/>
+    Optional<string> IPartialIntegration.Type => this.Type;
 
-    /// <summary>
-    /// The user for this integration.
-    /// </summary>
-    public Optional<IUser> User { get; }
+    /// <inheritdoc/>
+    Optional<bool> IPartialIntegration.Enabled => this.Enabled;
 
-    /// <summary>
-    /// Contains additional integration account metadata.
-    /// </summary>
-    public IIntegrationAccount Account { get; }
-
-    /// <summary>
-    /// Indicates when this integration was last synced.
-    /// </summary>
-    public Optional<DateTimeOffset> SyncedAt { get; }
-
-    /// <summary>
-    /// The amount of subscribers this integration has.
-    /// </summary>
-    public Optional<int> SubscriberCount { get; }
-
-    /// <summary>
-    /// Indicates whether this integration has been revoked.
-    /// </summary>
-    public Optional<bool> Revoked { get; }
-
-    /// <summary>
-    /// The bot/oauth2 application for discord integrations.
-    /// </summary>
-    public Optional<IIntegrationApplication> Application { get; }
-
-    /// <summary>
-    /// The OAuth2 scopes this application has been authorized for.
-    /// </summary>
-    public Optional<IReadOnlyList<string>> Scopes { get; }
+    /// <inheritdoc/>
+    Optional<IIntegrationAccount> IPartialIntegration.Account => new(this.Account);
 }
