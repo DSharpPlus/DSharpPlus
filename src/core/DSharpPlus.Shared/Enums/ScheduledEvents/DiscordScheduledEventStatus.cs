8833b0c9db5ccc2e5303782597b6d542c76d64e9
akiraveliara <akiraveliara@outlook.com>
2023-07-01 14:06:31 +0200

    add scheduled events

---
+++ src/core/DSharpPlus.Shared/Enums/ScheduledEvents/DiscordScheduledEventStatus.cs
@@ -0,0 +1,20 @@
+// This Source Code form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+namespace DSharpPlus.Entities;
+
+/// <summary>
+/// Specifies the valid states of a scheduled event.
+/// </summary>
+/// <remarks>
+/// Once the status is set to <seealso cref="Completed"/> or <seealso cref="Canceled"/>, it can no longer
+/// be updated.
+/// </remarks>
+public enum DiscordScheduledEventStatus
+{
+    Scheduled = 1,
+    Active,
+    Completed,
+    Canceled
+}
