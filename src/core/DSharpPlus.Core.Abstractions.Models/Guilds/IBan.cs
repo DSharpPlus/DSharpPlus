1247df1362b8f5b23c26afb9f4391952b0cae5a5
akiraveliara <akiraveliara@outlook.com>
2023-07-02 11:08:04 +0200

    add ban

---
+++ src/core/DSharpPlus.Core.Abstractions.Models/Guilds/IBan.cs
@@ -0,0 +1,21 @@
+// This Source Code form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+namespace DSharpPlus.Core.Abstractions.Models;
+
+/// <summary>
+/// Represents information about a ban from a guild.
+/// </summary>
+public interface IBan
+{
+    /// <summary>
+    /// The ban reason.
+    /// </summary>
+    public string? Reason { get; }
+
+    /// <summary>
+    /// The banned user.
+    /// </summary>
+    public IUser User { get; }
+}
