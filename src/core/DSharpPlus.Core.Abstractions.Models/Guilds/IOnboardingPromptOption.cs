b11cab3b2fc34665e8fa6e68c0e601d00c39696e
akiraveliara <akiraveliara@outlook.com>
2023-07-03 15:35:50 +0200

    add onboarding

---
+++ src/core/DSharpPlus.Core.Abstractions.Models/Guilds/IOnboardingPromptOption.cs
@@ -0,0 +1,45 @@
+// This Source Code form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+using System.Collections.Generic;
+
+using Remora.Rest.Core;
+
+namespace DSharpPlus.Core.Abstractions.Models;
+
+/// <summary>
+/// Represents an option for a guild onboarding prompt.
+/// </summary>
+public interface IOnboardingPromptOption
+{
+    /// <summary>
+    /// The snowflake identifier of the prompt option.
+    /// </summary>
+    public Snowflake Id { get; }
+
+    /// <summary>
+    /// The snowflake identifiers for channels a member is added to when this option is selected.
+    /// </summary>
+    public IReadOnlyList<Snowflake> ChannelIds { get; }
+
+    /// <summary>
+    /// The snowflake identifiers for roles assigned to a member when this option is selected.
+    /// </summary>
+    public IReadOnlyList<Snowflake> RoleIds { get; }
+
+    /// <summary>
+    /// The emoji for this option.
+    /// </summary>
+    public IEmoji Emoji { get; }
+
+    /// <summary>
+    /// The title of this option.
+    /// </summary>
+    public string Title { get; }
+
+    /// <summary>
+    /// The description of this option.
+    /// </summary>
+    public string? Description { get; }
+}
---
+++ src/core/DSharpPlus.Shared/Entities/Guilds/DiscordGuildOnboardingPromptType.cs
@@ -0,0 +1,14 @@
+// This Source Code form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+namespace DSharpPlus.Entities;
+
+/// <summary>
+/// Represents the type of a guild onboarding prompt
+/// </summary>
+public enum DiscordGuildOnboardingPromptType
+{
+    MultipleChoice,
+    Dropdown
+}
