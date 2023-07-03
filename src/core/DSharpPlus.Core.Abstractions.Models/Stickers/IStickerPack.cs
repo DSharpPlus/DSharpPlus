7485a6d5ac4d0ef6fb3ab6b4f8832a38a39c6f90
akiraveliara <akiraveliara@outlook.com>
2023-07-03 16:24:19 +0200

    add sticker packs

---
+++ src/core/DSharpPlus.Core.Abstractions.Models/Stickers/IStickerPack.cs
@@ -0,0 +1,50 @@
+// This Source Code form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+using System.Collections.Generic;
+
+using Remora.Rest.Core;
+
+namespace DSharpPlus.Core.Abstractions.Models;
+
+/// <summary>
+/// Represents a pack of standard stickers.
+/// </summary>
+public interface IStickerPack
+{
+    /// <summary>
+    /// The snowflake identifier of this sticker pack.
+    /// </summary>
+    public Snowflake Id { get; }
+
+    /// <summary>
+    /// The stickers in this pack.
+    /// </summary>
+    public IReadOnlyList<ISticker> Stickers { get; }
+
+    /// <summary>
+    /// The name of this sticker pack.
+    /// </summary>
+    public string Name { get; }
+
+    /// <summary>
+    /// The snowflake identifier of the pack's SKU.
+    /// </summary>
+    public Snowflake SkuId { get; }
+
+    /// <summary>
+    /// The snowflake of the sticker shown as the pack's icon.
+    /// </summary>
+    public Optional<Snowflake> CoverStickerId { get; }
+
+    /// <summary>
+    /// The description of this sticker pack.
+    /// </summary>
+    public string Description { get; }
+
+    /// <summary>
+    /// The snowflake identifier of this pack's banner image.
+    /// </summary>
+    public Optional<Snowflake> BannerAssetId { get; }
+}
