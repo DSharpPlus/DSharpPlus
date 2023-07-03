7be5fbdbe4f4b143c21f5d62c80d7f050f46b67a
akiraveliara <akiraveliara@outlook.com>
2023-07-03 17:14:19 +0200

    add user connections

---
+++ src/core/DSharpPlus.Shared/Entities/Users/DiscordConnectionVisibility.cs
@@ -0,0 +1,21 @@
+// This Source Code form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+namespace DSharpPlus.Entities;
+
+/// <summary>
+/// Represents the visibility of a user profile connection.
+/// </summary>
+public enum DiscordConnectionVisibility
+{
+    /// <summary>
+    /// Visible to only the user themself.
+    /// </summary>
+    None,
+
+    /// <summary>
+    /// Visible to everyone.
+    /// </summary>
+    Everyone
+}
