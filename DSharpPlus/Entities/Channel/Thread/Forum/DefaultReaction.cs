8e5ce2e16c782a02543fb7571e0e2085b60a40f0
Jan Geburt <omnyseus@gmail.com>
2023-08-12 00:41:56 +0200

    Add static constructor to DefaultReaction

--- DSharpPlus/Entities/Channel/Thread/Forum/DefaultReaction.cs
+++ DSharpPlus/Entities/Channel/Thread/Forum/DefaultReaction.cs
@@ -40,5 +40,13 @@ namespace DSharpPlus.Entities
         /// </summary>
         [JsonProperty("emoji_name")]
         public string EmojiName { get; internal set; }
+        
+        /// <summary>
+        /// Creates a DefaultReaction object from an emoji.
+        /// </summary>
+        /// <param name="emoji">The <see cref="DiscordEmoji"/>.</param>
+        /// <returns>Create <see cref="DefaultReaction"/> object.</returns>
+        public static DefaultReaction FromEmoji(DiscordEmoji emoji)
+            => new DefaultReaction { EmojiId = emoji.Id, EmojiName = emoji.Name};
     }
 }
