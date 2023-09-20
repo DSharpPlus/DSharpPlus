f6665a509531d753370617a3a65cab24977fa47d
akiraveliara <akiraveliara@outlook.com>
2023-09-20 22:22:04 +0200

    expose reading strings

---
+++ src/etf/DSharpPlus.Etf/Serialization/EtfReader.Strings.cs
@@ -0,0 +1,74 @@
+// This Source Code form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
+
+using DSharpPlus.Etf.Extensions;
+
+namespace DSharpPlus.Etf.Serialization;
+
+partial struct EtfReader
+{
+    /// <summary>
+    /// Reads the current term as a string.
+    /// </summary>
+    /// <returns>True if reading was successful, false if unsuccessful.</returns>
+    public readonly bool TryReadString
+    (
+        [NotNullWhen(true)]
+        out string? value
+    )
+    {
+        if (this.TermType.IsString())
+        {
+            value = Encoding.Unicode.GetString(this.CurrentTermContents);
+            return true;
+        }
+
+        value = default;
+        return false;
+    }
+
+    /// <summary>
+    /// Copies the current term to a Span as UTF-8 string.
+    /// </summary>
+    /// <returns>True if successful, false if unsuccessful.</returns>
+    public readonly bool TryReadUtf8String
+    (
+        Span<byte> buffer
+    ) 
+        => this.TermType.IsString() && this.CurrentTermContents.TryCopyTo(buffer);
+
+    /// <summary>
+    /// Reads the current term as a string.
+    /// </summary>
+    public readonly string ReadString()
+    {
+        if (this.TryReadString(out string? value))
+        {
+            return value;
+        }
+
+        ThrowHelper.ThrowInvalidDecode(typeof(string));
+        return default!;
+    }
+
+    /// <summary>
+    /// Copies the current term to a Span as UTF-8 string.
+    /// </summary>
+    public readonly void ReadUtf8String
+    (
+        Span<byte> buffer
+    )
+    {
+        if (this.TryReadUtf8String(buffer))
+        {
+            return;
+        }
+
+        ThrowHelper.ThrowInvalidDecode(typeof(string));
+    }
+}
