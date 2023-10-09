addd2398b857da33fdeaa363b47634d86e6aa4a2
Plerx <50530305+Plerx2493@users.noreply.github.com>
2023-10-09 23:09:54 +0200

    use generic cache methods

--- DSharpPlus/Cache/IDiscordCache.cs
+++ DSharpPlus/Cache/IDiscordCache.cs
@@ -5,21 +5,9 @@ using Entities;
 
 public interface IDiscordCache
 {
-    ValueTask AddUser(DiscordUser user);
-    ValueTask AddChannel(DiscordChannel channel);
-    ValueTask AddGuild(DiscordGuild guild);
-    ValueTask AddMessage(DiscordMessage message);
-    ValueTask AddThread(DiscordThreadChannel thread);
+    ValueTask Add<T>(T entity);
     
-    ValueTask RemoveUser(ulong userId);
-    ValueTask RemoveChannel(ulong channelId);
-    ValueTask RemoveGuild(ulong guildId);
-    ValueTask RemoveMessage(ulong messageId);
-    ValueTask RemoveThread(ulong threadId);
+    ValueTask Remove(ICacheKey key);
     
-    ValueTask<bool> TryGetUser(ulong userId, out DiscordUser? user);
-    ValueTask<bool> TryGetChannel(ulong channelId, out DiscordChannel? channel);
-    ValueTask<bool> TryGetGuild(ulong guildId, out DiscordGuild? guild);
-    ValueTask<bool> TryGetMessage(ulong messageId, out DiscordMessage? message);
-    ValueTask<bool> TryGetThread(ulong threadId, out DiscordThreadChannel? thread);
+    ValueTask<bool> TryGet<T>(ICacheKey key, out T? entity);
 }
