45030ade4b9ca8c3535774732b332195c16c24a1
Lunar Starstrum <lunar@forsaken-borders.net>
2023-11-03 16:19:39 -0500

    Remove HTTP Interactions until v6

--- src/Processors/HttpCommands/HttpCommandConfiguration.cs
+++
@@ -1,8 +0,0 @@
-namespace DSharpPlus.CommandAll.Processors.HttpCommands
-{
-    public record HttpCommandConfiguration
-    {
-        public required byte[] PublicKey { get; init; } = new byte[32];
-        public required string[] Prefixes { get; init; } = ["http://0.0.0.0/", "http://0.0.0.0:8080/"];
-    }
-}
