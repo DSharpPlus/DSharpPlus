77ef8005d92dbaa8a8b328d1bb15cb0df8b7191a
Lunar Starstrum <lunar@forsaken-borders.net>
2023-11-06 16:14:11 -0600

    Primary constructors do not, in fact, generate properties

--- src/Exceptions/ParseArgumentException.cs
+++ src/Exceptions/ParseArgumentException.cs
@@ -3,5 +3,14 @@ using DSharpPlus.CommandAll.Commands;
 
 namespace DSharpPlus.CommandAll.Exceptions
 {
-    public sealed class ParseArgumentException(CommandArgument Argument, Exception? innerException = null, string? message = null) : Exception(message ?? $"Failed to parse {Argument.Name}.", innerException);
+    public sealed class ParseArgumentException : Exception
+    {
+        public CommandArgument Argument { get; init; }
+
+        public ParseArgumentException(CommandArgument argument, Exception? innerException = null, string? message = null) : base(message ?? $"Failed to parse {argument.Name}.", innerException)
+        {
+            ArgumentNullException.ThrowIfNull(argument, nameof(argument));
+            Argument = argument;
+        }
+    }
 }
