a39a7e8223472f46f7ca75eae96b0af862b68cd1
Lunar Starstrum <lunar@forsaken-borders.net>
2023-11-12 12:54:59 -0600

    Fix null check

--- src/ContextChecks/RequireOwnerAttribute.cs
+++ src/ContextChecks/RequireOwnerAttribute.cs
@@ -8,6 +8,6 @@ namespace DSharpPlus.CommandAll.ContextChecks
     [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method | AttributeTargets.Delegate)]
     public class RequireOwnerAttribute : ContextCheckAttribute
     {
-        public override Task<bool> ExecuteCheckAsync(CommandContext context) => Task.FromResult(context.Client.CurrentApplication.Owners.Contains(context.User) || context.User.Id == context.Client.CurrentUser.Id);
+        public override Task<bool> ExecuteCheckAsync(CommandContext context) => Task.FromResult(context.Client.CurrentApplication.Owners?.Contains(context.User) ?? context.User.Id == context.Client.CurrentUser.Id);
     }
 }
