d2d21732f032cb5e750526a5159b700729a8912d
Lunar Starstrum <lunar@forsaken-borders.net>
2023-11-13 16:05:53 -0600

    Semicolon

--- DSharpPlus.Commands/Commands/Attributes/AllowDMUsageAttribute.cs
+++ DSharpPlus.Commands/Commands/Attributes/AllowDMUsageAttribute.cs
@@ -3,4 +3,4 @@ namespace DSharpPlus.Commands.Commands.Attributes;
 using System;
 
 [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
-public sealed class AllowDMUsageAttribute : Attribute { }
+public sealed class AllowDMUsageAttribute : Attribute;
