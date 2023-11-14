411a7fa50a2adbbaf62f9b191ce1a3adc697e4b5
akiraveliara <akiraveliara@outlook.com>
2023-11-14 22:23:29 +0100

    create interaction responses

---
+++ src/core/DSharpPlus.Core.Abstractions.Rest/API/IInteractionRestAPI.cs
@@ -0,0 +1,35 @@
+// This Source Code form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+using System.Threading;
+using System.Threading.Tasks;
+
+using DSharpPlus.Core.Abstractions.Models;
+
+using Remora.Results;
+
+namespace DSharpPlus.Core.Abstractions.Rest.API;
+
+/// <summary>
+/// Provides access to interaction-related rest API calls.
+/// </summary>
+public interface IInteractionRestAPI
+{
+    /// <summary>
+    /// Creates a response to an interaction from the gateway.
+    /// </summary>
+    /// <param name="interactionId">The snowflake identifier of the interaction.</param>
+    /// <param name="interactionToken">The interaction token received with the interaction.</param>
+    /// <param name="payload">The response to this interaction.</param>
+    /// <param name="info">Additional instructions regarding this request.</param>
+    /// <param name="ct">A cancellation token for this operation.</param>
+    public ValueTask<Result> CreateInteractionResponseAsync
+    (
+        Snowflake interactionId,
+        string interactionToken,
+        IInteractionResponse payload,
+        RequestInfo info = default,
+        CancellationToken ct = default
+    );
+}
