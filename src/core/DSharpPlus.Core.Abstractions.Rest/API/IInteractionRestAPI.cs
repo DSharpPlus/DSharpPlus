01da6deea0c1b39b9c327744b4a8254ac07703c7
akiraveliara <akiraveliara@outlook.com>
2023-11-16 12:04:46 +0100

    delete interaction responses

--- src/core/DSharpPlus.Core.Abstractions.Rest/API/IInteractionRestAPI.cs
+++ src/core/DSharpPlus.Core.Abstractions.Rest/API/IInteractionRestAPI.cs
@@ -66,4 +66,19 @@ public interface IInteractionRestAPI
         RequestInfo info = default,
         CancellationToken ct = default
     );
+
+    /// <summary>
+    /// Deletes the original response to this interaction, if it was a message.
+    /// </summary>
+    /// <param name="applicationId">The snowflake identifier of your application.</param>
+    /// <param name="interactionToken">The interaction token received with the interaction.</param>
+    /// <param name="info">Additional instructions regarding this request.</param>
+    /// <param name="ct">A cancellation token for this operation.</param>
+    public ValueTask<Result> DeleteInteractionResponseAsync
+    (
+        Snowflake applicationId,
+        string interactionToken,
+        RequestInfo info = default,
+        CancellationToken ct = default
+    );
 }
