329a62758c9c9914cabef8e1067902b0db3e2cb5
Lunar Starstrum <lunar@forsaken-borders.net>
2023-11-21 12:06:26 -0600

    MethodInfo is nullable on group commands

--- DSharpPlus.Commands/Trees/Command.cs
+++ DSharpPlus.Commands/Trees/Command.cs
@@ -9,7 +9,7 @@ public record Command
 {
     public required string Name { get; init; }
     public string? Description { get; init; }
-    public required MethodInfo Method { get; init; }
+    public required MethodInfo? Method { get; init; }
     public required Ulid Id { get; init; }
     public object? Target { get; init; }
     public Command? Parent { get; init; }
