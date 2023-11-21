bdf6d392934cef4a83c76e5e8a169f8c7474b72d
Lunar Starstrum <lunar@forsaken-borders.net>
2023-11-21 12:15:57 -0600

    Group all exceptions under CommandsException for user convenience

--- DSharpPlus.Commands/Exceptions/ChecksFailedException.cs
+++ DSharpPlus.Commands/Exceptions/ChecksFailedException.cs
@@ -3,16 +3,8 @@ namespace DSharpPlus.Commands.Exceptions;
 using System;
 using System.Collections.Generic;
 
-public sealed class ChecksFailedException
-(
-    IReadOnlyList<Exception> errors,
-    string commandName,
-    string? message = null
-) 
-: Exception
-(
-    message ?? $"Checks for {commandName} failed."
-)
+public sealed class ChecksFailedException(IReadOnlyList<Exception> errors, string commandName, string? message = null)
+    : CommandsException(message ?? $"Checks for {commandName} failed.")
 {
     public IReadOnlyList<Exception> Check { get; init; } = errors;
 }
