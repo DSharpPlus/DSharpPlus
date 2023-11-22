b46ce78cc8b31be1deaf3bf07dc5ffcfa477a2c6
akiraveliara <akiraveliara@outlook.com>
2023-11-22 12:31:55 +0100

    image data json converter
    this sucks, but... oh well.

---
+++ src/core/DSharpPlus.Shared/Serialization/ImageDataConverter.cs
@@ -0,0 +1,33 @@
+// This Source Code form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+using System;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+using CommunityToolkit.HighPerformance.Buffers;
+
+namespace DSharpPlus.Serialization;
+
+public sealed class ImageDataConverter : JsonConverter<ImageData>
+{
+    /// <summary>
+    /// Deserializing image data is unsupported.
+    /// </summary>
+    public override ImageData Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options) => throw new NotSupportedException();
+
+    public override void Write
+    (
+        Utf8JsonWriter writer, 
+        ImageData value, 
+        JsonSerializerOptions options
+    )
+    {
+        using ArrayPoolBufferWriter<byte> bufferWriter = new(65536);
+
+        value.WriteToAsync(bufferWriter).AsTask().GetAwaiter().GetResult();
+
+        writer.WriteStringValue(bufferWriter.WrittenSpan);
+    }
+}
