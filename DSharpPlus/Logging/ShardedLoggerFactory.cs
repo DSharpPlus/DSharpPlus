0ca45d3a9e5d266b533ec81636ccb6e63bc8a53a
Plerx <50530305+Plerx2493@users.noreply.github.com>
2023-12-23 14:30:33 +0100

    Remove restriction from ShardedLoggerFactory

--- DSharpPlus/Logging/ShardedLoggerFactory.cs
+++ DSharpPlus/Logging/ShardedLoggerFactory.cs
@@ -3,20 +3,18 @@ using Microsoft.Extensions.Logging;
 
 namespace DSharpPlus;
 
+using System.Collections.Concurrent;
+
 internal class ShardedLoggerFactory : ILoggerFactory
 {
-    private ILogger<BaseDiscordClient> Logger { get; }
+    private ConcurrentDictionary<string, ILogger> Loggers { get; } = new();
+    private ILoggerFactory Factory { get; }
 
-    public ShardedLoggerFactory(ILogger<BaseDiscordClient> instance) => this.Logger = instance;
+    public ShardedLoggerFactory(ILoggerFactory factory) => this.Factory = factory;
 
     public void AddProvider(ILoggerProvider provider) => throw new InvalidOperationException("This is a passthrough logger container, it cannot register new providers.");
 
-    public ILogger CreateLogger(string categoryName)
-    {
-        return categoryName != typeof(BaseDiscordClient).FullName
-            ? throw new ArgumentException($"This factory can only provide instances of loggers for {typeof(BaseDiscordClient).FullName}.", nameof(categoryName))
-            : this.Logger;
-    }
+    public ILogger CreateLogger(string categoryName) => this.Loggers.GetOrAdd(categoryName, this.Factory.CreateLogger(categoryName));
 
     public void Dispose()
     { }
