f58f00f1638892976d15d9e30285e3bed4bea2ad
akiraveliara <akiraveliara@outlook.com>
2023-12-24 14:48:31 +0100

    guild template api too for good measure

---
+++ src/core/DSharpPlus.Core.Abstractions.Rest/Payloads/GuildTemplates/IModifyGuildTemplatePayload.cs
@@ -0,0 +1,21 @@
+// This Source Code form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+namespace DSharpPlus.Core.Abstractions.Rest.Payloads;
+
+/// <summary>
+/// Represents a payload to <c>POST /guilds/:guild-id/templates</c>
+/// </summary>
+public interface IModifyGuildTemplatePayload
+{
+    /// <summary>
+    /// The name of this template, up to 100 characters.
+    /// </summary>
+    public Optional<string> Name { get; }
+
+    /// <summary>
+    /// The description of this template, up to 120 characters.
+    /// </summary>
+    public Optional<string?> Description { get; }
+}
