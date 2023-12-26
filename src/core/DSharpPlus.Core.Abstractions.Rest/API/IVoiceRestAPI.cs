f07c71a4394b5164484823ad86422def2337072d
akiraveliara <akiraveliara@outlook.com>
2023-12-26 20:39:42 +0100

    a voice api too

---
+++ src/core/DSharpPlus.Core.Abstractions.Rest/API/IVoiceRestAPI.cs
@@ -0,0 +1,30 @@
+// This Source Code form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+using DSharpPlus.Core.Abstractions.Models;
+
+using Remora.Results;
+
+namespace DSharpPlus.Core.Abstractions.Rest.API;
+
+/// <summary>
+/// Provides access to voice-related API calls.
+/// </summary>
+public interface IVoiceRestAPI
+{
+    /// <summary>
+    /// Returns an array of voice region objects that can be used when setting a voice or stage channel's rtc region.
+    /// </summary>
+    /// <param name="info">Additional instructions regarding this request.</param>
+    /// <param name="ct">A cancellation token for this operation.</param>
+    public ValueTask<Result<IReadOnlyList<IVoiceRegion>>> ListVoiceRegionsAsync
+    (
+        RequestInfo info = default,
+        CancellationToken ct = default
+    );
+}
