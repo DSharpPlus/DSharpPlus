78e1bff8cd0a0e0f4d353c617a061392dff50168
akiraveliara <akiraveliara@outlook.com>
2023-12-29 21:08:10 +0100

    wrap entitlement query parameters

--- src/core/DSharpPlus.Core.Abstractions.Rest/API/IEntitlementsRestAPI.cs
+++ src/core/DSharpPlus.Core.Abstractions.Rest/API/IEntitlementsRestAPI.cs
@@ -8,7 +8,7 @@ using System.Threading.Tasks;
 
 using DSharpPlus.Core.Abstractions.Models;
 using DSharpPlus.Core.Abstractions.Rest.Payloads;
-
+using DSharpPlus.Core.Abstractions.Rest.Queries;
 using Remora.Results;
 
 namespace DSharpPlus.Core.Abstractions.Rest.API;
@@ -22,25 +22,13 @@ public interface IEntitlementsRestAPI
     /// Returns all entitlements for a given app, according to the query parameters.
     /// </summary>
     /// <param name="applicationId">The snowflake identifier of the current application.</param>
-    /// <param name="userId">The snowflake identifier of the user to look up entitlements for.</param>
-    /// <param name="skuIds">A comma-delimited set of snowflakes of SKUs to check entitlements for.</param>
-    /// <param name="before">Specifies an entitlement to retrieve entitlements before of, for pagination.</param>
-    /// <param name="after">Specifies an entitlement to retrieve entitlements after of, for pagination.</param>
-    /// <param name="limit">The maximum amount of entitlements to return, up to 100.</param>
-    /// <param name="guildId">The snowflake identifier of the guild to look up entitlements for.</param>
-    /// <param name="excludeEnded">Specifies whether or not to include ended entitlements.</param>
+    /// <param name="query">Contains filtering and pagination options for this request.</param>
     /// <param name="info">Additional instructions regarding this request.</param>
     /// <param name="ct">A cancellation token for this operation.</param>
     public ValueTask<Result<IReadOnlyList<IEntitlement>>> ListEntitlementsAsync
     (
         Snowflake applicationId,
-        Snowflake? userId = null,
-        string? skuIds = null,
-        Snowflake? before = null,
-        Snowflake? after = null,
-        int? limit = null,
-        Snowflake? guildId = null,
-        bool? excludeEnded = null,
+        ListEntitlementsQuery query = default,
         RequestInfo info = default,
         CancellationToken ct = default
     );
