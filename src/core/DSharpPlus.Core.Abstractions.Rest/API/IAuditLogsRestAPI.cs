c3325b3c3e4f27127116ea672f69b6c5e53ddae8
akiraveliara <akiraveliara@outlook.com>
2023-12-29 19:03:46 +0100

    wrap audit log queries

--- src/core/DSharpPlus.Core.Abstractions.Rest/API/IAuditLogsRestAPI.cs
+++ src/core/DSharpPlus.Core.Abstractions.Rest/API/IAuditLogsRestAPI.cs
@@ -6,7 +6,7 @@ using System.Threading;
 using System.Threading.Tasks;
 
 using DSharpPlus.Core.Abstractions.Models;
-using DSharpPlus.Entities;
+using DSharpPlus.Core.Abstractions.Rest.Queries;
 
 using Remora.Results;
 
@@ -21,22 +21,17 @@ public interface IAuditLogsRestAPI
     /// Lists audit log entries for a given guild.
     /// </summary>
     /// <param name="guildId">The snowflake identifier of the guild to list audit logs for.</param>
-    /// <param name="userId">If specified, only lists entries from this user ID.</param>
-    /// <param name="actionType">If specified, only lists entries of this audit log event type.</param>
-    /// <param name="before">If specified, only lists entries with an ID less than this ID.</param>
-    /// <param name="after">If specified, only lists entries with an ID greater than this ID.</param>
-    /// <param name="limit">The maximum number of entries, between 1 and 100.</param>
+    /// <param name="query">
+    /// Contains the query parameters for this request, comprised of request pagination and log entry
+    /// filtering options.
+    /// </param>
     /// <param name="info">Additional instructions regarding this request.</param>
     /// <param name="ct">A cancellation token for this operation.</param>
     /// <returns>An audit log object, containing all relevant other entities and the main audit log.</returns>
     public ValueTask<Result<IAuditLog>> ListGuildAuditLogEntriesAsync
     (
         Snowflake guildId,
-        Snowflake? userId = null,
-        DiscordAuditLogEvent? actionType = null,
-        Snowflake? before = null,
-        Snowflake? after = null,
-        int? limit = null,
+        ListGuildAuditLogEntriesQuery query = default,
         RequestInfo info = default,
         CancellationToken ct = default
     );
