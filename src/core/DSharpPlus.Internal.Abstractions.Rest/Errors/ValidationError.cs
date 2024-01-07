7266c42cb365d7da82faaac394579858471715a7
akiraveliara <akiraveliara@outlook.com>
2024-01-07 19:41:44 +0100

    slim ValidationError

--- src/core/DSharpPlus.Internal.Abstractions.Rest/Errors/ValidationError.cs
+++ src/core/DSharpPlus.Internal.Abstractions.Rest/Errors/ValidationError.cs
@@ -2,8 +2,6 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
-using System.Collections.Generic;
-
 using Remora.Results;
 
 namespace DSharpPlus.Internal.Abstractions.Rest.Errors;
@@ -11,19 +9,5 @@ namespace DSharpPlus.Internal.Abstractions.Rest.Errors;
 /// <summary>
 /// Represents an error encountered during parameter validation.
 /// </summary>
-public record ValidationError : ResultError
-{
-    /// <summary>
-    /// If this error was caused by any specific parameters, contains the names of the invalid parameters.
-    /// </summary>
-    public IReadOnlyList<string>? ParameterNames { get; init; }
-
-    /// <summary>
-    /// Initializes a new validation error.
-    /// </summary>
-    /// <param name="message">The human-readable error message.</param>
-    /// <param name="parameters">If applicable, the names of parameters that failed validation.</param>
-    public ValidationError(string message, params string[] parameters)
-        : base(message) 
-        => this.ParameterNames = parameters.Length != 0 ? parameters : null;
-}
+/// <param name="message">The human-readable error message.</param>
+public record ValidationError(string message) : ResultError(message);
