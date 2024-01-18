47e75784f355799f522df17477ba5285c859e24e
Plerx <50530305+Plerx2493@users.noreply.github.com>
2024-01-19 00:04:02 +0100

    remove old now unnecessary check

--- DSharpPlus/Net/Rest/DiscordApiClient.cs
+++ DSharpPlus/Net/Rest/DiscordApiClient.cs
@@ -6041,12 +6041,7 @@ public sealed class DiscordApiClient
     internal async ValueTask<GatewayInfo> GetGatewayInfoAsync()
     {
         Dictionary<string, string> headers = new();
-        string route = Endpoints.GATEWAY + "/";
-        if (this._discord!.Configuration.TokenType == TokenType.Bot)
-        {
-            route += Endpoints.BOT;
-        }
-
+        string route = Endpoints.GATEWAY + "/" + Endpoints.BOT;
         string url = route;
 
         RestRequest request = new()
