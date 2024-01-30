aabd8ffb8cf697df54721252f224703577ef2878
InFTord <inftord@inftord.tech>
2024-01-30 22:28:56 +0500

    add xmldocs to eventargs

--- DSharpPlus.Commands/EventArgs/CommandErroredEventArgs.cs
+++ DSharpPlus.Commands/EventArgs/CommandErroredEventArgs.cs
@@ -4,6 +4,9 @@ using System;
 using DSharpPlus.AsyncEvents;
 using DSharpPlus.Commands.Trees;
 
+/// <summary>
+/// Represents arguments for <see cref="CommandsExtension.CommandErrored"/> event.
+/// </summary>
 public sealed class CommandErroredEventArgs : AsyncEventArgs
 {
     public required CommandContext Context { get; init; }
