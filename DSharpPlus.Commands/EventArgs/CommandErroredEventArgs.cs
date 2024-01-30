da062a29acdaddd000a03ba096705fa238c2afec
InFTord <inftord@inftord.tech>
2024-01-30 22:31:24 +0500

    add docs to CommandErroredEventArgs.cs, which i forgot to add

--- DSharpPlus.Commands/EventArgs/CommandErroredEventArgs.cs
+++ DSharpPlus.Commands/EventArgs/CommandErroredEventArgs.cs
@@ -9,7 +9,16 @@ using DSharpPlus.Commands.Trees;
 /// </summary>
 public sealed class CommandErroredEventArgs : AsyncEventArgs
 {
+    /// <summary>
+    /// Gets the context in which the command errored.
+    /// </summary>
     public required CommandContext Context { get; init; }
+    /// <summary>
+    /// Gets the exception that got thrown.
+    /// </summary>
     public required Exception Exception { get; init; }
+    /// <summary>
+    /// Gets the command object that errored.
+    /// </summary>
     public required object? CommandObject { get; init; }
 }
