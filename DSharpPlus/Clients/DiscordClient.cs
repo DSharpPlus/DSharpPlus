2ac74be0b394e57c0faab40e25f73ad44528a8b2
Plerx <50530305+Plerx2493@users.noreply.github.com>
2024-02-01 12:38:59 +0100

    formatting

--- DSharpPlus/Clients/DiscordClient.cs
+++ DSharpPlus/Clients/DiscordClient.cs
@@ -74,7 +74,8 @@ public sealed partial class DiscordClient : BaseDiscordClient
     /// <summary>
     /// Gets whether this client is connected to the gateway.
     /// </summary>
-    public bool IsConnected => this.ConnectionLock.IsSet;
+    public bool IsConnected 
+        => this.ConnectionLock.IsSet;
 
     /// <summary>
     /// Gets a dictionary of DM channels that have been cached by this client. The dictionary's key is the channel
