4ecd24430ddcea119d9d52134eb4b7325b7937bd
Livia Silvercrown <61018569+akiraveliara@users.noreply.github.com>
2024-02-17 14:45:13 +0100

    kills an allocation in parsing the response for good measure (#1773)

---
+++ DSharpPlus/Net/Rest/RateLimitCandidateBucket.cs
@@ -0,0 +1,12 @@
+namespace DSharpPlus.Net;
+
+using System;
+
+/// <summary>
+/// A value-type variant of <seealso cref="RateLimitBucket"/> for extraction, in case we don't need the object.
+/// </summary>
+internal readonly record struct RateLimitCandidateBucket(int Maximum, int Remaining, DateTime Reset)
+{
+    public RateLimitBucket ToFullBucket() 
+        => new(this.Maximum, this.Remaining, this.Reset);
+}
