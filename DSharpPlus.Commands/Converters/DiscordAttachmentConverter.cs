47d18870c705828b8f6500b939dbccaffd839e7d
Lunar Starstrum <lunar@forsaken-borders.net>
2024-02-26 10:24:21 -0600

    Allow for attachments to be grabbed from replies

--- DSharpPlus.Commands/Converters/DiscordAttachmentConverter.cs
+++ DSharpPlus.Commands/Converters/DiscordAttachmentConverter.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using DSharpPlus.Commands.Processors.SlashCommands;
 using DSharpPlus.Commands.Processors.TextCommands;
+using DSharpPlus.Commands.Processors.TextCommands.ContextChecks;
 using DSharpPlus.Entities;
 using DSharpPlus.EventArgs;
 
@@ -14,10 +15,24 @@ public class AttachmentConverter : ISlashArgumentConverter<DiscordAttachment>, I
 
     public Task<Optional<DiscordAttachment>> ConvertAsync(ConverterContext context, MessageCreateEventArgs eventArgs)
     {
+        DiscordMessage message = eventArgs.Message;
         int currentAttachmentArgumentIndex = context.Command.Parameters.Where(argument => argument.Type == typeof(DiscordAttachment)).IndexOf(context.Parameter);
-        return eventArgs.Message.Attachments.Count <= currentAttachmentArgumentIndex
-            ? Task.FromResult(Optional.FromNoValue<DiscordAttachment>()) // Too many parameters, not enough attachments
-            : Task.FromResult(Optional.FromValue(eventArgs.Message.Attachments[currentAttachmentArgumentIndex]));
+        if (context.Parameter.Attributes.FirstOrDefault(x => x is TextMessageReplyAttribute) is TextMessageReplyAttribute textMessageReplyAttribute)
+        {
+            if (eventArgs.Message.ReferencedMessage is not null)
+            {
+                message = eventArgs.Message.ReferencedMessage;
+            }
+            else if (textMessageReplyAttribute.RequireReplies)
+            {
+                // No referenced message, but the attribute requires it
+                return Task.FromResult(Optional.FromNoValue<DiscordAttachment>());
+            }
+        }
+
+        return message.Attachments.Count <= currentAttachmentArgumentIndex
+            ? Task.FromResult(Optional.FromNoValue<DiscordAttachment>())
+            : Task.FromResult(Optional.FromValue(message.Attachments[currentAttachmentArgumentIndex]));
     }
 
     public Task<Optional<DiscordAttachment>> ConvertAsync(ConverterContext context, InteractionCreateEventArgs eventArgs)
