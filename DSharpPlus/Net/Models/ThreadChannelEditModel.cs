b3815e599410cfa7ee9ed3fd0f445ca944e26f62
Lunar Starstrum <lunar@forsaken-borders.net>
2024-05-01 16:26:47 -0500

    CS0108: 'ThreadChannelEditModel.Flags' hides inherited member 'ChannelEditModel.Flags'. Use the new keyword if hiding was intended.

--- DSharpPlus/Net/Models/ThreadChannelEditModel.cs
+++ DSharpPlus/Net/Models/ThreadChannelEditModel.cs
@@ -29,7 +29,7 @@ public class ThreadChannelEditModel : ChannelEditModel
     /// <summary>
     /// Sets the flags for the channel (Either PINNED or REQUIRE_TAG)
     /// </summary>
-    public DiscordChannelFlags? Flags { internal get; set; }
+    public new DiscordChannelFlags? Flags { internal get; set; }
 
     /// <summary>
     /// Sets whether non-moderators can add other non-moderators to a thread. Only available on private threads
