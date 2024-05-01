537c72e2e69620c2ff63d33a21c60fc8c1bec732
Lunar Starstrum <lunar@forsaken-borders.net>
2024-05-01 16:19:02 -0500

    Split different namespaces/class into separate files

--- DSharpPlus.Interactivity/EventHandling/Requests/PaginationRequest.cs
+++ DSharpPlus.Interactivity/EventHandling/Requests/PaginationRequest.cs
@@ -1,3 +1,5 @@
+namespace DSharpPlus.Interactivity.EventHandling;
+
 using System;
 using System.Collections.Generic;
 using System.Threading;
@@ -5,227 +7,191 @@ using System.Threading.Tasks;
 using DSharpPlus.Entities;
 using DSharpPlus.Interactivity.Enums;
 
-namespace DSharpPlus.Interactivity.EventHandling
+internal class PaginationRequest : IPaginationRequest
 {
-    internal class PaginationRequest : IPaginationRequest
+    private TaskCompletionSource<bool> _tcs;
+    private readonly CancellationTokenSource _ct;
+    private readonly TimeSpan _timeout;
+    private readonly List<Page> _pages;
+    private readonly PaginationBehaviour _behaviour;
+    private readonly DiscordMessage _message;
+    private readonly PaginationEmojis _emojis;
+    private readonly DiscordUser _user;
+    private int _index = 0;
+
+    /// <summary>
+    /// Creates a new Pagination request
+    /// </summary>
+    /// <param name="message">Message to paginate</param>
+    /// <param name="user">User to allow control for</param>
+    /// <param name="behaviour">Behaviour during pagination</param>
+    /// <param name="deletion">Behavior on pagination end</param>
+    /// <param name="emojis">Emojis for this pagination object</param>
+    /// <param name="timeout">Timeout time</param>
+    /// <param name="pages">Pagination pages</param>
+    internal PaginationRequest(DiscordMessage message, DiscordUser user, PaginationBehaviour behaviour, PaginationDeletion deletion,
+        PaginationEmojis emojis, TimeSpan timeout, params Page[] pages)
     {
-        private TaskCompletionSource<bool> _tcs;
-        private readonly CancellationTokenSource _ct;
-        private readonly TimeSpan _timeout;
-        private readonly List<Page> _pages;
-        private readonly PaginationBehaviour _behaviour;
-        private readonly DiscordMessage _message;
-        private readonly PaginationEmojis _emojis;
-        private readonly DiscordUser _user;
-        private int _index = 0;
+        _tcs = new();
+        _ct = new(timeout);
+        _ct.Token.Register(() => _tcs.TrySetResult(true));
+        _timeout = timeout;
 
-        /// <summary>
-        /// Creates a new Pagination request
-        /// </summary>
-        /// <param name="message">Message to paginate</param>
-        /// <param name="user">User to allow control for</param>
-        /// <param name="behaviour">Behaviour during pagination</param>
-        /// <param name="deletion">Behavior on pagination end</param>
-        /// <param name="emojis">Emojis for this pagination object</param>
-        /// <param name="timeout">Timeout time</param>
-        /// <param name="pages">Pagination pages</param>
-        internal PaginationRequest(DiscordMessage message, DiscordUser user, PaginationBehaviour behaviour, PaginationDeletion deletion,
-            PaginationEmojis emojis, TimeSpan timeout, params Page[] pages)
-        {
-            _tcs = new();
-            _ct = new(timeout);
-            _ct.Token.Register(() => _tcs.TrySetResult(true));
-            _timeout = timeout;
+        _message = message;
+        _user = user;
 
-            _message = message;
-            _user = user;
+        PaginationDeletion = deletion;
+        _behaviour = behaviour;
+        _emojis = emojis;
 
-            PaginationDeletion = deletion;
-            _behaviour = behaviour;
-            _emojis = emojis;
-
-            _pages = [.. pages];
-        }
-
-        public int PageCount => _pages.Count;
-
-        public PaginationDeletion PaginationDeletion { get; }
-
-        public async Task<Page> GetPageAsync()
-        {
-            await Task.Yield();
-
-            return _pages[_index];
-        }
-
-        public async Task SkipLeftAsync()
-        {
-            await Task.Yield();
-
-            _index = 0;
-        }
-
-        public async Task SkipRightAsync()
-        {
-            await Task.Yield();
-
-            _index = _pages.Count - 1;
-        }
-
-        public async Task NextPageAsync()
-        {
-            await Task.Yield();
-
-            switch (_behaviour)
-            {
-                case PaginationBehaviour.Ignore:
-                    if (_index == _pages.Count - 1)
-                    {
-                        break;
-                    }
-                    else
-                    {
-                        _index++;
-                    }
-
-                    break;
-
-                case PaginationBehaviour.WrapAround:
-                    if (_index == _pages.Count - 1)
-                    {
-                        _index = 0;
-                    }
-                    else
-                    {
-                        _index++;
-                    }
-
-                    break;
-            }
-        }
-
-        public async Task PreviousPageAsync()
-        {
-            await Task.Yield();
-
-            switch (_behaviour)
-            {
-                case PaginationBehaviour.Ignore:
-                    if (_index == 0)
-                    {
-                        break;
-                    }
-                    else
-                    {
-                        _index--;
-                    }
-
-                    break;
-
-                case PaginationBehaviour.WrapAround:
-                    if (_index == 0)
-                    {
-                        _index = _pages.Count - 1;
-                    }
-                    else
-                    {
-                        _index--;
-                    }
-
-                    break;
-            }
-        }
-
-        public async Task<PaginationEmojis> GetEmojisAsync()
-        {
-            await Task.Yield();
-
-            return _emojis;
-        }
-
-        public Task<IEnumerable<DiscordButtonComponent>> GetButtonsAsync()
-            => throw new NotSupportedException("This request does not support buttons.");
-
-        public async Task<DiscordMessage> GetMessageAsync()
-        {
-            await Task.Yield();
-
-            return _message;
-        }
-
-        public async Task<DiscordUser> GetUserAsync()
-        {
-            await Task.Yield();
-
-            return _user;
-        }
-
-        public async Task DoCleanupAsync()
-        {
-            switch (PaginationDeletion)
-            {
-                case PaginationDeletion.DeleteEmojis:
-                    await _message.DeleteAllReactionsAsync();
-                    break;
-
-                case PaginationDeletion.DeleteMessage:
-                    await _message.DeleteAsync();
-                    break;
-
-                case PaginationDeletion.KeepEmojis:
-                    break;
-            }
-        }
-
-        public async Task<TaskCompletionSource<bool>> GetTaskCompletionSourceAsync()
-        {
-            await Task.Yield();
-
-            return _tcs;
-        }
-
-        /// <summary>
-        /// Disposes this PaginationRequest.
-        /// </summary>
-        public void Dispose()
-        {
-            // Why doesn't this class implement IDisposable?
-
-            _ct?.Dispose();
-            _tcs = null!;
-        }
+        _pages = [.. pages];
     }
-}
 
-namespace DSharpPlus.Interactivity
-{
-    public class PaginationEmojis
+    public int PageCount => _pages.Count;
+
+    public PaginationDeletion PaginationDeletion { get; }
+
+    public async Task<Page> GetPageAsync()
     {
-        public DiscordEmoji SkipLeft;
-        public DiscordEmoji SkipRight;
-        public DiscordEmoji Left;
-        public DiscordEmoji Right;
-        public DiscordEmoji Stop;
+        await Task.Yield();
 
-        public PaginationEmojis()
+        return _pages[_index];
+    }
+
+    public async Task SkipLeftAsync()
+    {
+        await Task.Yield();
+
+        _index = 0;
+    }
+
+    public async Task SkipRightAsync()
+    {
+        await Task.Yield();
+
+        _index = _pages.Count - 1;
+    }
+
+    public async Task NextPageAsync()
+    {
+        await Task.Yield();
+
+        switch (_behaviour)
         {
-            Left = DiscordEmoji.FromUnicode("◀");
-            Right = DiscordEmoji.FromUnicode("▶");
-            SkipLeft = DiscordEmoji.FromUnicode("⏮");
-            SkipRight = DiscordEmoji.FromUnicode("⏭");
-            Stop = DiscordEmoji.FromUnicode("⏹");
+            case PaginationBehaviour.Ignore:
+                if (_index == _pages.Count - 1)
+                {
+                    break;
+                }
+                else
+                {
+                    _index++;
+                }
+
+                break;
+
+            case PaginationBehaviour.WrapAround:
+                if (_index == _pages.Count - 1)
+                {
+                    _index = 0;
+                }
+                else
+                {
+                    _index++;
+                }
+
+                break;
         }
     }
 
-    public class Page
+    public async Task PreviousPageAsync()
     {
-        public string Content { get; set; }
-        public DiscordEmbed Embed { get; set; }
+        await Task.Yield();
 
-        public Page(string content = "", DiscordEmbedBuilder embed = null)
+        switch (_behaviour)
         {
-            Content = content;
-            Embed = embed?.Build();
+            case PaginationBehaviour.Ignore:
+                if (_index == 0)
+                {
+                    break;
+                }
+                else
+                {
+                    _index--;
+                }
+
+                break;
+
+            case PaginationBehaviour.WrapAround:
+                if (_index == 0)
+                {
+                    _index = _pages.Count - 1;
+                }
+                else
+                {
+                    _index--;
+                }
+
+                break;
         }
+    }
+
+    public async Task<PaginationEmojis> GetEmojisAsync()
+    {
+        await Task.Yield();
+
+        return _emojis;
+    }
+
+    public Task<IEnumerable<DiscordButtonComponent>> GetButtonsAsync()
+        => throw new NotSupportedException("This request does not support buttons.");
+
+    public async Task<DiscordMessage> GetMessageAsync()
+    {
+        await Task.Yield();
+
+        return _message;
+    }
+
+    public async Task<DiscordUser> GetUserAsync()
+    {
+        await Task.Yield();
+
+        return _user;
+    }
+
+    public async Task DoCleanupAsync()
+    {
+        switch (PaginationDeletion)
+        {
+            case PaginationDeletion.DeleteEmojis:
+                await _message.DeleteAllReactionsAsync();
+                break;
+
+            case PaginationDeletion.DeleteMessage:
+                await _message.DeleteAsync();
+                break;
+
+            case PaginationDeletion.KeepEmojis:
+                break;
+        }
+    }
+
+    public async Task<TaskCompletionSource<bool>> GetTaskCompletionSourceAsync()
+    {
+        await Task.Yield();
+
+        return _tcs;
+    }
+
+    /// <summary>
+    /// Disposes this PaginationRequest.
+    /// </summary>
+    public void Dispose()
+    {
+        // Why doesn't this class implement IDisposable?
+
+        _ct?.Dispose();
+        _tcs = null!;
     }
 }
