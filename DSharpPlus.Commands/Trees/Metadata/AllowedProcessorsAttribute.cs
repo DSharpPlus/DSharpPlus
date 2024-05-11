65f33db8128791f4e3718bd29682b44b02c9fe73
Plerx <50530305+Plerx2493@users.noreply.github.com>
2024-05-11 15:02:08 +0200

    Add xml docs

--- DSharpPlus.Commands/Trees/Metadata/AllowedProcessorsAttribute.cs
+++ DSharpPlus.Commands/Trees/Metadata/AllowedProcessorsAttribute.cs
@@ -5,20 +5,28 @@ using DSharpPlus.Commands.Processors;
 namespace DSharpPlus.Commands.Trees.Metadata;
 
 /// <summary>
-/// 
+/// Allows to restrict commands to certain processors.
 /// </summary>
+/// <remarks>
+/// This attribute only works on top-level commands.
+/// </remarks>
 [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
 public class AllowedProcessorsAttribute : Attribute
 {
     public AllowedProcessorsAttribute(params Type[] processors)
     {
+        if (processors.Length < 1)
+        {
+            throw new ArgumentException("Provide atleast one processor", nameof(processors));
+        }
+
         if (processors.Any(x => x.IsAssignableTo(typeof(ICommandProcessor)) is false))
         {
             throw new ArgumentException("All processors must implement ICommandProcessor.", nameof(processors));
         }
-        
+
         this.Processors = processors;
-    } 
+    }
 
     public Type[] Processors { get; private set; }
 }
