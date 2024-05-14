544fa91f76c3a0d82fc2ef4ec52d43bd9c2df0fd
akiraveliara <akiraveliara@outlook.com>
2024-05-14 19:37:57 +0200

    delete a remnant of earlier attempts

--- DSharpPlus.Commands/Invocation/ConverterUtil.cs
+++
@@ -1,8 +0,0 @@
-namespace DSharpPlus.Commands.Invocation;
-
-/// <summary>
-/// Provides utility methods for handling converters and their state management.
-/// </summary>
-internal static class ConverterUtil
-{
-}
