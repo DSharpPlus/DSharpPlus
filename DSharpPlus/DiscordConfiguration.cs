87c2868a7901b092833bb62fc35e44dda79f8edc
Velvet <42438262+VelvetToroyashi@users.noreply.github.com>
2024-06-06 18:08:12 -0400

    fix(gateway): Disable zlib compression by default (#1945)
    * fix(gateway): Disable zlib compression by default
    
    Fixes a potential issue wherein our decompressor dies because of rapid reconnections, causing mangled payloads. That issue still needs to be fixed, but this is a stopgap to help see if this is Discord's fault.
    
    * docs(gateway): Update documentation
    
    * docs(gateway): Documentation 2: Electric Boogaloo
    
    * docs(gateway): Fix justification grammer
    
    Thank you, Livia "grammar" Silvercrown :when:

--- DSharpPlus/DiscordConfiguration.cs
+++ DSharpPlus/DiscordConfiguration.cs
@@ -84,9 +84,11 @@ public sealed class DiscordConfiguration
     /// <summary>
     /// <para>Sets the level of compression for WebSocket traffic.</para>
     /// <para>Disabling this option will increase the amount of traffic sent via WebSocket. Setting <see cref="GatewayCompressionLevel.Payload"/> will enable compression for READY and GUILD_CREATE payloads. Setting <see cref="Stream"/> will enable compression for the entire WebSocket stream, drastically reducing amount of traffic.</para>
-    /// <para>Defaults to <see cref="Stream"/>.</para>
+    /// <para>Defaults to <see cref="GatewayCompressionLevel.None"/>.</para>
     /// </summary>
-    public GatewayCompressionLevel GatewayCompressionLevel { internal get; set; } = GatewayCompressionLevel.Stream;
+    /// <remarks>This property's default has been changed from <see cref="GatewayCompressionLevel.Stream"/> due to a bug in the client wherein rapid reconnections causes payloads to be decompressed into a mangled string.</remarks>
+    // Here be dragons, ye who use compression.
+    public GatewayCompressionLevel GatewayCompressionLevel { internal get; set; } = GatewayCompressionLevel.None;
 
     /// <summary>
     /// Specifies the probing interval in ms to use when first making requests to the API. This should be slightly higher than your average ping to the discord rest api.
