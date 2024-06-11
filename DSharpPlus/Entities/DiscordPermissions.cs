3effc7b3c183e508e3e6c288723b9e8c62745777
Plerx <50530305+Plerx2493@users.noreply.github.com>
2024-06-11 19:52:55 +0200

    formatting

--- DSharpPlus/Entities/DiscordPermissions.cs
+++ DSharpPlus/Entities/DiscordPermissions.cs
@@ -5,6 +5,7 @@ namespace DSharpPlus.Entities;
 public static class PermissionMethods
 {
     internal static DiscordPermissions FULL_PERMS { get; } = (DiscordPermissions)0x0003_3FFF_FFFF_FFFF;
+    
     /// <summary>
     /// Calculates whether this permission set contains the given permission.
     /// </summary>
