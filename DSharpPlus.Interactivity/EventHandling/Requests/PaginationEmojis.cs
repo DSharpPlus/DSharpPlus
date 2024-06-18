752fb7131b10c180aedaa2bcb123ce83a1da6492
Velvet Toroyashi <42438262+VelvetToroyashi@users.noreply.github.com>
2024-06-18 04:06:40 -0400

    feat(interactivity): Add support for components in pages

--- DSharpPlus.Interactivity/EventHandling/Requests/PaginationEmojis.cs
+++ DSharpPlus.Interactivity/EventHandling/Requests/PaginationEmojis.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using DSharpPlus.Entities;
 
 namespace DSharpPlus.Interactivity;
@@ -25,9 +28,59 @@ public class Page
     public string Content { get; set; }
     public DiscordEmbed Embed { get; set; }
 
-    public Page(string content = "", DiscordEmbedBuilder embed = null)
+    public IReadOnlyList<DiscordActionRowComponent> Components { get; }
+
+    public Page(string content = "", DiscordEmbedBuilder embed = null, IReadOnlyList<DiscordComponent> components = null)
     {
         this.Content = content;
         this.Embed = embed?.Build();
+
+        if (components is null or [])
+        {
+            this.Components = [];
+
+            return;
+        }
+
+        if (components[0] is DiscordActionRowComponent arc)
+        {
+            if (components.Count > 4)
+            {
+                throw new ArgumentException("Pages can only contain four rows of components");
+            }
+
+            this.Components = [arc];
+        }
+        else
+        {
+            List<DiscordActionRowComponent> componentRows = [];
+            List<DiscordComponent> currentRow = new(5);
+
+            foreach (DiscordComponent component in components)
+            {
+                if (component is BaseDiscordSelectComponent)
+                {
+                    componentRows.Add(new([component]));
+
+                    continue;
+                }
+
+                if (currentRow.Count == 5)
+                {
+                    componentRows.Add(new DiscordActionRowComponent(currentRow));
+                    currentRow = new List<DiscordComponent>(5);
+                }
+
+                currentRow.Add(component);
+            }
+
+            if (currentRow.Count > 0)
+            {
+                componentRows.Add(new DiscordActionRowComponent(currentRow));
+            }
+
+            this.Components = componentRows;
+
+        }
     }
 }
