25412dc6d7fae76dd4707bfa6eb09ce44e9baf5a
akiraveliara <akiraveliara@outlook.com>
2024-06-22 13:20:45 +0200

    theoretically multi-shard orchestration works too now

--- DSharpPlus/Clients/MultiShardOrchestrator.cs
+++ DSharpPlus/Clients/MultiShardOrchestrator.cs
@@ -1,8 +1,13 @@
 using System;
 using System.Threading.Tasks;
 
+using DSharpPlus.Entities;
 using DSharpPlus.Net;
+using DSharpPlus.Net.Abstractions;
 using DSharpPlus.Net.Gateway;
+using DSharpPlus.Net.WebSocket;
+
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Options;
 
 namespace DSharpPlus.Clients;
@@ -16,28 +21,30 @@ public sealed class MultiShardOrchestrator : IShardOrchestrator
     private readonly DiscordApiClient apiClient;
     private readonly ShardingOptions options;
     private readonly IServiceProvider serviceProvider;
+    private readonly PayloadDecompressor decompressor;
 
     public MultiShardOrchestrator
     (
         IServiceProvider serviceProvider, 
         IOptions<ShardingOptions> options,
-        DiscordApiClient apiClient
+        DiscordApiClient apiClient,
+        PayloadDecompressor decompressor
     )
     {
         this.apiClient = apiClient;
         this.options = options.Value;
         this.serviceProvider = serviceProvider;
+        this.decompressor = decompressor;
     }
 
     /// <inheritdoc/>
-    public async ValueTask StartAsync()
+    public async ValueTask StartAsync(DiscordActivity? activity, DiscordUserStatus? status, DateTimeOffset? idleSince)
     {
         uint startShards, totalShards, stride;
+        GatewayInfo info = await this.apiClient.GetGatewayInfoAsync();
 
         if (this.options.ShardCount is null)
         {
-            GatewayInfo info = await this.apiClient.GetGatewayInfoAsync();
-
             startShards = totalShards = (uint)info.ShardCount;
             stride = 0;
         }
@@ -57,6 +64,42 @@ public sealed class MultiShardOrchestrator : IShardOrchestrator
             }
         }
 
+        QueryUriBuilder gwuri = new(info.Url);
+
+        gwuri.AddParameter("v", "10")
+             .AddParameter("encoding", "json");
+
+        if (this.decompressor.CompressionLevel == GatewayCompressionLevel.Stream)
+        {
+            gwuri.AddParameter("compress", "zlib-stream");
+        }
+
         this.shards = new IGatewayClient[startShards];
+
+        for (int i = 0; i < startShards; i++)
+        {
+            this.shards[i] = this.serviceProvider.GetRequiredService<IGatewayClient>();
+
+            await this.shards[i].ConnectAsync
+            (
+                gwuri.Build(),
+                activity,
+                status,
+                idleSince,
+                new ShardInfo
+                {
+                    ShardCount = (int)totalShards,
+                    ShardId = (int)stride + i
+                }
+            );
+        }
+    }
+
+    public async ValueTask StopAsync()
+    {
+        foreach (IGatewayClient client in this.shards)
+        {
+            await client.DisconnectAsync();
+        }
     }
 }
