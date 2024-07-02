cf4eee8cf815087327633f451cab9fa452a2bd59
akiraveliara <akiraveliara@outlook.com>
2024-07-02 16:59:32 +0200

    xmldocs for IGatewayClient

--- DSharpPlus/Net/Gateway/IGatewayClient.cs
+++ DSharpPlus/Net/Gateway/IGatewayClient.cs
@@ -11,6 +11,14 @@ namespace DSharpPlus.Net.Gateway;
 /// </summary>
 public interface IGatewayClient
 {
+    /// <summary>
+    /// Connects this client to the gateway.
+    /// </summary>
+    /// <param name="url">The gateway URL to use for connecting, including any potential reconnects.</param>
+    /// <param name="activity">An optional activity to send to the gateway when connecting.</param>
+    /// <param name="status">An optional status to send to the gateway when connecting.</param>
+    /// <param name="idleSince">An optional idle timer to send to the gateway when connecting.</param>
+    /// <param name="shardInfo">If this isn't the only shard, additional information about this shard.</param>
     public ValueTask ConnectAsync
     (
         string url,
@@ -20,13 +28,28 @@ public interface IGatewayClient
         ShardInfo? shardInfo = null
     );
 
+    /// <summary>
+    /// Disconnects from the gateway.
+    /// </summary>
     public ValueTask DisconnectAsync();
 
+    /// <summary>
+    /// Reconnects to the gateway.
+    /// </summary>
     public ValueTask ReconnectAsync();
 
+    /// <summary>
+    /// Sends the provided payload to the gateway.
+    /// </summary>
     public ValueTask WriteAsync(byte[] payload);
 
+    /// <summary>
+    /// Indicates whether this client is connected.
+    /// </summary>
     public bool IsConnected { get; }
 
+    /// <summary>
+    /// Indicates the latency between this client and Discord.
+    /// </summary>
     public TimeSpan Ping { get; }
 }
