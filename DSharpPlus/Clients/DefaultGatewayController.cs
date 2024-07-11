0269dbe9485a6da969a7f3703abf182aa1e85ccb
Plerx <50530305+Plerx2493@users.noreply.github.com>
2024-07-11 14:50:27 +0200

    I should build locally before pushing and open a pr

--- DSharpPlus/Clients/DefaultGatewayController.cs
+++ DSharpPlus/Clients/DefaultGatewayController.cs
@@ -9,4 +9,5 @@ namespace DSharpPlus.Clients;
 internal class DefaultGatewayController : IGatewayController
 {
     public ValueTask ZombiedAsync(IGatewayClient client) => ValueTask.CompletedTask;
+    public ValueTask HeartbeatedAsync(IGatewayClient client) => ValueTask.CompletedTask;
 }
