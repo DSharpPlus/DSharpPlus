d2d0a481335cd69ef01d82633bdba6f63802d5ea
akiraveliara <akiraveliara@outlook.com>
2024-07-11 17:04:03 +0200

    the last remaining straws

--- DSharpPlus/Net/Gateway/Compression/IPayloadDecompressor.cs
+++ DSharpPlus/Net/Gateway/Compression/IPayloadDecompressor.cs
@@ -1,5 +1,29 @@
+using System;
+
+using CommunityToolkit.HighPerformance.Buffers;
+
 namespace DSharpPlus.Net.Gateway.Compression;
 
+/// <summary>
+/// Contains functionality for decompressing inbound gateway payloads.
+/// </summary>
 public interface IPayloadDecompressor
 {
+    /// <summary>
+    /// Gets the name of the decompressor.
+    /// </summary>
+    public string Name { get; }
+
+    /// <summary>
+    /// Indicates whether the present compression format is connection-wide.
+    /// </summary>
+    public bool IsTransportCompression { get; }
+
+    /// <summary>
+    /// Attempts to decompress the provided payload.
+    /// </summary>
+    /// <param name="compressed">The raw, compressed data.</param>
+    /// <param name="decompressed">A buffer writer for writing decompressed data.</param>
+    /// <returns>A value indicating whether the operation was successful.</returns>
+    public bool TryDecompress(ReadOnlySpan<byte> compressed, ArrayPoolBufferWriter<byte> decompressed);
 }
--- DSharpPlus/Net/Gateway/Compression/NullDecompressor.cs
+++ DSharpPlus/Net/Gateway/Compression/NullDecompressor.cs
@@ -1,5 +1,26 @@
+using System;
+
+using CommunityToolkit.HighPerformance;
+using CommunityToolkit.HighPerformance.Buffers;
+
 namespace DSharpPlus.Net.Gateway.Compression;
 
+/// <summary>
+/// Represents a decompressor that doesn't decompress at all.
+/// </summary>
 public sealed class NullDecompressor : IPayloadDecompressor
 {
+    /// <inheritdoc/>
+    public string Name => string.Empty;
+
+    // this decompressor *technically* applies transport-wide, and this simplifies composing the IDENTIFY payload.
+    /// <inheritdoc/>
+    public bool IsTransportCompression => true;
+
+    /// <inheritdoc/>
+    public bool TryDecompress(ReadOnlySpan<byte> compressed, ArrayPoolBufferWriter<byte> decompressed)
+    {
+        decompressed.Write(compressed);
+        return true;
+    }
 }
