8d04dc787a5dabc8d138f21dc9fd93add81017c4
Lunar Starstrum <lunar@forsaken-borders.net>
2024-07-26 13:03:17 -0500

    Formatting

--- DSharpPlus.Commands/Processors/TextCommands/TextExtensions.cs
+++ DSharpPlus.Commands/Processors/TextCommands/TextExtensions.cs
@@ -11,33 +11,66 @@ namespace DSharpPlus.Commands.Processors.TextCommands;
 
 internal static partial class TextExtensions
 {
-    [GeneratedRegex(@"<@&(\d+)>", RegexOptions.Compiled)] private static partial Regex _roleMentionRegex();
-    [GeneratedRegex(@"<@!?(\d+)>", RegexOptions.Compiled)] private static partial Regex _userMentionRegex();
-    [GeneratedRegex(@"<#(\d+)>", RegexOptions.Compiled)] private static partial Regex _channelMentionRegex();
+    [GeneratedRegex(@"<@&(\d+)>", RegexOptions.Compiled)]
+    private static partial Regex roleMentionRegex();
 
-    [UnsafeAccessor(UnsafeAccessorKind.Constructor)] private static extern MessageCreatedEventArgs _messageCreateEventArgsConstructor();
-    [UnsafeAccessor(UnsafeAccessorKind.Method, Name = "set_Message")] private static extern void _messageCreateEventArgsMessageSetter(MessageCreatedEventArgs messageCreateEventArgs, DiscordMessage message);
-    [UnsafeAccessor(UnsafeAccessorKind.Method, Name = "set_MentionedUsers")] private static extern void _messageCreateEventArgsMentionedUsersSetter(MessageCreatedEventArgs messageCreateEventArgs, IReadOnlyList<DiscordUser> mentionedUsers);
-    [UnsafeAccessor(UnsafeAccessorKind.Method, Name = "set_MentionedRoles")] private static extern void _messageCreateEventArgsMentionedRolesSetter(MessageCreatedEventArgs messageCreateEventArgs, IReadOnlyList<DiscordRole> mentionedRoles);
-    [UnsafeAccessor(UnsafeAccessorKind.Method, Name = "set_MentionedChannels")] private static extern void _messageCreateEventArgsMentionedChannelsSetter(MessageCreatedEventArgs messageCreateEventArgs, IReadOnlyList<DiscordChannel> mentionedChannels);
+    [GeneratedRegex(@"<@!?(\d+)>", RegexOptions.Compiled)]
+    private static partial Regex userMentionRegex();
 
-    [UnsafeAccessor(UnsafeAccessorKind.Constructor)] private static extern DiscordMessage _messageConstructor();
-    [UnsafeAccessor(UnsafeAccessorKind.Constructor)] private static extern DiscordMessage _messageConstructor(DiscordMessage message);
-    [UnsafeAccessor(UnsafeAccessorKind.Method, Name = "set_Content")] private static extern void _messageContentSetter(DiscordMessage message, string content);
-    [UnsafeAccessor(UnsafeAccessorKind.Method, Name = "set_Channel")] private static extern void _messageChannelSetter(DiscordMessage message, DiscordChannel channel);
-    [UnsafeAccessor(UnsafeAccessorKind.Method, Name = "set_ChannelId")] private static extern void _messageChannelIdSetter(DiscordMessage message, ulong channelId);
-    [UnsafeAccessor(UnsafeAccessorKind.Method, Name = "set_guildId")] private static extern void _messageGuildIdSetter(DiscordMessage message, ulong? guildId);
-    [UnsafeAccessor(UnsafeAccessorKind.Method, Name = "set_Author")] private static extern void _messageAuthorSetter(DiscordMessage message, DiscordUser author);
-    [UnsafeAccessor(UnsafeAccessorKind.Field, Name = "mentionedUsers")] private static extern ref List<DiscordUser> _messageMentionedUsersSetter(DiscordMessage message);
-    [UnsafeAccessor(UnsafeAccessorKind.Field, Name = "mentionedRoles")] private static extern ref List<DiscordRole> _messageMentionedRolesSetter(DiscordMessage message);
-    [UnsafeAccessor(UnsafeAccessorKind.Field, Name = "mentionedChannels")] private static extern ref List<DiscordChannel> _messageMentionedChannelsSetter(DiscordMessage message);
+    [GeneratedRegex(@"<#(\d+)>", RegexOptions.Compiled)]
+    private static partial Regex channelMentionRegex();
+
+    [UnsafeAccessor(UnsafeAccessorKind.Constructor)]
+    private static extern MessageCreatedEventArgs messageCreateEventArgsConstructor();
+
+    [UnsafeAccessor(UnsafeAccessorKind.Method, Name = "set_Message")]
+    private static extern void messageCreateEventArgsMessageSetter(MessageCreatedEventArgs messageCreateEventArgs, DiscordMessage message);
+
+    [UnsafeAccessor(UnsafeAccessorKind.Method, Name = "set_MentionedUsers")]
+    private static extern void messageCreateEventArgsMentionedUsersSetter(MessageCreatedEventArgs messageCreateEventArgs, IReadOnlyList<DiscordUser> mentionedUsers);
+
+    [UnsafeAccessor(UnsafeAccessorKind.Method, Name = "set_MentionedRoles")]
+    private static extern void messageCreateEventArgsMentionedRolesSetter(MessageCreatedEventArgs messageCreateEventArgs, IReadOnlyList<DiscordRole> mentionedRoles);
+
+    [UnsafeAccessor(UnsafeAccessorKind.Method, Name = "set_MentionedChannels")]
+    private static extern void messageCreateEventArgsMentionedChannelsSetter(MessageCreatedEventArgs messageCreateEventArgs, IReadOnlyList<DiscordChannel> mentionedChannels);
+
+    [UnsafeAccessor(UnsafeAccessorKind.Constructor)]
+    private static extern DiscordMessage messageConstructor();
+
+    [UnsafeAccessor(UnsafeAccessorKind.Constructor)]
+    private static extern DiscordMessage messageConstructor(DiscordMessage message);
+
+    [UnsafeAccessor(UnsafeAccessorKind.Method, Name = "set_Content")]
+    private static extern void messageContentSetter(DiscordMessage message, string content);
+
+    [UnsafeAccessor(UnsafeAccessorKind.Method, Name = "set_Channel")]
+    private static extern void messageChannelSetter(DiscordMessage message, DiscordChannel channel);
+
+    [UnsafeAccessor(UnsafeAccessorKind.Method, Name = "set_ChannelId")]
+    private static extern void messageChannelIdSetter(DiscordMessage message, ulong channelId);
+
+    [UnsafeAccessor(UnsafeAccessorKind.Method, Name = "set_guildId")]
+    private static extern void messageGuildIdSetter(DiscordMessage message, ulong? guildId);
+
+    [UnsafeAccessor(UnsafeAccessorKind.Method, Name = "set_Author")]
+    private static extern void messageAuthorSetter(DiscordMessage message, DiscordUser author);
+
+    [UnsafeAccessor(UnsafeAccessorKind.Field, Name = "mentionedUsers")]
+    private static extern ref List<DiscordUser> messageMentionedUsersSetter(DiscordMessage message);
+
+    [UnsafeAccessor(UnsafeAccessorKind.Field, Name = "mentionedRoles")]
+    private static extern ref List<DiscordRole> messageMentionedRolesSetter(DiscordMessage message);
+
+    [UnsafeAccessor(UnsafeAccessorKind.Field, Name = "mentionedChannels")]
+    private static extern ref List<DiscordChannel> messageMentionedChannelsSetter(DiscordMessage message);
 
     /// <summary>
     /// Creates a shallow copy of a Discord message.
     /// </summary>
     /// <param name="message">The message to copy.</param>
     /// <returns>A shallow copy of the message.</returns>
-    public static DiscordMessage Copy(this DiscordMessage message) => _messageConstructor(message);
+    public static DiscordMessage Copy(this DiscordMessage message) => messageConstructor(message);
 
     /// <summary>
     /// Creates a fake message created event args object with the specified content.
@@ -60,11 +93,11 @@ internal static partial class TextExtensions
         messageCopy.ModifyMessageProperties(content, context.Client, context.User, context.Channel, context.Guild);
 
         // Create the message created event args.
-        MessageCreatedEventArgs messageCreateEventArgs = _messageCreateEventArgsConstructor();
-        _messageCreateEventArgsMessageSetter(messageCreateEventArgs, messageCopy);
-        _messageCreateEventArgsMentionedUsersSetter(messageCreateEventArgs, message.MentionedUsers);
-        _messageCreateEventArgsMentionedRolesSetter(messageCreateEventArgs, message.MentionedRoles);
-        _messageCreateEventArgsMentionedChannelsSetter(messageCreateEventArgs, message.MentionedChannels);
+        MessageCreatedEventArgs messageCreateEventArgs = messageCreateEventArgsConstructor();
+        messageCreateEventArgsMessageSetter(messageCreateEventArgs, messageCopy);
+        messageCreateEventArgsMentionedUsersSetter(messageCreateEventArgs, messageCopy.MentionedUsers);
+        messageCreateEventArgsMentionedRolesSetter(messageCreateEventArgs, messageCopy.MentionedRoles);
+        messageCreateEventArgsMentionedChannelsSetter(messageCreateEventArgs, messageCopy.MentionedChannels);
 
         // Return the message created event args, which can be used for argument conversion or command execution.
         return messageCreateEventArgs;
@@ -79,14 +112,20 @@ internal static partial class TextExtensions
     /// <param name="user">The new author of the message.</param>
     /// <param name="channel">The new channel of the message.</param>
     /// <param name="guild">The guild to use for fetching roles.</param>
-    public static void ModifyMessageProperties(this DiscordMessage message, string content, DiscordClient client, DiscordUser user, DiscordChannel channel, DiscordGuild? guild = null)
+    public static void ModifyMessageProperties(
+        this DiscordMessage message,
+        string content,
+        DiscordClient client,
+        DiscordUser user,
+        DiscordChannel channel,
+        DiscordGuild? guild = null)
     {
         List<DiscordRole> roleMentions = [];
         List<DiscordUser> userMentions = [];
         List<DiscordChannel> channelMentions = [];
         if (guild is not null)
         {
-            MatchCollection roleMatches = _roleMentionRegex().Matches(content);
+            MatchCollection roleMatches = roleMentionRegex().Matches(content);
             foreach (Match match in roleMatches.Cast<Match>())
             {
                 if (ulong.TryParse(match.Groups[1].Value, out ulong roleId) && guild.GetRole(roleId) is DiscordRole role)
@@ -95,7 +134,7 @@ internal static partial class TextExtensions
                 }
             }
 
-            MatchCollection userMatches = _userMentionRegex().Matches(content);
+            MatchCollection userMatches = userMentionRegex().Matches(content);
             foreach (Match match in userMatches.Cast<Match>())
             {
                 if (ulong.TryParse(match.Groups[1].Value, out ulong userId))
@@ -105,7 +144,7 @@ internal static partial class TextExtensions
             }
         }
 
-        MatchCollection channelMatches = _channelMentionRegex().Matches(content);
+        MatchCollection channelMatches = channelMentionRegex().Matches(content);
         foreach (Match match in channelMatches.Cast<Match>())
         {
             if (ulong.TryParse(match.Groups[1].Value, out ulong channelId))
@@ -124,13 +163,13 @@ internal static partial class TextExtensions
             }
         }
 
-        _messageContentSetter(message, content);
-        _messageChannelSetter(message, channel);
-        _messageChannelIdSetter(message, channel.Id);
-        _messageGuildIdSetter(message, channel.GuildId);
-        _messageAuthorSetter(message, user);
-        _messageMentionedUsersSetter(message) = userMentions;
-        _messageMentionedRolesSetter(message) = roleMentions;
-        _messageMentionedChannelsSetter(message) = channelMentions;
+        messageContentSetter(message, content);
+        messageChannelSetter(message, channel);
+        messageChannelIdSetter(message, channel.Id);
+        messageGuildIdSetter(message, channel.GuildId);
+        messageAuthorSetter(message, user);
+        messageMentionedUsersSetter(message) = userMentions;
+        messageMentionedRolesSetter(message) = roleMentions;
+        messageMentionedChannelsSetter(message) = channelMentions;
     }
 }
