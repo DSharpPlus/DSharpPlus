b41dd988a4b8e629da7f03932b558d0826877eaa
Lunar Starstrum <lunar@forsaken-borders.net>
2024-07-31 12:21:37 -0500

    New DisableCommandNotFoundException for the text processor (#2048)
    * New DisableCommandNotFoundException for the text processor
    
    * Flip flop
    
    * It built fine locally

--- DSharpPlus.Commands/Processors/TextCommands/TextCommandConfiguration.cs
+++ DSharpPlus.Commands/Processors/TextCommands/TextCommandConfiguration.cs
@@ -10,10 +10,15 @@ public record TextCommandConfiguration
     /// The function to use to resolve prefixes for commands.
     /// </summary>
     /// <remarks>For dynamic prefix resolving, <see cref="IPrefixResolver"/> registered to the <see cref="DiscordClient"/>'s <see cref="IServiceProvider"/> should be preferred.</remarks>
-    public ResolvePrefixDelegateAsync PrefixResolver { get; init; } = new DefaultPrefixResolver(true,"!").ResolvePrefixAsync;
+    public ResolvePrefixDelegateAsync PrefixResolver { get; init; } = new DefaultPrefixResolver(true, "!").ResolvePrefixAsync;
     public TextArgumentSplicer TextArgumentSplicer { get; init; } = DefaultTextArgumentSplicer.Splice;
     public char[] QuoteCharacters { get; init; } = ['"', '\'', '«', '»', '‘', '“', '„', '‟'];
     public bool IgnoreBots { get; init; } = true;
+
+    /// <summary>
+    /// Disables the exception thrown when a command is not found.
+    /// </summary>
+    public bool EnableCommandNotFoundException { get; init; }
 
     /// <summary>
     /// Whether to suppress the missing message content intent warning.
