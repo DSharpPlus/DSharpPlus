a5d58d93fd3937c1785a2eadb5154e016c34b863
Livia-Valeriya Silvercrown <61018569+akiraveliara@users.noreply.github.com>
2024-08-13 17:37:03 +0200

    expose the guild IDs in SessionCreated too, otherwise the event is significantly less useful (#2070)

--- DSharpPlus/EventArgs/SessionCreatedEventArgs.cs
+++ DSharpPlus/EventArgs/SessionCreatedEventArgs.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+
 namespace DSharpPlus.EventArgs;
 
 /// <summary>
@@ -11,4 +13,11 @@ public sealed class SessionCreatedEventArgs : DiscordEventArgs
     /// The ID of the shard this event occurred on.
     /// </summary>
     public int ShardId { get; internal set; }
+
+    /// <summary>
+    /// Gets the IDs of guilds connected to the shard that created this session. Note that DiscordGuild objects may
+    /// not yet be available by the time this event is fired, and if you require access to any objects, you should wait
+    /// for GuildDownloadCompleted.
+    /// </summary>
+    public IReadOnlyList<ulong> GuildIds { get; internal set; }
 }
