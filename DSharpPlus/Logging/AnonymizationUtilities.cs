c2d7302a193c437f9d666cca11c60e8fc23be207
akiraveliara <akiraveliara@outlook.com>
2024-09-17 19:05:15 +0200

    reduce backslash spam in the regexes

--- DSharpPlus/Logging/AnonymizationUtilities.cs
+++ DSharpPlus/Logging/AnonymizationUtilities.cs
@@ -4,10 +4,10 @@ namespace DSharpPlus.Logging;
 
 internal static partial class AnonymizationUtilities
 {
-    [GeneratedRegex("\\\"token\\\":\\\"[a-zA-Z0-9\\-\\. ]+\\\"")]
+    [GeneratedRegex(@"""token"":""[a-zA-Z0-9\-\. ]+""")]
     private static partial Regex GetJsonEncodedTokenRegex();
 
-    [GeneratedRegex("\\/webhooks\\/[0-9]+\\/[a-zA-Z0-9\\-\\. ]+\\/")]
+    [GeneratedRegex(@"\/webhooks\/[0-9]+\/[a-zA-Z0-9\-\. ]+\/")]
     private static partial Regex GetWebhookPathRegex();
 
     public static string AnonymizeTokens(string input)
@@ -19,13 +19,13 @@ internal static partial class AnonymizationUtilities
 
     // --------------------------------------------------------------------------------------------------
 
-    [GeneratedRegex("\\\"[0-9]{17,22}\\\"")]
+    [GeneratedRegex(@"""[0-9]{17,22}""")]
     private static partial Regex GetSnowflakeRegex();
 
-    [GeneratedRegex("\\\"content\\\":\\\"[^\"]+\\\"")]
+    [GeneratedRegex(@"""content"":""[^""]+""")]
     private static partial Regex GetMessageContentRegex();
 
-    [GeneratedRegex("\\\"username\\\":\\\"[a-zA-Z0-9\\.\\-_]{3,32}\\\"")]
+    [GeneratedRegex(@"""username"":""[a-zA-Z0-9\.\-_]{3,32}""")]
     private static partial Regex GetUsernameRegex();
 
     public static string AnonymizeContents(string input)
