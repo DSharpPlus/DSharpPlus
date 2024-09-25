da8b28318cd693e3229b22471035553e2923514a
Lunar Starstrum <lunar@forsaken-borders.net>
2024-09-24 23:49:50 -0500

    Fix enum parsing for text commands, which now properly supports boxing (is it called boxing?)

--- DSharpPlus.Commands/Converters/EnumArgumentConverter.cs
+++ DSharpPlus.Commands/Converters/EnumArgumentConverter.cs
@@ -15,36 +15,22 @@ public class EnumConverter : ISlashArgumentConverter<Enum>, ITextArgumentConvert
 
     public Task<Optional<Enum>> ConvertAsync(ConverterContext context)
     {
-        if (context.Argument is string stringArgument && Enum.TryParse(context.Parameter.Type, stringArgument, true, out object? result))
+        // The parameter type could be an Enum? or an Enum[] or an Enum?[] or an Enum[][]. You get it.
+        Type enumType = IArgumentConverter.GetConverterFriendlyBaseType(context.Parameter.Type, ConverterFriendlySearchFlags.ReturnEnumType);
+        if (context.Argument is string stringArgument && Enum.TryParse(enumType, stringArgument, true, out object? result))
         {
             return Task.FromResult(Optional.FromValue((Enum)result));
         }
 
-        // The parameter type could be an Enum? or an Enum[] or an Enum?[] or an Enum[][]. You get it.
-        // Let's just figure out what the base type of Enum actually is (int, long, byte, etc).
-        Type baseEnumType = GetStrongEnumType(context.Parameter.Type);
+        // Figure out what the base type of Enum actually is (int, long, byte, etc).
+        Type baseEnumType = Enum.GetUnderlyingType(enumType);
 
         // Convert the argument to the base type of the enum. If this was invoked via slash commands,
         // Discord will send us the argument as a number, which STJ will convert to an unknown numeric type.
         // We need to ensure that the argument is the same type as it's enum base type.
-        object? value = Convert.ChangeType(context.Argument, Enum.GetUnderlyingType(baseEnumType), CultureInfo.InvariantCulture);
-        return value is not null && Enum.IsDefined(baseEnumType, value)
-            ? Task.FromResult(Optional.FromValue((Enum)Enum.ToObject(baseEnumType, value)))
+        object? value = Convert.ChangeType(context.Argument, baseEnumType, CultureInfo.InvariantCulture);
+        return value is not null && Enum.IsDefined(enumType, value)
+            ? Task.FromResult(Optional.FromValue((Enum)Enum.ToObject(enumType, value)))
             : Task.FromResult(Optional.FromNoValue<Enum>());
-    }
-
-    private static Type GetStrongEnumType(Type type)
-    {
-        ArgumentNullException.ThrowIfNull(type, nameof(type));
-
-        Type effectiveType = Nullable.GetUnderlyingType(type) ?? type;
-        if (effectiveType.IsArray)
-        {
-            // The type could be an array of enums or nullable
-            // objects or worse: an array of arrays.
-            return GetStrongEnumType(effectiveType.GetElementType()!);
-        }
-
-        return effectiveType;
     }
 }
