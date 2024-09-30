61a8fc4cd36f08a8f5a2ff8b689e3518115e69a2
Lunar Starstrum <lunar@forsaken-borders.net>
2024-09-30 13:40:10 -0500

    Add generic overloads

--- DSharpPlus.Commands/Trees/Metadata/AllowedProcessorsAttribute.cs
+++ DSharpPlus.Commands/Trees/Metadata/AllowedProcessorsAttribute.cs
@@ -16,6 +16,10 @@ namespace DSharpPlus.Commands.Trees.Metadata;
 [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
 public class AllowedProcessorsAttribute : Attribute
 {
+    /// <summary>
+    /// Specifies which processors are allowed to execute this command.
+    /// </summary>
+    /// <param name="processors">Types of processors that are allowed to execute this command.</param>
     public AllowedProcessorsAttribute(params Type[] processors)
     {
         if (processors.Length < 1)
@@ -44,4 +48,64 @@ public class AllowedProcessorsAttribute : Attribute
     /// Types of allowed processors
     /// </summary>
     public Type[] Processors { get; private set; }
+}
+
+/// <inheritdoc />
+[AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
+public class AllowedProcessorsAttribute<T> : AllowedProcessorsAttribute
+    where T : ICommandProcessor
+{
+    /// <inheritdoc />
+    public AllowedProcessorsAttribute()
+        : base(typeof(T)) { }
+}
+
+/// <inheritdoc />
+[AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
+public class AllowedProcessorsAttribute<T1, T2> : AllowedProcessorsAttribute
+    where T1 : ICommandProcessor
+    where T2 : ICommandProcessor
+{
+    /// <inheritdoc />
+    public AllowedProcessorsAttribute()
+        : base(typeof(T1), typeof(T2)) { }
+}
+
+/// <inheritdoc />
+[AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
+public class AllowedProcessorsAttribute<T1, T2, T3> : AllowedProcessorsAttribute
+    where T1 : ICommandProcessor
+    where T2 : ICommandProcessor
+    where T3 : ICommandProcessor
+{
+    /// <inheritdoc />
+    public AllowedProcessorsAttribute()
+        : base(typeof(T1), typeof(T2), typeof(T3)) { }
+}
+
+/// <inheritdoc />
+[AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
+public class AllowedProcessorsAttribute<T1, T2, T3, T4> : AllowedProcessorsAttribute
+    where T1 : ICommandProcessor
+    where T2 : ICommandProcessor
+    where T3 : ICommandProcessor
+    where T4 : ICommandProcessor
+{
+    /// <inheritdoc />
+    public AllowedProcessorsAttribute()
+        : base(typeof(T1), typeof(T2), typeof(T3), typeof(T4)) { }
+}
+
+/// <inheritdoc />
+[AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
+public class AllowedProcessorsAttribute<T1, T2, T3, T4, T5> : AllowedProcessorsAttribute
+    where T1 : ICommandProcessor
+    where T2 : ICommandProcessor
+    where T3 : ICommandProcessor
+    where T4 : ICommandProcessor
+    where T5 : ICommandProcessor
+{
+    /// <inheritdoc />
+    public AllowedProcessorsAttribute()
+        : base(typeof(T1), typeof(T2), typeof(T3), typeof(T4), typeof(T5)) { }
 }
