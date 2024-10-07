UnicordDev/DSharpPlus-Unicord

Add entitlement events

DSharpPlus/EventArgs/Entitlement/EntitlementCreatedEventArgs.cs
@@ -1,0 +1,12 @@
+﻿using DSharpPlus.AsyncEvents;
+using DSharpPlus.Entities;
+
+namespace DSharpPlus.EventArgs;
+
+/// <summary>
+/// Represents arguments for EntitlementCreated event.
+/// </summary>
+public class EntitlementCreatedEventArgs : AsyncEventArgs
+{
+    public DiscordEntitlement Entitlement { get; internal set; }
+}

DSharpPlus/EventArgs/Entitlement/EntitlementDeletedEventArgs.cs
@@ -1,0 +1,12 @@
+﻿using DSharpPlus.AsyncEvents;
+using DSharpPlus.Entities;
+
+namespace DSharpPlus.EventArgs;
+
+/// <summary>
+/// Represents arguments for EntitlementDeleted event.
+/// </summary>
+public class EntitlementDeletedEventArgs : AsyncEventArgs
+{
+    public DiscordEntitlement Entitlement { get; internal set; }
+}

DSharpPlus/EventArgs/Entitlement/EntitlementUpdatedEventArgs.cs
@@ -1,0 +1,12 @@
+﻿using DSharpPlus.AsyncEvents;
+using DSharpPlus.Entities;
+
+namespace DSharpPlus.EventArgs;
+
+/// <summary>
+/// Represents arguments for EntitlementUpdated event.
+/// </summary>
+public class EntitlementUpdatedEventArgs : AsyncEventArgs
+{
+    public DiscordEntitlement Entitlement { get; internal set; }
+}

DSharpPlus/DSharpPlus

DSharpPlus/EventArgs/Entitlement/EntitlementCreatedEventArgs.cs
@@ -1,0 +1,12 @@
+﻿using DSharpPlus.AsyncEvents;
+using DSharpPlus.Entities;
+
+namespace DSharpPlus.EventArgs;
+
+/// <summary>
+/// Represents arguments for EntitlementCreated event.
+/// </summary>
+public class EntitlementCreatedEventArgs : DiscordEventArgs
+{
+    public DiscordEntitlement Entitlement { get; internal set; }
+}

DSharpPlus/EventArgs/Entitlement/EntitlementDeletedEventArgs.cs
@@ -1,0 +1,12 @@
+﻿using DSharpPlus.AsyncEvents;
+using DSharpPlus.Entities;
+
+namespace DSharpPlus.EventArgs;
+
+/// <summary>
+/// Represents arguments for EntitlementDeleted event.
+/// </summary>
+public class EntitlementDeletedEventArgs : DiscordEventArgs
+{
+    public DiscordEntitlement Entitlement { get; internal set; }
+}

DSharpPlus/EventArgs/Entitlement/EntitlementUpdatedEventArgs.cs
@@ -1,0 +1,12 @@
+﻿using DSharpPlus.AsyncEvents;
+using DSharpPlus.Entities;
+
+namespace DSharpPlus.EventArgs;
+
+/// <summary>
+/// Represents arguments for EntitlementUpdated event.
+/// </summary>
+public class EntitlementUpdatedEventArgs : DiscordEventArgs
+{
+    public DiscordEntitlement Entitlement { get; internal set; }
+}