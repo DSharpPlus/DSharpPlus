af1c2060da95744700c9b896b0db2b6402b60b71
Plerx <50530305+Plerx2493@users.noreply.github.com>
2024-12-13 22:26:52 +0100

    Only update user cache if interaction has a guild entity

--- DSharpPlus/Clients/DiscordClient.Dispatch.cs
+++ DSharpPlus/Clients/DiscordClient.Dispatch.cs
@@ -2763,7 +2763,7 @@ public sealed partial class DiscordClient
         interaction.Discord = this;
         interaction.Data.Discord = this;
 
-        if (member is not null && guildId is not null)
+        if (member is not null && guildId is not null && interaction.Guild is not null)
         {
             usr = new DiscordMember(member) { guild_id = guildId.Value, Discord = this };
             UpdateUser(usr, guildId, interaction.Guild, member);
