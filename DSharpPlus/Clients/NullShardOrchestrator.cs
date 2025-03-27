48ed1d1c58c5b32a5f4c5cb21d4526b2678eb91a
Lunar Starstrum <lunar@forsaken-borders.net>
2025-03-27 04:13:09 -0500

    🐳

---
+++ DSharpPlus/Clients/NullShardOrchestrator.cs
@@ -0,0 +1,55 @@
+using System;
+using System.Threading.Tasks;
+using DSharpPlus.Entities;
+
+namespace DSharpPlus.Clients;
+
+/// <summary>
+/// Orchestrates a single "shard".
+/// </summary>
+public sealed class NullShardOrchestrator : IShardOrchestrator
+{
+    /// <inheritdoc/>
+    public bool AllShardsConnected { get; private set; }
+
+    /// <inheritdoc/>
+    public int TotalShardCount => 0;
+
+    /// <inheritdoc/>
+    public int ConnectedShardCount => 0;
+
+    /// <inheritdoc/>
+    public ValueTask BroadcastOutboundEventAsync(byte[] payload) => ValueTask.CompletedTask;
+
+    /// <inheritdoc/>
+    public TimeSpan GetConnectionLatency(ulong guildId) => TimeSpan.Zero;
+
+    /// <inheritdoc/>
+    public bool IsConnected(ulong guildId) => this.AllShardsConnected;
+
+    /// <inheritdoc/>
+    public ValueTask ReconnectAsync()
+    {
+        this.AllShardsConnected = true;
+        return ValueTask.CompletedTask;
+    }
+
+    /// <summary>
+    /// Sends an outbound event to Discord.
+    /// </summary>
+    public ValueTask SendOutboundEventAsync(byte[] payload, ulong _) => ValueTask.CompletedTask;
+
+    /// <inheritdoc/>
+    public ValueTask StartAsync(DiscordActivity? activity, DiscordUserStatus? status, DateTimeOffset? idleSince)
+    {
+        this.AllShardsConnected = true;
+        return ValueTask.CompletedTask;
+    }
+
+    /// <inheritdoc/>
+    public ValueTask StopAsync()
+    {
+        this.AllShardsConnected = false;
+        return ValueTask.CompletedTask;
+    }
+}
