e1426d71c4ee618e0dcfaae47f910d86a07c18a9
Plerx <50530305+Plerx2493@users.noreply.github.com>
2025-04-04 16:16:47 +0200

    Add soundboard enpoints

---
+++ DSharpPlus/Net/Abstractions/Rest/RestCreateSoundboardSoundPayload.cs
@@ -0,0 +1,21 @@
+﻿using Newtonsoft.Json;
+
+namespace DSharpPlus.Net.Abstractions.Rest;
+
+internal sealed record RestCreateSoundboardSoundPayload
+{
+    [JsonProperty("name")]
+    public required string Name { get; set; }
+    
+    [JsonProperty("sound")]
+    public required string Sound { get; set; }
+    
+    [JsonProperty("volume", DefaultValueHandling = DefaultValueHandling.Ignore)]
+    public double? Volume { get; set; }
+    
+    [JsonProperty("emoji_id", DefaultValueHandling = DefaultValueHandling.Ignore)]
+    public ulong? EmojiId { get; set; }
+    
+    [JsonProperty("emoji_name", DefaultValueHandling = DefaultValueHandling.Ignore)]
+    public string? EmojiName { get; set; }
+}
