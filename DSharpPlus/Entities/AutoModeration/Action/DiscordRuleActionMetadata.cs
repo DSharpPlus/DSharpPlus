a6584b8ac52e588c7528fc2f3e0d3f45be486865
Invix <74636900+InvixGG@users.noreply.github.com>
2025-07-27 17:51:31 +0200

    improve XML documentation for DiscordRuleActionMetadata (#2355)

--- DSharpPlus/Entities/AutoModeration/Action/DiscordRuleActionMetadata.cs
+++ DSharpPlus/Entities/AutoModeration/Action/DiscordRuleActionMetadata.cs
@@ -9,7 +9,7 @@ namespace DSharpPlus.Entities;
 public class DiscordRuleActionMetadata
 {
     /// <summary>
-    /// Gets the channel which the blocked content should be logged.
+    /// Gets the ID of the channel where blocked content or moderation events should be logged.
     /// </summary>
     [JsonProperty("channel_id")]
     public ulong ChannelId { get; internal set; }
@@ -20,9 +20,8 @@ public class DiscordRuleActionMetadata
     [JsonIgnore]
     public TimeSpan TimeoutSeconds => TimeSpan.FromSeconds(this.DurationSeconds);
 
-    /// Gets the timeout duration in seconds.
     /// <summary>
-    /// Gets the message that will be shown on the user screen whenever their message is blocked.
+    /// Gets the custom message that will be shown to a user when their content is blocked.
     /// </summary>
     [JsonProperty("custom_message", NullValueHandling = NullValueHandling.Ignore)]
     public string? CustomMessage { get; internal set; }
