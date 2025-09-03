29a874a09ec8b4e343d88400208b486993b36595
Plerx <50530305+Plerx2493@users.noreply.github.com>
2025-09-03 19:17:04 +0200

    Add role color and flags support with new DiscordRoleColors and DiscordRoleFlags classes

---
+++ DSharpPlus/Entities/Guild/DiscordRoleFlags.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace DSharpPlus.Entities;
+
+/// <summary>
+/// Flags describing a role's properties.
+/// </summary>
+[Flags]
+public enum DiscordRoleFlags
+{
+    /// <summary>
+    ///	Role can be selected by members in an onboarding prompt.
+    /// </summary>
+    InPrompt = 1 << 0,
+}
