d408d79caf4ebaf9f1e7a4e4f04ddea3291f3668
Sombody101 <84810042+Sombody101@users.noreply.github.com>
2025-09-26 22:15:54 -0600

    Make InteractionAllowedContextsAttribute an unsealed type

--- DSharpPlus.Commands/Processors/SlashCommands/Metadata/InteractionAllowedContextsAttribute.cs
+++ DSharpPlus.Commands/Processors/SlashCommands/Metadata/InteractionAllowedContextsAttribute.cs
@@ -8,7 +8,7 @@ namespace DSharpPlus.Commands.Processors.SlashCommands.Metadata;
 /// Specifies the allowed interaction contexts for a command.
 /// </summary>
 [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = false)]
-public sealed class InteractionAllowedContextsAttribute(params DiscordInteractionContextType[] allowedContexts) : Attribute
+public class InteractionAllowedContextsAttribute(params DiscordInteractionContextType[] allowedContexts) : Attribute
 {
     /// <summary>
     /// The contexts the command is allowed to be used in.
