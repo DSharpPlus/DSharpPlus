3910e2047850bf54a03818394051bd614eda4d6c
Alexander <84968269+MisterX2050@users.noreply.github.com>
2026-03-15 10:48:33 +0100

    moved AddFileOptions to a seperate file

--- DSharpPlus/Entities/BaseDiscordMessageBuilder.cs
+++ DSharpPlus/Entities/BaseDiscordMessageBuilder.cs
@@ -743,44 +743,6 @@ public abstract class BaseDiscordMessageBuilder<T> : IDiscordMessageBuilder wher
 }
 
 /// <summary>
-/// Additional flags for file handling
-/// </summary>
-[Flags]
-public enum AddFileOptions
-{
-    /// <summary>
-    /// No additional behavior. The stream will read to completion and is left at that position after sending.
-    /// </summary>
-    None = 0,
-
-    /// <summary>
-    /// Resets the stream to its original position after sending.
-    /// </summary>
-    ResetStream = 0x1,
-
-    /// <summary>
-    /// Closes the stream upon disposal of the message builder or upon sending if not used in a message builder.
-    /// </summary>
-    /// <remarks>
-    /// Streams will not be disposed upon sending if the file is used in a message builder. Disposal of the message builder is necessary.
-    /// </remarks>
-    CloseStream = 0x2,
-
-    /// <summary>
-    /// Immediately reads the stream to completion and copies its contents to an in-memory stream.
-    /// </summary>
-    /// <remarks>
-    /// <para>
-    /// Note that this incurs an additional memory overhead and may perform synchronous I/O and should only be used if the source stream cannot be kept open any longer.
-    /// </para>
-    /// <para>
-    /// If specified together with <see cref="CloseStream"/>, the stream will closed immediately after the copy.
-    /// </para>
-    /// </remarks>
-    CopyStream = 0x4,
-}
-
-/// <summary>
 /// Base interface for any discord message builder.
 /// </summary>
 public interface IDiscordMessageBuilder : IDisposable, IAsyncDisposable
